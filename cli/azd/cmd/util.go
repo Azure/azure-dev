f3c6daa59fe49b2666d7ccfa6f1429abe46b6e13
Victor Vazquez <victor.vazquez@microsoft.com>
2022-07-08 11:39:23 -0700

    unify colors (#16)
    * unify colors in terminal

--- cli/azd/cmd/util.go
+++ cli/azd/cmd/util.go
@@ -20,7 +20,9 @@ import (
 	"github.com/azure/azure-dev/cli/azd/pkg/environment"
 	"github.com/azure/azure-dev/cli/azd/pkg/templates"
 	"github.com/azure/azure-dev/cli/azd/pkg/tools"
+	"github.com/fatih/color"
 	"github.com/mattn/go-isatty"
+	"github.com/mgutz/ansi"
 )
 
 type Asker func(p survey.Prompt, response interface{}) error
@@ -549,4 +551,23 @@ func ensureProject(path string) error {
 	}
 
 	return nil
+}
+
+// withLinkFormat creates string with hyperlink-looking color
+func withLinkFormat(link string, a ...interface{}) string {
+	// See ansi colors: https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+	// ansi code `30` is the one that matches the survey selection
+	return ansi.Color(fmt.Sprintf(link, a...), "30")
+}
+
+// withHighLightFormat creates string with highlight-looking color
+func withHighLightFormat(text string, a ...interface{}) string {
+	return color.CyanString(text, a...)
+}
+
+// printWithStyling prints text to stdout and handles Windows terminals to support
+// escape chars from the text for adding style (color, font, etc)
+func printWithStyling(text string, a ...interface{}) {
+	colorTerminal := color.New()
+	colorTerminal.Printf(text, a...)
 }

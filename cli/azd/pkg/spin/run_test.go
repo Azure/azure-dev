7d7d6b6b9bf29983eaa14d919fc3e8d3caf49df7
Victor Vazquez <vhvb1989@gmail.com>
2022-07-08 00:46:22 -0700

    more

--- cli/azd/pkg/spin/run_test.go
+++ cli/azd/pkg/spin/run_test.go
@@ -11,7 +11,7 @@ import (
 func Test_Run(t *testing.T) {
 	t.Run("FinalFuncs Are Called", func(t *testing.T) {
 		runCount := 0
-		increment := func(s *yacspin.Spinner) {
+		increment := func(s *yacspin.Spinner, noError bool) {
 			assert.NotNil(t, s, "spinner should be passed to final functions")
 			runCount++
 		}

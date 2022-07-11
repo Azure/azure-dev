35cd7a493eaff893097acb9d57247c313bb9973c
Wei Lim <weilim@microsoft.com>
2022-07-11 08:38:10 -0700

    Add info to user-agent when running in GitHub Actions. (#12)
    This change:
    
    - Adds a product identifier `GhActions` to user agent when the CLI is running on GitHub Actions.
    - Centralizes user agent logic
    - Adds running platform info `(Go <Version>; [windows|linux|openbsd|..]/<ARCH>)` to user agent

--- cli/azd/internal/useragent_test.go
+++ cli/azd/internal/useragent_test.go
@@ -8,28 +8,133 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGetUserAgent(t *testing.T) {
-	orig := os.Getenv("AZURE_DEV_USER_AGENT")
-	defer func() { os.Setenv("AZURE_DEV_USER_AGENT", orig) }()
+func TestGetAzDevCliIdentifier(t *testing.T) {
+	version := GetVersionNumber()
+	require.NotEmpty(t, version)
+
+	require.Equal(t, fmt.Sprintf("%s/%s %s", azDevProductIdentifierKey, version, getPlatformInfo()), getAzDevCliIdentifier())
+}
+
+func TestUserSpecifiedAgentIdentifier(t *testing.T) {
+	devUserAgent := "MyAgent/1.0.0"
+	restorer := EnvironmentVariablesSetter(map[string]string{
+		userSpecifiedAgentEnvironmentVariableName: devUserAgent,
+	})
+
+	require.Equal(t, devUserAgent, getUserSpecifiedIdentifier())
+
+	// Empty case
+	os.Setenv(userSpecifiedAgentEnvironmentVariableName, "")
+	require.Equal(t, "", getUserSpecifiedIdentifier())
+
+	t.Cleanup(restorer)
+}
+
+func TestGithubActionIdentifier(t *testing.T) {
+	restorer := EnvironmentVariablesSetter(map[string]string{
+		githubActionsEnvironmentVariableName: "",
+	})
+
+	require.Equal(t, "", getGithubActionsIdentifier())
+
+	// Empty case
+	os.Setenv(githubActionsEnvironmentVariableName, "true")
+	require.Equal(t, "GhActions", getGithubActionsIdentifier())
+
+	t.Cleanup(restorer)
+}
+
+func TestFormatTemplate(t *testing.T) {
+	require.Equal(t, "", formatTemplateIdentifier(""))
+	require.Equal(t, fmt.Sprintf("%s/todo-python-mongo", templateProductIdentifierKey), formatTemplateIdentifier("todo-python-mongo"))
+	require.Equal(t, fmt.Sprintf("%s/todo-csharp-sql@0.0.1-beta", templateProductIdentifierKey), formatTemplateIdentifier("todo-csharp-sql@0.0.1-beta"))
+}
+
+// Scenario tests
+func TestUserAgentStringScenarios(t *testing.T) {
+	restorer := EnvironmentVariablesSetter(map[string]string{
+		userSpecifiedAgentEnvironmentVariableName: "",
+		githubActionsEnvironmentVariableName:      "",
+	})
 
 	version := GetVersionNumber()
 	require.NotEmpty(t, version)
 
-	os.Setenv("AZURE_DEV_USER_AGENT", "")
+	azDevIdentifier := fmt.Sprintf("azdev/%s %s", version, getPlatformInfo())
 
-	require.Equal(t, fmt.Sprintf("azdev/%s", version), FormatUserAgent([]string{}))
-	require.Equal(t, fmt.Sprintf("azdev/%s", version), FormatUserAgent(nil))
-	require.Equal(t, fmt.Sprintf("azdev/%s extra values", version), FormatUserAgent([]string{"extra", "values"}))
+	// Scenario: default agent
+	require.Equal(t, azDevIdentifier, MakeUserAgentString(""))
 
-	os.Setenv("AZURE_DEV_USER_AGENT", "dev_user_agent")
+	// Scenario: user specifies agent variable
+	os.Setenv(userSpecifiedAgentEnvironmentVariableName, "dev_user_agent")
+	require.Equal(t, fmt.Sprintf("%s dev_user_agent", azDevIdentifier), MakeUserAgentString(""))
+	os.Setenv(userSpecifiedAgentEnvironmentVariableName, "")
 
-	require.Equal(t, fmt.Sprintf("azdev/%s dev_user_agent", version), FormatUserAgent([]string{}))
-	require.Equal(t, fmt.Sprintf("azdev/%s dev_user_agent", version), FormatUserAgent(nil))
-	require.Equal(t, fmt.Sprintf("azdev/%s dev_user_agent extra values", version), FormatUserAgent([]string{"extra", "values"}))
+	// Scenario: running on github actions
+	os.Setenv(githubActionsEnvironmentVariableName, "true")
+	require.Equal(t, fmt.Sprintf("%s GhActions", azDevIdentifier), MakeUserAgentString(""))
+	os.Setenv(githubActionsEnvironmentVariableName, "")
+
+	// Scenario: template present
+	require.Equal(t, fmt.Sprintf("%s azdtempl/template@0.0.1", azDevIdentifier), MakeUserAgentString("template@0.0.1"))
+
+	// Scenario: full combination
+	os.Setenv(userSpecifiedAgentEnvironmentVariableName, "dev_user_agent")
+	os.Setenv(githubActionsEnvironmentVariableName, "true")
+	require.Equal(t, fmt.Sprintf("%s dev_user_agent azdtempl/template@0.0.1 GhActions", azDevIdentifier), MakeUserAgentString("template@0.0.1"))
+
+	t.Cleanup(restorer)
 }
 
-func TestFormatTemplateForUserAgent(t *testing.T) {
-	require.Equal(t, "azdtempl/[none]", FormatTemplateForUserAgent(""))
-	require.Equal(t, "azdtempl/todo-python-mongo", FormatTemplateForUserAgent("todo-python-mongo"))
-	require.Equal(t, "azdtempl/todo-csharp-sql@0.0.1-beta", FormatTemplateForUserAgent("todo-csharp-sql@0.0.1-beta"))
+func TestUserAgentString(t *testing.T) {
+	userAgent := UserAgent{
+		azDevCliIdentifier: "cli/1.0.0",
+	}
+
+	require.Equal(t, userAgent.String(), userAgent.azDevCliIdentifier)
+
+	// Verify complete formatting
+	userAgent = UserAgent{
+		azDevCliIdentifier:      "cli/1.0.0",
+		userSpecifiedIdentifier: "dev-user-agent/0.0.0-beta",
+		templateIdentifier:      "template/mytemplate@2.0.0",
+		githubActionsIdentifier: "gh/3.0.2",
+	}
+
+	require.Equal(
+		t,
+		userAgent.String(),
+		fmt.Sprintf("%s %s %s %s", userAgent.azDevCliIdentifier, userAgent.userSpecifiedIdentifier, userAgent.templateIdentifier, userAgent.githubActionsIdentifier))
+}
+
+// EnvironmentVariablesSetter sets the provided environment variables,
+// returning a function that restores the environment variables to their original values.
+// Example usage:
+// fn test(t *testing.T) {
+//   closer := helpers.EnvironmentVariablesSetter(map[string]string { "FOO_ENV": "Bar", "OTHER_FOO_ENV": "Bar2"})
+//   require.Equal(t, os.GetEnv("FOO_ENV"), "Bar")
+//   require.Equal(t, os.GetEnv("OTHER_FOO_ENV"), "Bar2")
+//   t.Cleanup(closer)
+// }
+//
+func EnvironmentVariablesSetter(envContext map[string]string) func() {
+	restoreContext := map[string]string{}
+	for key, value := range envContext {
+		orig, present := os.LookupEnv(key)
+		if present {
+			restoreContext[key] = orig
+		}
+
+		os.Setenv(key, value)
+	}
+
+	return func() {
+		for key := range envContext {
+			if restoreValue, present := restoreContext[key]; present {
+				os.Setenv(key, restoreValue)
+			} else {
+				os.Unsetenv(key)
+			}
+		}
+	}
 }

0daecd3c19f6f232642ea9de19c6f6caee479394
Wallace Breza <wallace.breza@microsoft.com>
2022-07-11 14:59:07 -0700

    Updates template testing script for more options

--- templates/tests/test-templates.sh
+++ templates/tests/test-templates.sh
@@ -5,6 +5,8 @@ FOLDER_PATH=""
 TEMPLATE_NAME=""
 BRANCH_NAME=""
 ENV_NAME_PREFIX=""
+PLAYWRIGHT_RETRIES="3"
+LOCATION="eastus2"
 
 function usage {
     echo "Tests azd template init, provision & deploy"
@@ -15,6 +17,8 @@ function usage {
     echo "  -b    Sets the template branch name. Override to test a any custom branches (default: main)"
     echo "  -e    Sets the environment name prefix. Environment prefix is used in the azd environment name along with the template name (default: whoami)"
     echo "  -t    Sets the template name. Use values from 'azd template list'. When omitted will run for all templates available in 'azd template list'"
+    echo "  -r    Sets the number of retries for playwright tests (default: 3)"
+    echo "  -l    Sets the Azure location for the template infrastructure (default: eastus2)"
     echo ""
     echo "Examples:"
     echo "  Testing a single template with custom branch"
@@ -26,12 +30,14 @@ function usage {
     exit 1
 }
 
-while getopts "f:t:b:e:h" arg; do
+while getopts "f:t:b:e:r:l:h" arg; do
     case ${arg} in
     f) FOLDER_PATH=$OPTARG ;;
     t) TEMPLATE_NAME=$OPTARG ;;
     b) BRANCH_NAME=$OPTARG ;;
     e) ENV_NAME_PREFIX=$OPTARG ;;
+    r) PLAYWRIGHT_RETRIES=$OPTARG ;;
+    l) LOCATION=$OPTARG ;;
     h)
         usage
         ;;
@@ -62,11 +68,11 @@ if [[ -z $ENV_NAME_PREFIX ]]; then
     ENV_NAME_PREFIX=$(whoami)
 fi
 
-# Tests the specified template
+# Deploys the specified template
 # $1 - The template name
 # $2 - The branch name
 # $3 - The environment name
-function testTemplate {
+function deployTemplate {
     echo "Creating new project folder @ '$FOLDER_PATH/$3'..."
     cd "$FOLDER_PATH"
     mkdir "$3"
@@ -80,28 +86,64 @@ function testTemplate {
 
     echo "Deploying apps for $3..."
     azd deploy -e "$3"
+}
 
+# Tests the specified template
+# $1 - The template name
+# $2 - The branch name
+# $3 - The environment name
+function testTemplate {
     echo "Running template smoke tests for $3..."
-    cd tests
+    cd "$FOLDER_PATH/$3/tests"
     npm i && npx playwright install
-    npx playwright test
+    npx playwright test --retries=$PLAYWRIGHT_RETRIES
+}
 
+# Cleans the specified template
+# $1 - The template name
+# $2 - The branch name
+# $3 - The environment name
+function cleanupTemplate {
     echo "Deprovisioning infrastructure for $3..."
+    cd "$FOLDER_PATH/$3"
     azd down -e "$3" --force --purge
 
     echo "Cleaning up local project @ '$FOLDER_PATH/$3'..."
     rm -rf "$FOLDER_PATH/$3"
 }
 
+export AZURE_LOCATION="$LOCATION"
+
 if [[ -z $TEMPLATE_NAME ]]; then
+    declare -A ENV_TEMPLATE_MAP
+
     # If a template is not specified, run for all templates from output of 'azd template list'
+    echo "Getting list of availabe templates..."
     TEMPLATES_JSON=$(azd template list --output json)
 
-    echo "$TEMPLATES_JSON" | jq -r '.[].name' | sed 's/\\n/\n/g' | while read -r TEMPLATE; do
+    while read -r TEMPLATE; do
         ENV_NAME="${ENV_NAME_PREFIX}-${TEMPLATE:14}-$RANDOM"
+        ENV_TEMPLATE_MAP[$TEMPLATE]=$ENV_NAME
+    done < <( echo "$TEMPLATES_JSON" | jq -r '.[].name' | sed 's/\\n/\n/g' )
 
-        testTemplate "$TEMPLATE" "$BRANCH_NAME" "$ENV_NAME" || continue
+    # Deploy the templates in parallel
+    for TEMPLATE in "${!ENV_TEMPLATE_MAP[@]}"; do
+        (deployTemplate "$TEMPLATE" "$BRANCH_NAME" "${ENV_TEMPLATE_MAP[$TEMPLATE]}" || continue) &
     done
+
+    wait
+
+    # Test the templates serially
+    for TEMPLATE in "${!ENV_TEMPLATE_MAP[@]}"; do
+        testTemplate "$TEMPLATE" "$BRANCH_NAME" "${ENV_TEMPLATE_MAP[$TEMPLATE]}" || continue
+    done
+
+    # Cleanup the templates in parallel
+    for TEMPLATE in "${!ENV_TEMPLATE_MAP[@]}"; do
+        (cleanupTemplate "$TEMPLATE" "$BRANCH_NAME" "${ENV_TEMPLATE_MAP[$TEMPLATE]}" || continue) &
+    done
+
+    wait
 
     echo ""
     echo "Done!"

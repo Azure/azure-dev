97f1f5423afd319c1dca93b7b5b2e3f8f887521c
hemarina <hemarina@microsoft.com>
2022-07-12 14:55:47 -0700

    unique error check

--- cli/azd/pkg/tools/ensure.go
+++ cli/azd/pkg/tools/ensure.go
@@ -37,16 +37,20 @@ func EnsureInstalled(ctx context.Context, tools ...ExternalTool) error {
 	for _, tool := range tools {
 		has, err := tool.CheckInstalled(ctx)
 		var errSem *ErrSemver
-		errorMsg := err.Error()
-		if _, hasV := errorsEncountered[errorMsg]; !hasV {
-			if errors.As(err, &errSem) {
+		if errors.As(err, &errSem) {
+			errorMsg := err.Error()
+			if _, hasV := errorsEncountered[errorMsg]; !hasV {
 				allErrors = append(allErrors, err)
-			} else if err != nil {
-				allErrors = append(allErrors, fmt.Errorf("error checking for external tool %s: %w", tool.Name(), err))
-			} else if !has {
-				allErrors = append(allErrors, fmt.Errorf("%s is not installed, please see %s to install", tool.Name(), tool.InstallUrl()))
+				errorsEncountered[errorMsg] = struct{}{}
 			}
-			errorsEncountered[errorMsg] = struct{}{}
+		} else if err != nil {
+			errorMsg := err.Error()
+			if _, hasV := errorsEncountered[errorMsg]; !hasV {
+				allErrors = append(allErrors, fmt.Errorf("error checking for external tool %s: %w", tool.Name(), err))
+				errorsEncountered[errorMsg] = struct{}{}
+			}
+		} else if !has {
+			allErrors = append(allErrors, fmt.Errorf("%s is not installed, please see %s to install", tool.Name(), tool.InstallUrl()))
 		}
 	}
 

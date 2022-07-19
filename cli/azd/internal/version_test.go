376cd80c7c9ccaef084462044b5b41736c98fdc7
Karol Zadora-Przylecki <karolz@microsoft.com>
2022-07-19 14:24:42 -0700

    Add JSON output to version command

--- cli/azd/internal/version_test.go
+++ cli/azd/internal/version_test.go
@@ -21,3 +21,23 @@ func TestGetVersionNumber(t *testing.T) {
 	Version = ""
 	require.Equal(t, "unknown", GetVersionNumber())
 }
+
+func TestGetVersionSpec(t *testing.T) {
+	orig := Version
+	defer func() { Version = orig }()
+
+	Version = "0.1.0-beta.2 (commit 13ec2b11aa755b11640fa16b8664cb8741d5d300)"
+	vSpec := GetVersionSpec()
+	require.Equal(t, "0.1.0-beta.2", vSpec.Version)
+	require.Equal(t, "13ec2b11aa755b11640fa16b8664cb8741d5d300", vSpec.Commit)
+
+	Version = "invalid"
+	vSpec = GetVersionSpec()
+	require.Equal(t, "unknown", vSpec.Version)
+	require.Equal(t, "unknown", vSpec.Commit)
+
+	Version = ""
+	vSpec = GetVersionSpec()
+	require.Equal(t, "unknown", vSpec.Version)
+	require.Equal(t, "unknown", vSpec.Commit)
+}

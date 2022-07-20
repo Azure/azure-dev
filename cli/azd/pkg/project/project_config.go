137a5cdcb3bbeb2d4580bde9e7644f6d5e44e72a
Wallace Breza <wallace.breza@microsoft.com>
2022-07-20 11:49:04 -0700

    Updates inline comment with reference to moduleName

--- cli/azd/pkg/project/project_config.go
+++ cli/azd/pkg/project/project_config.go
@@ -132,7 +132,7 @@ func ParseProjectConfig(yamlContent string, env *environment.Environment) (*Proj
 		svc.Project = &projectFile
 
 		// By convention, the name of the infrastructure module to use when doing an IaC based deployment is the friendly
-		// name of the service. This may be overridden by the `moduleName` property of `azure.yaml`
+		// name of the service. This may be overridden by the `module` property of `azure.yaml`
 		if svc.Module == "" {
 			svc.Module = key
 		}

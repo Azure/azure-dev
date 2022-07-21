74d97bd02e046183db9a53a068dd8bf77d9faa34
Daniel Jurek <djurek@microsoft.com>
2022-07-21 07:44:55 -0700

    Windows installer path verification

--- cli/installer/test-windows-install.cmd
+++ cli/installer/test-windows-install.cmd
@@ -1,3 +1,1 @@
-powershell -c "if ((Get-ExecutionPolicy) -ne 'Unrestricted') { Set-ExecutionPolicy -ExecutionPolicy 'Unrestricted' -Scope 'Process' }; ./install-azd.ps1 -BaseUrl '%1' -Version '%2' -Verbose;"
-set "PATH=%PATH%;%LOCALAPPDATA%\Programs\Azure Dev CLI\"
-azd version
+powershell -ex AllSigned -c "./install-azd.ps1 -BaseUrl '%1' -Version '' -Verbose"
---
+++ cli/installer/test-windows-uninstall.cmd
@@ -0,0 +1,1 @@
+powershell -ex AllSigned -c "./uninstall-azd.ps1 -Verbose"

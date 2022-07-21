4deceda9400676ddce365742fc921f956623cc50
Daniel Jurek <djurek@microsoft.com>
2022-07-21 13:20:46 -0700

    No quotes

--- cli/installer/test-windows-install.cmd
+++ cli/installer/test-windows-install.cmd
@@ -2,4 +2,4 @@ powershell -ex Bypass -c "./install-azd.ps1 -BaseUrl '%1' -Version '' -Verbose"
 
 REM In DevOps prepend the azd CLI location to DevOps' view of %PATH% so azd is
 REM accessable in subsequent steps.
-echo "##vso[task.prependpath]%LOCALAPPDATA%\Programs\Azure Dev CLI"
+echo ##vso[task.prependpath]%LOCALAPPDATA%\Programs\Azure Dev CLI

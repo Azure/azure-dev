c6e47d090914e9053a398079dd27db2d7c0290cf
Daniel Jurek <djurek@microsoft.com>
2022-07-21 11:14:59 -0700

    Validate update and cleanup of $PATH

--- cli/installer/test-windows-install.cmd
+++ cli/installer/test-windows-install.cmd
@@ -1,1 +1,5 @@
 powershell -ex Bypass -c "./install-azd.ps1 -BaseUrl '%1' -Version '' -Verbose"
+
+REM In DevOps prepend the azd CLI location to DevOps' view of %PATH% so azd is
+REM accessable in subsequent steps.
+echo "##vso[task.prependpath]%LOCALAPPDATA%\Programs\Azure Dev CLI"

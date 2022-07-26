5d57f0eb1aa81e4529c96c72a9b1fd71ddd486cd
Karol Zadora-Przylecki <karolz@microsoft.com>
2022-07-26 15:52:26 -0700

    Fix experimentation-related telemetry
    The call to registerUIExtensionVariables() needs to happen before the call to createExperimentationService(),
    because the former is what actually creates the telemetry reporter.
    
    Also making sure the configuration prefix passed to createAzExtOutputChannel(), but that is a minor issue.

--- ext/vscode/src/extension.ts
+++ ext/vscode/src/extension.ts
@@ -31,7 +31,8 @@ export async function activateInternal(vscodeCtx: vscode.ExtensionContext, loadS
     // The following is necessary for telemetry to work, so do this before callWithTelemetryAndErrorHandling()
     ext.context = vscodeCtx;
     ext.ignoreBundle = false;
-    ext.outputChannel = registerDisposable(createAzExtOutputChannel('Azure Developer', 'az dev'));
+    ext.outputChannel = registerDisposable(createAzExtOutputChannel('Azure Developer', "azure-dev"));
+    registerUIExtensionVariables(ext);
     
     await callWithTelemetryAndErrorHandling(TelemetryId.Activation, async (activationCtx: IActionContext) => {
         activationCtx.errorHandling.rethrow = true;
@@ -43,7 +44,6 @@ export async function activateInternal(vscodeCtx: vscode.ExtensionContext, loadS
         ext.userAgent = `${ext.azureDevExtensionNamespace}/v${vscodeCtx.extension.packageJSON.version}`;
         ext.experimentationSvc = await createExperimentationService(vscodeCtx, undefined);
         ext.activitySvc = new ActivityStatisticsService(vscodeCtx.globalState);
-        registerUIExtensionVariables(ext);
         registerCommands();
         registerDisposable(vscode.tasks.registerTaskProvider('dotenv', new DotEnvTaskProvider()));
         scheduleSurveys(vscodeCtx.globalState, activeSurveys);

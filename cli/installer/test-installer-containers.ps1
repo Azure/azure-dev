09954f0b6d1b01719c6aef49bdf17994c3e3d3b0
Daniel Jurek <djurek@microsoft.com>
2022-08-01 15:46:28 -0700

    Use docker prefix, fix for CentOS

--- cli/installer/test-installer-containers.ps1
+++ cli/installer/test-installer-containers.ps1
@@ -2,13 +2,19 @@ param(
     [Parameter (Mandatory=$true)]
     [string] $BaseUrl,
 
-    [string] $Version = ''
+    [string] $Version = '',
+    [string] $ContainerPrefix = ''
 )
 
 $dockerfiles = Get-ChildItem test/Dockerfile.*
 $exitCode = 0
 foreach ($dockerfile in $dockerfiles) {
-    docker build  . -f $dockerfile -t azd-test --build-arg baseUrl="$BaseUrl" --build-arg version="$Version"
+    docker build  . `
+        -f $dockerfile `
+        -t azd-test `
+        --build-arg baseUrl="$BaseUrl" `
+        --build-arg version="$Version" `
+        --build-arg prefix="$Prefix"
     if ($LASTEXITCODE) {
         Write-Error "Could not build for $dockerfile"
         $exitCode = 1

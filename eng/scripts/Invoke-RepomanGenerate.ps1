bb37d8aaed45fc4a9d5519eb953810d4c2fbabd0
Hadwa Gaber <hadwaa@microsoft.com>
2022-08-04 15:13:22 -0700

    update to set -t to the project template
    Co-authored-by: Hattan Shobokshi <hattan.shobokshi@microsoft.com>

--- eng/scripts/Invoke-RepomanGenerate.ps1
+++ eng/scripts/Invoke-RepomanGenerate.ps1
@@ -8,18 +8,20 @@ param(
 $projectsJson = repoman list --format json | Out-String
 $projects = ConvertFrom-Json $projectsJson
 
-$projectPaths = $projects.projectPath
-
-foreach ($projectPath in $projectPaths) {
+foreach ($project in $projects) {
     $additionalParameters = '--update'
     if ($WhatIf) {
         $additionalParameters = ''
     }
 
+    $projectPath = $project.projectPath
+    $templatePath = $project.templatePath.Replace($projectPath, "")
+
     Write-Host @"
 repoman generate `
     -s $projectPath `
     -o $RunnerTemp `
+    -t $templatePath `
     --branch "$TargetBranchName" `
     --https `
     --fail-on-update-error `
@@ -30,6 +32,7 @@ repoman generate `
     repoman generate `
         -s $projectPath `
         -o $RunnerTemp `
+        -t $templatePath `
         --branch `"$TargetBranchName`" `
         --https `
         --fail-on-update-error `

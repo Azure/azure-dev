6dc6da101a9bfab90249e25e5f2e4b67ae19d3a8
Daniel Jurek <djurek@microsoft.com>
2022-08-05 09:07:09 -0700

    Add pinvoke code to send a WM_SETTINGCHANGE message after setting PATH

--- cli/installer/install-azd.ps1
+++ cli/installer/install-azd.ps1
@@ -61,6 +61,51 @@ param(
     [int] $DownloadTimeoutSeconds = 120
 )
 
+
+# Windows specific:
+# This functions sends a WM_SETTINGCHANGE message which causes new processes to
+# pick up the updates to environment variables. Not calling this funciton after
+# updating environment variables means that new processes will use an older view
+# of the environment variables.
+function broadcastSettingChange {
+    $SEND_MESSAGE_TIMEOUT_DEFINITION = @"
+    [DllImport("user32.dll")]public static extern IntPtr SendMessageTimeout(
+        IntPtr hWnd,
+        uint Msg,
+        UIntPtr wParam,
+        string lParam,
+        uint fuFlags,
+        uint uTimeout,
+        out UIntPtr lpdwResult
+    );
+"@
+
+    # Broadcast environment variable change to Windows. Processes launched after
+    # this broadcast will use the new PATH environment variable.
+    # Use "Environment" as the lParam
+    # https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-settingchange
+    Write-Verbose "Broadcasting environment variable change to Windows"
+    $sendMessageTimeout = Add-Type `
+        -MemberDefinition $SEND_MESSAGE_TIMEOUT_DEFINITION `
+        -Name 'Win322SendMessageTimeout' `
+        -Namespace Win32Functions `
+        -PassThru
+    $messageResult = [UIntPtr]::Zero
+    $result = $sendMessageTimeout::SendMessageTimeout(
+        [IntPtr] 0xffff,        # HWND_BROADCAST
+        0x001A,                 # WM_SETTINGCHANGE
+        [UIntPtr]::Zero,
+        "Environment",
+        0x0002,                 # SMTO_ABORTIFHUNG
+        1000,                   # Wait 1000ms per window
+        [ref] $messageResult
+    )
+
+    if ($result -eq 0) {
+        Write-Error "Windows runtime environment variable update did not succeed. To use azd, log out of Windows and log back in."
+    }
+}
+
 function isLinuxOrMac {
     return $IsLinux -or $IsMacOS
 }
@@ -208,13 +253,12 @@ if (!$NoPath -and !(isLinuxOrMac)) {
         if (!($pathParts -contains $InstallFolder)) {
             Write-Host "Adding $InstallFolder to PATH"
 
-            # Set-ItemProperty preserves value type and also broadcasts the
-            # environment variable change to Windows.
-            Set-ItemProperty `
-                -Path HKCU:\Environment\ `
-                -Name 'PATH' `
-                -Value "$originalPath;$InstallFolder" `
-                -Type $originalValueKind
+            $registryKey.SetValue( `
+                'PATH', `
+                "$originalPath;$InstallFolder", `
+                $originalValueKind `
+            )
+            broadcastSettingChange
 
             # Also add the path to the current session
             $env:PATH += ";$InstallFolder"
--- cli/installer/uninstall-azd.ps1
+++ cli/installer/uninstall-azd.ps1
@@ -3,6 +3,51 @@ param(
     [string] $InstallFolder = ""
 )
 
+
+# Windows specific:
+# This functions sends a WM_SETTINGCHANGE message which causes new processes to
+# pick up the updates to environment variables. Not calling this funciton after
+# updating environment variables means that new processes will use an older view
+# of the environment variables.
+function broadcastSettingChange {
+    $SEND_MESSAGE_TIMEOUT_DEFINITION = @"
+    [DllImport("user32.dll")]public static extern IntPtr SendMessageTimeout(
+        IntPtr hWnd,
+        uint Msg,
+        UIntPtr wParam,
+        string lParam,
+        uint fuFlags,
+        uint uTimeout,
+        out UIntPtr lpdwResult
+    );
+"@
+
+    # Broadcast environment variable change to Windows. Processes launched after
+    # this broadcast will use the new PATH environment variable.
+    # Use "Environment" as the lParam
+    # https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-settingchange
+    Write-Verbose "Broadcasting environment variable change to Windows"
+    $sendMessageTimeout = Add-Type `
+        -MemberDefinition $SEND_MESSAGE_TIMEOUT_DEFINITION `
+        -Name 'Win322SendMessageTimeout' `
+        -Namespace Win32Functions `
+        -PassThru
+    $messageResult = [UIntPtr]::Zero
+    $result = $sendMessageTimeout::SendMessageTimeout(
+        [IntPtr] 0xffff,        # HWND_BROADCAST
+        0x001A,                 # WM_SETTINGCHANGE
+        [UIntPtr]::Zero,
+        "Environment",
+        0x0002,                 # SMTO_ABORTIFHUNG
+        1000,                   # Wait 1000ms per window
+        [ref] $messageResult
+    )
+
+    if ($result -eq 0) {
+        Write-Error "Windows runtime environment variable update did not succeed. To use azd, log out of Windows and log back in."
+    }
+}
+
 function isLinuxOrMac {
     return $IsLinux -or $IsMacOS
 }
@@ -69,13 +114,12 @@ if (isLinuxOrMac) {
             $newPathParts = $pathParts.Where({ $_ -ne $InstallFolder })
             $newPath = $newPathParts -join ';'
 
-            # Set-ItemProperty preserves value type and also broadcasts the
-            # environment variable change to Windows.
-            Set-ItemProperty `
-                -Path HKCU:\Environment\ `
-                -Name 'PATH' `
-                -Value $newPath `
-                -Type $originalValueKind
+            $registryKey.SetValue( `
+                'PATH', `
+                $newPath, `
+                $originalValueKind `
+            )
+            broadcastSettingChange
         } else {
             Write-Host "Could not find an entry for $InstallFolder in PATH"
         }

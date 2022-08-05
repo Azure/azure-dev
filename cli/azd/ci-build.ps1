e7bc5b98155cc07c27fef0f6818645ef05fba0fc
Victor Vazquez <vhvb1989@gmail.com>
2022-08-05 23:24:56 +0000

    revert

--- cli/azd/ci-build.ps1
+++ cli/azd/ci-build.ps1
@@ -2,5 +2,4 @@ param(
     [string] $Version,
     [string] $SourceVersion
 )
-azd version
 go build -ldflags="-X 'github.com/azure/azure-dev/cli/azd/internal.Version=$Version (commit $SourceVersion)'"

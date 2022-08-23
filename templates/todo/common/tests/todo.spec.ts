944af57c6f0133c955277f72e22005695a4da236
hongli750210 <v-hongli1@microsoft.com>
2022-08-23 09:27:57 +0800

    Fix issue#246 at 20220823 by lihong

--- templates/todo/common/tests/todo.spec.ts
+++ templates/todo/common/tests/todo.spec.ts
@@ -2,29 +2,26 @@ import { test, expect } from "@playwright/test";
 import { v4 as uuidv4 } from "uuid";
 
 test("Create and delete item test", async ({ page }) => {
-  await page.goto("/");
+  await page.goto("/", {waitUntil：'networkidle'});
 
-  await new Promise<void>(resolve => setTimeout(() => resolve(), 5000)).then(async () => {
-
-    await expect(page.locator("text=My List").first()).toBeVisible({
-      timeout: 240 * 1000,
-    });
-
-    await expect(page.locator("text=This list is empty.").first()).toBeVisible()
-
-    const guid = uuidv4();
-    console.log(`Creating item with text: ${guid}`);
-
-    await page.locator('[placeholder="Add an item"]').focus();
-    await page.locator('[placeholder="Add an item"]').type(guid);
-    await page.locator('[placeholder="Add an item"]').press("Enter");
-
-    console.log(`Deleting item with text: ${guid}`);
-    await expect(page.locator(`text=${guid}`).first()).toBeVisible()
-
-    await page.locator(`text=${guid}`).click();
-    await page.locator('button[role="menuitem"]:has-text("Delete")').click();
-
-    await expect(page.locator(`text=${guid}`).first()).toBeHidden()
-
+  await expect(page.locator("text=My List").first()).toBeVisible({
+    timeout: 240 * 1000,
   });
+
+  await expect(page.locator("text=This list is empty.").first()).toBeVisible()
+
+  const guid = uuidv4();
+  console.log(`Creating item with text: ${guid}`);
+
+  await page.locator('[placeholder="Add an item"]').focus();
+  await page.locator('[placeholder="Add an item"]').type(guid);
+  await page.locator('[placeholder="Add an item"]').press("Enter");
+
+  console.log(`Deleting item with text: ${guid}`);
+  await expect(page.locator(`text=${guid}`).first()).toBeVisible()
+
+  await page.locator(`text=${guid}`).click();
+  await page.locator('button[role="menuitem"]:has-text("Delete")').click();
+
+  await expect(page.locator(`text=${guid}`).first()).toBeHidden()
+});

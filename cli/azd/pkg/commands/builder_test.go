b9ccad2685c1df7b84fc1b9bd056d046b41b657d
Wei Lim <weikanglim@gmail.com>
2022-08-29 16:47:24 -0700

    Remove scaffolded test

--- cli/azd/pkg/commands/builder_test.go
+++ cli/azd/pkg/commands/builder_test.go
@@ -66,35 +66,3 @@ func TestBuild(t *testing.T) {
 		})
 	}
 }
-
-func Test_createRootContext(t *testing.T) {
-	type args struct {
-		ctx         context.Context
-		cmd         *cobra.Command
-		rootOptions *internal.GlobalCommandOptions
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    context.Context
-		want1   *azdcontext.AzdContext
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, got1, err := createRootContext(tt.args.ctx, tt.args.cmd, tt.args.rootOptions)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("createRootContext() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("createRootContext() got = %v, want %v", got, tt.want)
-			}
-			if !reflect.DeepEqual(got1, tt.want1) {
-				t.Errorf("createRootContext() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
-}

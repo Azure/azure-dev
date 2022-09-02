f2e2b81e340b8658ce4693b2a8761d18d1b33e4e
Wei Lim <weilim@microsoft.com>
2022-09-02 09:41:58 -0700

    what about us

--- cli/azd/pkg/osutil/osversion/osversion_darwin.go
+++ cli/azd/pkg/osutil/osversion/osversion_darwin.go
@@ -6,39 +6,19 @@ package osversion
 #import <Foundation/Foundation.h>
 #import <Foundation/NSProcessInfo.h>
 
-typedef struct _OsVersion {
-	int major;
-	int minor;
-	int patch;
-} OsVersion;
-
-int toInt(NSNumber* i) {
-	if (i == NULL) { return 0; }
-	return i.intValue;
-}
-
-OsVersion toOsVer(NSOperatingSystemVersion ver) {
-	OsVersion v;
-	v.major = toInt(ver.majorVersion);
-	v.minor = toInt(ver.minorVersion);
-	v.patch = toInt(ver.patchVersion);
-
-	return v;
-}
-OsVersion c_getVersion() {
+NSOperatingSystemVersion c_getVersion() {
 	NSProcessInfo *pInfo = [NSProcessInfo processInfo];
 	// check availability of the property operatingSystemVersion (10.10+) at runtime
 	if ([pInfo respondsToSelector:@selector(operatingSystemVersion)])
 	{
-		NSOperatingSystemVersion ver = [pInfo operatingSystemVersion];
-		return toOsVer(ver);
+		return [pInfo operatingSystemVersion];
 	}
 	else
 	{
-		OsVersion version;
-		version.major = 10;
-		version.minor = 9;
-		version.patch = 0;
+		NSOperatingSystemVersion version;
+		version.majorVersion = 10;
+		version.minorVersion = 9;
+		version.patchVersion = 0;
 		return version;
 	}
 }
@@ -49,8 +29,8 @@ import (
 	"fmt"
 )
 
-func verToStr(ver C.OsVersion) string {
-	res := fmt.Sprintf("%d.%d.%d", int(ver.major), int(ver.minor), int(ver.patch))
+func verToStr(ver C.NSOperatingSystemVersion) string {
+	res := fmt.Sprintf("%d.%d.%d", int(ver.majorVersion), int(ver.minorVersion), int(ver.patchVersion))
 	return res
 }
 

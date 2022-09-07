42815b0faa6da6193a0279a70b4d806ada31ed51
Wei Lim <weilim@microsoft.com>
2022-09-07 11:07:30 -0700

    Add documentation

--- cli/azd/internal/telemetry/baggage/baggage.go
+++ cli/azd/internal/telemetry/baggage/baggage.go
@@ -15,6 +15,7 @@ type Baggage struct {
 	m map[attribute.Key]attribute.Value
 }
 
+// NewBaggage creates a properly-constructed Baggage.
 func NewBaggage() Baggage {
 	return Baggage{m: map[attribute.Key]attribute.Value{}}
 }
@@ -28,6 +29,8 @@ func (mb Baggage) copy() Baggage {
 	return copied
 }
 
+// Set sets the provided key value pairs, returning a new copy of Baggage.
+// For any existing keys, the value is overridden.
 func (mb Baggage) Set(keyValue ...attribute.KeyValue) Baggage {
 	if len(keyValue) == 0 {
 		return mb
@@ -41,6 +44,8 @@ func (mb Baggage) Set(keyValue ...attribute.KeyValue) Baggage {
 	return copied
 }
 
+// Delete removes the provided key, returning a new copy of Baggage.
+// The new Baggage copy is returned that should be used.
 func (mb Baggage) Delete(key attribute.Key) Baggage {
 	copied := Baggage{m: make(map[attribute.Key]attribute.Value, len(mb.m))}
 	for k, v := range mb.m {
@@ -52,19 +57,25 @@ func (mb Baggage) Delete(key attribute.Key) Baggage {
 	return copied
 }
 
+// Lookup returns the value of the given key.
+// If the key does not exist, the boolean value returned is false.
 func (mb Baggage) Lookup(key attribute.Key) (val attribute.Value, ok bool) {
 	val, ok = mb.m[key]
 	return
 }
 
+// Get returns the value of the given key.
+// If the key does not exist, the default value is returned.
 func (mb Baggage) Get(key attribute.Key) attribute.Value {
 	return mb.m[key]
 }
 
+// Keys returns a copy of the keys contained.
 func (mb Baggage) Keys() []attribute.Key {
 	return maps.Keys(mb.m)
 }
 
+// Attributes returns a copy of the key-value attributes contained.
 func (mb Baggage) Attributes() []attribute.KeyValue {
 	res := make([]attribute.KeyValue, mb.Len())
 	for k, v := range mb.m {
@@ -74,6 +85,7 @@ func (mb Baggage) Attributes() []attribute.KeyValue {
 	return res
 }
 
+// Len returns the number of attributes contained.
 func (mb Baggage) Len() int {
 	return len(mb.m)
 }

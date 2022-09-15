ca8492df043231c06e3d008dea0af3c63686f565
Wei Lim <weilim@microsoft.com>
2022-09-15 15:13:45 -0700

    openapi support

--- templates/todo/api/java/src/main/java/com/microsoft/azure/simpletodo/model/TodoState.java
+++ templates/todo/api/java/src/main/java/com/microsoft/azure/simpletodo/model/TodoState.java
@@ -1,47 +1,58 @@
 package com.microsoft.azure.simpletodo.model;
 
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import javax.annotation.Generated;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
-
-import javax.annotation.Generated;
 
 /**
  * Gets or Sets TodoState
  */
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-03-15T23:13:58.701016+01:00[Europe/Berlin]")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
 public enum TodoState {
+  
+  TODO("todo"),
+  
+  INPROGRESS("inprogress"),
+  
+  DONE("done");
 
-    TODO("todo"),
+  private String value;
 
-    INPROGRESS("inprogress"),
+  TodoState(String value) {
+    this.value = value;
+  }
 
-    DONE("done");
+  @JsonValue
+  public String getValue() {
+    return value;
+  }
 
-    private String value;
+  @Override
+  public String toString() {
+    return String.valueOf(value);
+  }
 
-    TodoState(String value) {
-        this.value = value;
+  @JsonCreator
+  public static TodoState fromValue(String value) {
+    for (TodoState b : TodoState.values()) {
+      if (b.value.equals(value)) {
+        return b;
+      }
     }
-
-    @JsonCreator
-    public static TodoState fromValue(String value) {
-        for (TodoState b : TodoState.values()) {
-            if (b.value.equals(value)) {
-                return b;
-            }
-        }
-        throw new IllegalArgumentException("Unexpected value '" + value + "'");
-    }
-
-    @JsonValue
-    public String getValue() {
-        return value;
-    }
-
-    @Override
-    public String toString() {
-        return String.valueOf(value);
-    }
+    throw new IllegalArgumentException("Unexpected value '" + value + "'");
+  }
 }
 

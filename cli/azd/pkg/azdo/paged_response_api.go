1597d33b9d4fcf99fc91946421d12502cc912171
Victor Vazquez <vhvb1989@gmail.com>
2022-09-27 20:53:45 +0000

    Change log, pager response, avoid pipeline duplicate

---
+++ cli/azd/pkg/azdo/paged_response_api.go
@@ -0,0 +1,51 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package azdo
+
+import (
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/build"
+)
+
+// Define an API for client methods that returns a page and a continuation token.
+
+// getDefinitionsPager providers a pager to iterate for all the pages from GetDefinitions.
+func getDefinitionsPager(
+	ctx context.Context,
+	client build.Client,
+	projectId *string,
+	pipelineName *string,
+) *runtime.Pager[*build.GetDefinitionsResponseValue] {
+
+	return runtime.NewPager(
+		runtime.PagingHandler[*build.GetDefinitionsResponseValue]{
+			More: func(current *build.GetDefinitionsResponseValue) bool {
+				return current.ContinuationToken != ""
+			},
+			Fetcher: func(ctx context.Context, current **build.GetDefinitionsResponseValue) (*build.GetDefinitionsResponseValue, error) {
+				var response *build.GetDefinitionsResponseValue
+				var err error
+
+				if current == nil {
+					// first page
+					response, err = client.GetDefinitions(ctx, build.GetDefinitionsArgs{
+						Project: projectId,
+						Name:    pipelineName,
+					})
+				} else {
+					// not first page
+					response, err = client.GetDefinitions(ctx, build.GetDefinitionsArgs{
+						ContinuationToken: &((*current).ContinuationToken),
+					})
+				}
+				if err != nil {
+					return nil, err
+				}
+				return response, nil
+			},
+		},
+	)
+}

9fc2dea349806337e8b4b63b006112f90948ee5d
Wallace Breza <wallace.breza@microsoft.com>
2022-10-06 13:50:32 -0700

    WIP: app service

---
+++ cli/azd/pkg/tools/azcli/webapp.go
@@ -0,0 +1,80 @@
+package azcli
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/appservice/armappservice"
+	"github.com/azure/azure-dev/cli/azd/pkg/identity"
+)
+
+type AzCliAppServiceProperties struct {
+	HostNames []string `json:"hostNames"`
+}
+
+func (cli *azCli) GetAppServiceProperties(ctx context.Context, subscriptionId string, resourceGroup string, appName string) (*AzCliAppServiceProperties, error) {
+	client, err := cli.createWebAppsClient(ctx, subscriptionId)
+	if err != nil {
+		return nil, err
+	}
+
+	webApp, err := client.Get(ctx, resourceGroup, appName, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed retrieving webapp properties: %w", err)
+	}
+
+	return &AzCliAppServiceProperties{
+		HostNames: webApp,
+	}
+
+	// res, err := cli.runAzCommand(ctx, "webapp", "show", "--subscription", subscriptionId, "--resource-group", resourceGroup, "--name", appName, "--output", "json")
+	// if isNotLoggedInMessage(res.Stderr) {
+	// 	return AzCliAppServiceProperties{}, ErrAzCliNotLoggedIn
+	// } else if err != nil {
+	// 	return AzCliAppServiceProperties{}, fmt.Errorf("failed running az webapp show: %s: %w", res.String(), err)
+	// }
+
+	// var appServiceProperties AzCliAppServiceProperties
+	// if err := json.Unmarshal([]byte(res.Stdout), &appServiceProperties); err != nil {
+	// 	return AzCliAppServiceProperties{}, fmt.Errorf("could not unmarshal output %s as an AzCliAppServiceProperties: %w", res.Stdout, err)
+	// }
+
+	// return appServiceProperties, nil
+}
+
+func (cli *azCli) DeployAppServiceZip(ctx context.Context, subscriptionId string, resourceGroup string, appName string, deployZipPath string) (string, error) {
+	res, err := cli.runAzCommand(ctx, "webapp", "deployment", "source", "config-zip", "--subscription", subscriptionId, "--resource-group", resourceGroup, "--name", appName, "--src", deployZipPath, "--timeout", "3600", "--output", "json")
+	if isNotLoggedInMessage(res.Stderr) {
+		return "", ErrAzCliNotLoggedIn
+	} else if err != nil {
+		return "", fmt.Errorf("failed running az deployment source config-zip: %s: %w", res.String(), err)
+	}
+
+	return res.Stdout, nil
+}
+
+func (cli *azCli) createWebAppsClient(ctx context.Context, subscriptionId string) (*armappservice.WebAppsClient, error) {
+	cred, err := identity.GetCredentials(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	options := cli.createArmClientOptions(ctx)
+	client, err := armappservice.NewWebAppsClient(subscriptionId, cred, options)
+	if err != nil {
+		return nil, fmt.Errorf("creating WebApps client: %w", err)
+	}
+
+	return client, nil
+}
+
+type CustomSite struct {
+	Id         string               `json:"id"`
+	Name       string               `json:"name"`
+	Properties CustomSiteProperties `json:"properties"`
+}
+
+type CustomSiteProperties struct {
+	DefaultHostName string   `json:"defaultHostName"`
+	HostNames       []string `json:"hostNames"`
+}

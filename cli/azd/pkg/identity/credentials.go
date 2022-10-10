5572f094c20458c163682ff5836b386a64677104
Victor Vazquez <vhvb1989@gmail.com>
2022-10-10 20:33:41 +0000

    undo

--- cli/azd/pkg/identity/credentials.go
+++ cli/azd/pkg/identity/credentials.go
@@ -18,7 +18,7 @@ func GetCredentials(ctx context.Context) (azcore.TokenCredential, error) {
 	cred, ok := ctx.Value(credentialsContextKey).(azcore.TokenCredential)
 
 	if !ok {
-		defaultCreds, err := azidentity.NewAzureCLICredential(nil)
+		defaultCreds, err := azidentity.NewDefaultAzureCredential(nil)
 		if err != nil {
 			return nil, fmt.Errorf("getting azure credentials: %w", err)
 		}

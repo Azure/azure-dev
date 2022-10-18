dc7f2fe87c90f9c5f7471d49ab913aa0527d52a1
hemarina <hemarina@microsoft.com>
2022-10-18 13:27:34 -0700

    test

--- cli/azd/pkg/infra/provisioning/bicep/bicep_provider_test.go
+++ cli/azd/pkg/infra/provisioning/bicep/bicep_provider_test.go
@@ -537,11 +537,7 @@ func prepareDestroyMocks(mockContext *mocks.MockContext) {
 
 	// Purge App configuration
 	mockContext.HttpClient.When(func(request *http.Request) bool {
-		return request.Method == http.MethodPost &&
-			strings.Contains(
-				request.URL.Path,
-				"Microsoft.AppConfiguration/locations/ac-123/deletedConfigurationStores/eastus2/purge",
-			)
+		return request.Method == http.MethodPost && strings.Contains(request.URL.Path, "deletedConfigurationStores/ac-123/purge")
 	}).RespondFn(func(request *http.Request) (*http.Response, error) {
 		return &http.Response{
 			Request:    request,

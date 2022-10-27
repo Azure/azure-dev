106cd01ac45a9d2314763d8c4302eadc1c44f87d
Matt Ellis <matell@microsoft.com>
2022-10-27 14:09:53 -0700

    WIP

--- cli/azd/pkg/auth/cache_posix.go
+++ cli/azd/pkg/auth/cache_posix.go
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-//go:build posix
-// +build posix
+//go:build unix
+// +build unix
 
 package auth
 

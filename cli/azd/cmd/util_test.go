c70d12a7fdbdbfab85ccb2d82c045dd81db65c17
Victor Vazquez <vhvb1989@gmail.com>
2022-11-02 05:00:09 +0000

    test case for nil and not nil

--- cli/azd/cmd/util_test.go
+++ cli/azd/cmd/util_test.go
@@ -12,6 +12,7 @@ import (
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armsubscriptions"
 	"github.com/azure/azure-dev/cli/azd/pkg/convert"
 	"github.com/azure/azure-dev/cli/azd/pkg/infra"
 	"github.com/azure/azure-dev/cli/azd/pkg/input"
@@ -109,5 +110,80 @@ func Test_promptEnvironmentName(t *testing.T) {
 
 		sort.Strings(groups)
 		require.Equal(t, []string{"groupA", "groupB", "groupC"}, groups)
+	})
+}
+
+func Test_getSubscriptionOptions(t *testing.T) {
+	t.Run("no default", func(t *testing.T) {
+		mockContext := mocks.NewMockContext(context.Background())
+		mockContext.HttpClient.When(func(request *http.Request) bool {
+			return strings.Contains(request.URL.Path, "subscriptions/")
+		}).RespondFn(func(request *http.Request) (*http.Response, error) {
+			return mocks.CreateHttpResponseWithBody(request, 200, armsubscriptions.Subscription{
+				ID:             convert.RefOf("SUBSCRIPTION"),
+				SubscriptionID: convert.RefOf("SUBSCRIPTION_ID"),
+				DisplayName:    convert.RefOf("DISPLAY"),
+				TenantID:       convert.RefOf("TENANT"),
+			})
+		})
+		mockContext.HttpClient.When(func(request *http.Request) bool {
+			return request.URL.Path == "/subscriptions"
+		}).RespondFn(func(request *http.Request) (*http.Response, error) {
+			return mocks.CreateHttpResponseWithBody(request, 200, armsubscriptions.ClientListResponse{
+				SubscriptionListResult: armsubscriptions.SubscriptionListResult{
+					Value: []*armsubscriptions.Subscription{
+						{
+							ID:             convert.RefOf("SUBSCRIPTION"),
+							SubscriptionID: convert.RefOf("SUBSCRIPTION_ID_OTHER"),
+							DisplayName:    convert.RefOf("DISPLAY"),
+							TenantID:       convert.RefOf("TENANT"),
+						},
+					},
+				},
+			})
+		})
+		subList, result, err := getSubscriptionOptions(*mockContext.Context)
+
+		require.Nil(t, err)
+		require.EqualValues(t, 2, len(subList))
+		require.EqualValues(t, nil, result)
+	})
+
+	t.Run("default", func(t *testing.T) {
+		mockContext := mocks.NewMockContext(context.Background())
+		mockContext.HttpClient.When(func(request *http.Request) bool {
+			return strings.Contains(request.URL.Path, "subscriptions/")
+		}).RespondFn(func(request *http.Request) (*http.Response, error) {
+			return mocks.CreateHttpResponseWithBody(request, 200, armsubscriptions.Subscription{
+				ID:             convert.RefOf("SUBSCRIPTION"),
+				SubscriptionID: convert.RefOf("SUBSCRIPTION_ID"),
+				DisplayName:    convert.RefOf("DISPLAY"),
+				TenantID:       convert.RefOf("TENANT"),
+			})
+		})
+		mockContext.HttpClient.When(func(request *http.Request) bool {
+			return request.URL.Path == "/subscriptions"
+		}).RespondFn(func(request *http.Request) (*http.Response, error) {
+			return mocks.CreateHttpResponseWithBody(request, 200, armsubscriptions.ClientListResponse{
+				SubscriptionListResult: armsubscriptions.SubscriptionListResult{
+					Value: []*armsubscriptions.Subscription{
+						{
+							ID:             convert.RefOf("SUBSCRIPTION"),
+							SubscriptionID: convert.RefOf("SUBSCRIPTION_ID"),
+							DisplayName:    convert.RefOf("DISPLAY"),
+							TenantID:       convert.RefOf("TENANT"),
+						},
+					},
+				},
+			})
+		})
+		subList, result, err := getSubscriptionOptions(*mockContext.Context)
+
+		require.Nil(t, err)
+		require.EqualValues(t, 2, len(subList))
+		require.NotNil(t, result)
+		defSub, ok := result.(string)
+		require.True(t, ok)
+		require.EqualValues(t, " 1. DISPLAY (SUBSCRIPTION_ID)", defSub)
 	})
 }

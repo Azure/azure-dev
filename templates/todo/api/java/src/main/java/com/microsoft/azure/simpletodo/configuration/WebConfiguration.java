ee5595e69011e28c37e7eee97bb5d75d48f85d67
Mingliang Wang <wangmi@microsoft.com>
2022-11-10 15:53:51 +0800

    spring can not convert string "todo" to enum `TodoState.TODO` without converters.

--- templates/todo/api/java/src/main/java/com/microsoft/azure/simpletodo/configuration/WebConfiguration.java
+++ templates/todo/api/java/src/main/java/com/microsoft/azure/simpletodo/configuration/WebConfiguration.java
@@ -2,11 +2,17 @@ package com.microsoft.azure.simpletodo.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfiguration {
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToTodoStateConverter());
+    }
 
     @Bean
     public WebMvcConfigurer webConfigurer() {

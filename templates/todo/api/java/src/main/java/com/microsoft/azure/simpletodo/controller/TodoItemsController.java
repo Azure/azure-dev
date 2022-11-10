b4c9246f83b6dfd83d2801ae553db1a0a71f17a5
Mingliang Wang <wangmi@microsoft.com>
2022-11-10 14:42:28 +0800

    keep spring annotations only in Api interfaces.

--- templates/todo/api/java/src/main/java/com/microsoft/azure/simpletodo/controller/TodoItemsController.java
+++ templates/todo/api/java/src/main/java/com/microsoft/azure/simpletodo/controller/TodoItemsController.java
@@ -12,40 +12,22 @@ import com.microsoft.azure.simpletodo.model.TodoState;
 import com.microsoft.azure.simpletodo.repository.TodoItemRepository;
 import com.microsoft.azure.simpletodo.repository.TodoListRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
-@RequestMapping("/lists/{listId}/items")
 @RestController
 @RequiredArgsConstructor
-@Validated
 public class TodoItemsController implements ItemsApi {
     private final TodoListRepository todoListRepository;
     private final TodoItemRepository todoItemRepository;
 
-    @RequestMapping(
-        method = RequestMethod.POST,
-        produces = MediaType.APPLICATION_JSON_VALUE,
-        consumes = MediaType.APPLICATION_JSON_VALUE
-    )
-    public ResponseEntity<TodoItem> createItem(
-        @PathVariable(value = "listId") String listId,
-        @Valid @RequestBody(required = false) TodoItem todoItem
-    ) {
+    public ResponseEntity<TodoItem> createItem(String listId, TodoItem todoItem) {
         final Optional<TodoList> optionalTodoList = todoListRepository.findById(listId);
         if (optionalTodoList.isPresent()) {
             todoItem.setListId(listId);
@@ -61,69 +43,27 @@ public class TodoItemsController implements ItemsApi {
         }
     }
 
-    @RequestMapping(
-        method = RequestMethod.DELETE,
-        value = "/{itemId}"
-    )
-    public ResponseEntity<Void> deleteItemById(
-        @PathVariable("listId") String listId,
-        @PathVariable("itemId") String itemId
-    ) {
+    public ResponseEntity<Void> deleteItemById(String listId, String itemId) {
         return todoItemRepository.findTodoItemByListIdAndId(listId, itemId)
             .map(i -> todoItemRepository.deleteTodoItemByListIdAndId(i.getListId(), i.getId()))
             .map(i -> ResponseEntity.noContent().<Void>build())
             .orElse(ResponseEntity.notFound().build());
     }
 
-    @RequestMapping(
-        method = RequestMethod.GET,
-        value = "/{itemId}",
-        produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public ResponseEntity<TodoItem> getItemById(
-        @PathVariable("listId") String listId,
-        @PathVariable("itemId") String itemId
-    ) {
+    public ResponseEntity<TodoItem> getItemById(String listId, String itemId) {
         return todoItemRepository.findTodoItemByListIdAndId(listId, itemId)
             .map(ResponseEntity::ok)
             .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    /**
-     * GET /lists/{listId}/items : Gets Todo items within the specified list
-     *
-     * @param listId The Todo list unique identifier (required)
-     * @param top    The max number of items to returns in a result (optional)
-     * @param skip   The number of items to skip within the results (optional)
-     * @return An array of Todo items (status code 200)
-     * or Todo list not found (status code 404)
-     */
-    @RequestMapping(
-        method = RequestMethod.GET,
-        produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public ResponseEntity<List<TodoItem>> getItemsByListId(
-        @PathVariable("listId") String listId,
-        @Valid @RequestParam(value = "top", required = false, defaultValue = "20") BigDecimal top,
-        @Valid @RequestParam(value = "skip", required = false, defaultValue = "0") BigDecimal skip
-    ) {
+    public ResponseEntity<List<TodoItem>> getItemsByListId(String listId, BigDecimal top, BigDecimal skip) {
         return todoListRepository.findById(listId)
             .map(l -> todoItemRepository.findTodoItemsByTodoList(l.getId(), skip.intValue(), top.intValue()))
             .map(ResponseEntity::ok)
             .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @RequestMapping(
-        method = RequestMethod.PUT,
-        value = "/{itemId}",
-        produces = MediaType.APPLICATION_JSON_VALUE,
-        consumes = MediaType.APPLICATION_JSON_VALUE
-    )
-    public ResponseEntity<TodoItem> updateItemById(
-        @PathVariable("listId") String listId,
-        @PathVariable("itemId") String itemId,
-        @Valid @RequestBody(required = false) TodoItem todoItem
-    ) {
+    public ResponseEntity<TodoItem> updateItemById(String listId, String itemId, TodoItem todoItem) {
         todoItem.setId(itemId);
         todoItem.setListId(listId);
         return todoItemRepository.findTodoItemByListIdAndId(listId, itemId)
@@ -132,32 +72,14 @@ public class TodoItemsController implements ItemsApi {
             .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @RequestMapping(
-        method = RequestMethod.GET,
-        value = "/state/{state}",
-        produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public ResponseEntity<List<TodoItem>> getItemsByListIdAndState(
-        @PathVariable("listId") String listId,
-        @PathVariable("state") TodoState state,
-        @Valid @RequestParam(value = "top", required = false, defaultValue = "20") BigDecimal top,
-        @Valid @RequestParam(value = "skip", required = false, defaultValue = "0") BigDecimal skip
-    ) {
+    public ResponseEntity<List<TodoItem>> getItemsByListIdAndState(String listId, TodoState state, BigDecimal top, BigDecimal skip) {
         return todoListRepository.findById(listId)
             .map(l -> todoItemRepository.findTodoItemsByTodoListAndState(l.getId(), state.name(), skip.intValue(), top.intValue()))
             .map(ResponseEntity::ok)
             .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @RequestMapping(
-        method = RequestMethod.PUT,
-        value = "/state/{state}",
-        consumes = MediaType.APPLICATION_JSON_VALUE
-    )
-    public ResponseEntity<Void> updateItemsStateByListId(
-        @PathVariable("listId") String listId,
-        @PathVariable("state") TodoState state
-    ) {
+    public ResponseEntity<Void> updateItemsStateByListId(String listId, TodoState state, List<String> requestBody) {
         final List<TodoItem> items = todoItemRepository.findTodoItemsByListId(listId);
         items.forEach(item -> item.setState(state));
         todoItemRepository.saveAll(items); // save items in batch.
--- templates/todo/api/java/src/main/java/com/microsoft/azure/simpletodo/controller/TodoListsController.java
+++ templates/todo/api/java/src/main/java/com/microsoft/azure/simpletodo/controller/TodoListsController.java
@@ -4,38 +4,21 @@ import com.microsoft.azure.simpletodo.api.ListsApi;
 import com.microsoft.azure.simpletodo.model.TodoList;
 import com.microsoft.azure.simpletodo.repository.TodoListRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.util.List;
 
-@RequestMapping("/lists")
 @RestController
 @RequiredArgsConstructor
-@Validated
 public class TodoListsController implements ListsApi {
     private final TodoListRepository todoListRepository;
 
-    @RequestMapping(
-        method = RequestMethod.POST,
-        produces = MediaType.APPLICATION_JSON_VALUE,
-        consumes = MediaType.APPLICATION_JSON_VALUE
-    )
-    public ResponseEntity<TodoList> createList(
-        @Valid @RequestBody(required = false) TodoList todoList
-    ) {
+    public ResponseEntity<TodoList> createList(TodoList todoList) {
         final TodoList savedTodoList = todoListRepository.save(todoList);
         URI location = ServletUriComponentsBuilder
             .fromCurrentRequest()
@@ -45,53 +28,24 @@ public class TodoListsController implements ListsApi {
         return ResponseEntity.created(location).body(savedTodoList);
     }
 
-    @RequestMapping(
-        method = RequestMethod.DELETE,
-        value = "/{listId}"
-    )
-    public ResponseEntity<Void> deleteListById(
-        @PathVariable("listId") String listId
-    ) {
+    public ResponseEntity<Void> deleteListById(String listId) {
         return todoListRepository.findById(listId)
             .map(l -> todoListRepository.deleteTodoListById(l.getId()))
             .map(l -> ResponseEntity.noContent().<Void>build())
             .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @RequestMapping(
-        method = RequestMethod.GET,
-        value = "/{listId}",
-        produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public ResponseEntity<TodoList> getListById(
-        @PathVariable("listId") String listId
-    ) {
+    public ResponseEntity<TodoList> getListById(String listId) {
         return todoListRepository.findById(listId)
             .map(ResponseEntity::ok)
             .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @RequestMapping(
-        method = RequestMethod.GET,
-        produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public ResponseEntity<List<TodoList>> getLists(
-        @Valid @RequestParam(value = "top", required = false, defaultValue = "20") BigDecimal top,
-        @Valid @RequestParam(value = "skip", required = false, defaultValue = "0") BigDecimal skip
-    ) {
+    public ResponseEntity<List<TodoList>> getLists(BigDecimal top, BigDecimal skip) {
         return ResponseEntity.ok(todoListRepository.findAll(skip.intValue(), top.intValue()));
     }
 
-    @RequestMapping(
-        method = RequestMethod.PUT,
-        value = "/{listId}",
-        produces = MediaType.APPLICATION_JSON_VALUE,
-        consumes = MediaType.APPLICATION_JSON_VALUE
-    )
-    public ResponseEntity<TodoList> updateListById(
-        @PathVariable(value = "listId") String listId,
-        @Valid @RequestBody @NotNull TodoList todoList
-    ) {
+    public ResponseEntity<TodoList> updateListById(String listId, @NotNull TodoList todoList) {
         todoList.setId(listId);
         return todoListRepository.findById(listId)
             .map(t -> ResponseEntity.ok(todoListRepository.save(todoList)))

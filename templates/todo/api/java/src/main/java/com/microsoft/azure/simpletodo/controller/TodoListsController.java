15d1e23ff5e9de0e0c0e8ae39da02a7f8394d35c
Mingliang Wang <wangmi@microsoft.com>
2022-11-10 14:32:06 +0800

    keep swagger annotations only in Api interfaces.

--- templates/todo/api/java/src/main/java/com/microsoft/azure/simpletodo/controller/TodoListsController.java
+++ templates/todo/api/java/src/main/java/com/microsoft/azure/simpletodo/controller/TodoListsController.java
@@ -1,13 +1,8 @@
 package com.microsoft.azure.simpletodo.controller;
 
+import com.microsoft.azure.simpletodo.api.ListsApi;
 import com.microsoft.azure.simpletodo.model.TodoList;
 import com.microsoft.azure.simpletodo.repository.TodoListRepository;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -30,35 +25,16 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Validated
-@Tag(name = "Lists", description = "the Lists API")
-public class TodoListsController {
+public class TodoListsController implements ListsApi {
     private final TodoListRepository todoListRepository;
 
-    /**
-     * POST /lists : Creates a new Todo list
-     *
-     * @param todoList The Todo List (optional)
-     * @return A Todo list result (status code 201)
-     * or Invalid request schema (status code 400)
-     */
-    @Operation(
-        operationId = "createList",
-        summary = "Creates a new Todo list",
-        tags = {"Lists"},
-        responses = {
-            @ApiResponse(responseCode = "201", description = "A Todo list result", content = {
-                @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = TodoList.class))
-            }),
-            @ApiResponse(responseCode = "400", description = "Invalid request schema")
-        }
-    )
     @RequestMapping(
         method = RequestMethod.POST,
         produces = MediaType.APPLICATION_JSON_VALUE,
         consumes = MediaType.APPLICATION_JSON_VALUE
     )
     public ResponseEntity<TodoList> createList(
-        @Parameter(name = "TodoList", description = "The Todo List") @Valid @RequestBody(required = false) TodoList todoList
+        @Valid @RequestBody(required = false) TodoList todoList
     ) {
         final TodoList savedTodoList = todoListRepository.save(todoList);
         URI location = ServletUriComponentsBuilder
@@ -69,28 +45,12 @@ public class TodoListsController {
         return ResponseEntity.created(location).body(savedTodoList);
     }
 
-    /**
-     * DELETE /lists/{listId} : Deletes a Todo list by unique identifier
-     *
-     * @param listId The Todo list unique identifier (required)
-     * @return Todo list deleted successfully (status code 204)
-     * or Todo list not found (status code 404)
-     */
-    @Operation(
-        operationId = "deleteListById",
-        summary = "Deletes a Todo list by unique identifier",
-        tags = {"Lists"},
-        responses = {
-            @ApiResponse(responseCode = "204", description = "Todo list deleted successfully"),
-            @ApiResponse(responseCode = "404", description = "Todo list not found")
-        }
-    )
     @RequestMapping(
         method = RequestMethod.DELETE,
         value = "/{listId}"
     )
     public ResponseEntity<Void> deleteListById(
-        @Parameter(name = "listId", description = "The Todo list unique identifier", required = true) @PathVariable("listId") String listId
+        @PathVariable("listId") String listId
     ) {
         return todoListRepository.findById(listId)
             .map(l -> todoListRepository.deleteTodoListById(l.getId()))
@@ -98,85 +58,30 @@ public class TodoListsController {
             .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    /**
-     * GET /lists/{listId} : Gets a Todo list by unique identifier
-     *
-     * @param listId The Todo list unique identifier (required)
-     * @return A Todo list result (status code 200)
-     * or Todo list not found (status code 404)
-     */
-    @Operation(
-        operationId = "getListById",
-        summary = "Gets a Todo list by unique identifier",
-        tags = {"Lists"},
-        responses = {
-            @ApiResponse(responseCode = "200", description = "A Todo list result", content = {
-                @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = TodoList.class))
-            }),
-            @ApiResponse(responseCode = "404", description = "Todo list not found")
-        }
-    )
     @RequestMapping(
         method = RequestMethod.GET,
         value = "/{listId}",
         produces = MediaType.APPLICATION_JSON_VALUE
     )
     public ResponseEntity<TodoList> getListById(
-        @Parameter(name = "listId", description = "The Todo list unique identifier", required = true) @PathVariable("listId") String listId
+        @PathVariable("listId") String listId
     ) {
         return todoListRepository.findById(listId)
             .map(ResponseEntity::ok)
             .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    /**
-     * GET /lists : Gets an array of Todo lists
-     *
-     * @param top  The max number of items to returns in a result (optional)
-     * @param skip The number of items to skip within the results (optional)
-     * @return An array of Todo lists (status code 200)
-     */
-    @Operation(
-        operationId = "getLists",
-        summary = "Gets an array of Todo lists",
-        tags = {"Lists"},
-        responses = {
-            @ApiResponse(responseCode = "200", description = "An array of Todo lists", content = {
-                @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = TodoList.class))
-            })
-        }
-    )
     @RequestMapping(
         method = RequestMethod.GET,
         produces = MediaType.APPLICATION_JSON_VALUE
     )
     public ResponseEntity<List<TodoList>> getLists(
-        @Parameter(name = "top", description = "The max number of items to returns in a result, 20 by default") @Valid @RequestParam(value = "top", required = false, defaultValue = "20") BigDecimal top,
-        @Parameter(name = "skip", description = "The number of items to skip within the results, 0 by default ") @Valid @RequestParam(value = "skip", required = false, defaultValue = "0") BigDecimal skip
+        @Valid @RequestParam(value = "top", required = false, defaultValue = "20") BigDecimal top,
+        @Valid @RequestParam(value = "skip", required = false, defaultValue = "0") BigDecimal skip
     ) {
         return ResponseEntity.ok(todoListRepository.findAll(skip.intValue(), top.intValue()));
     }
 
-
-    /**
-     * PUT /lists/{listId} : Updates a Todo list by unique identifier
-     *
-     * @param listId   The Todo list unique identifier (required)
-     * @param todoList The Todo List (optional)
-     * @return A Todo list result (status code 200)
-     * or Todo list is invalid (status code 400)
-     */
-    @Operation(
-        operationId = "updateListById",
-        summary = "Updates a Todo list by unique identifier",
-        tags = {"Lists"},
-        responses = {
-            @ApiResponse(responseCode = "200", description = "A Todo list result", content = {
-                @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = TodoList.class))
-            }),
-            @ApiResponse(responseCode = "400", description = "Todo list is invalid")
-        }
-    )
     @RequestMapping(
         method = RequestMethod.PUT,
         value = "/{listId}",
@@ -184,8 +89,8 @@ public class TodoListsController {
         consumes = MediaType.APPLICATION_JSON_VALUE
     )
     public ResponseEntity<TodoList> updateListById(
-        @Parameter(name = "listId", description = "The Todo list unique identifier", required = true) @PathVariable(value = "listId", required = true) String listId,
-        @Parameter(name = "TodoList", description = "The Todo List") @Valid @RequestBody @NotNull TodoList todoList
+        @PathVariable(value = "listId") String listId,
+        @Valid @RequestBody @NotNull TodoList todoList
     ) {
         todoList.setId(listId);
         return todoListRepository.findById(listId)

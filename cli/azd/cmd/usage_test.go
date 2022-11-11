c1ba726a47dbc82399902c432ba7a18b5a652b24
Wei Lim <weilim@microsoft.com>
2022-11-11 15:29:40 -0800

    Add usage snapshot test

---
+++ cli/azd/cmd/usage_test.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"log"
+	"testing"
+
+	"github.com/gkampitakis/go-snaps/snaps"
+	"github.com/spf13/cobra"
+)
+
+// To update snapshots (assuming your current directory is cli/azd):
+//
+// For Bash,
+// UPDATE_SNAPS=true go test ./cmd
+//
+// For Pwsh,
+// $env:UPDATE_SNAPS='true'; go test ./cmd; $env:UPDATE_SNAPS=$null
+func TestUsage(t *testing.T) {
+	root := NewRootCmd()
+
+	usageSnapshot(t, root)
+}
+
+func usageSnapshot(t *testing.T, cmd *cobra.Command) {
+	t.Run(cmd.Name(), func(t *testing.T) {
+		log.Printf("Command: %s", cmd.CommandPath())
+		snaps.MatchSnapshot(t, cmd.UsageString())
+
+		for _, c := range cmd.Commands() {
+			if !c.IsAvailableCommand() || c.IsAdditionalHelpTopicCommand() {
+				continue
+			}
+
+			usageSnapshot(t, c)
+		}
+	})
+}

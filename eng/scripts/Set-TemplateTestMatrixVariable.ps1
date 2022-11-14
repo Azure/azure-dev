600f686fe8e9bd565f27bc3f5297cb9fe00537fa
Wei Lim <weilim@microsoft.com>
2022-11-14 15:33:15 -0800

    Enable PR validation for template changes (#1102)
    This PR enables template tests to run as part of PR validation for modified templates.
    
    Only the specific templates being modified are tested.

--- eng/scripts/Set-TemplateTestMatrixVariable.ps1
+++ eng/scripts/Set-TemplateTestMatrixVariable.ps1
@@ -67,7 +67,6 @@ if ($TemplateList -eq '(azd template list)') {
 }
 
 if ($TemplateListFilter -ne '.*') {
-} else {
     Write-Host "Filtering with TemplateListFilter regex: $TemplateListFilter"
 
     $templateNames = $templateNames -match $TemplateListFilter

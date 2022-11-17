4f1257df91c4bac91411f5e5f14fb1fa458dadc3
Daniel Jurek <djurek@microsoft.com>
2022-11-17 09:20:15 -0800

    Add version updating scripts

--- eng/scripts/Update-VscodeExtensionVersion.ps1
+++ eng/scripts/Update-VscodeExtensionVersion.ps1
@@ -1,3 +1,22 @@
+<#
+.SYNOPSIS
+Sets the version for the VSCode Extension
+
+.DESCRIPTION
+Sets the verison for the VSCode Extension based on provided `newVersion` or
+updates after release following these rules:
+
+* VSCode does not support prerelease qualifiers for extensions (e.g. -beta.1)
+  source: https://code.visualstudio.com/api/working-with-extensions/publishing-extension#prerelease-extensions
+* Prevent unintended release by ensuring a prerelease qualifier is present in
+  package.json when incrementing version (in this case 'alpha' has been used)
+* Upon release increment the minor version (e.g. y in x.y.z)
+
+0.1.0 -> 0.2.0-alpha.1
+0.1.0-alpha.1 -> 0.2.0-alpha.1 (this scenario is unlikely as versions with prerelease qualifiers are not supported)
+
+#>
+
 param(
     [string] $NewVersion
 )
@@ -26,46 +45,36 @@ function setVersion([string] $version) {
     Set-Content -Path $PACKAGE_JSON -Value $packageJsonString
 }
 
-function vscodeSemVerToString($version) {
-    $output = "$($version.Major).$($version.Minor).$($version.Patch)"
-    if ($version.IsPrerelease) {
-        $output = "$output-$($version.PrereleaseLabel)"
-    }
-
-    return $output
-}
-
-$versionString = $NewVersion
+$version = $NewVersion
 $unreleased = $false
 $replaceLatestEntryTitle = $true
 
-if (!$versionString) {
+if (!$version) {
     # Increment after release
-    $currentVersion = getVersion
+    $version = getVersion
 
-    if ($currentVersion.IsPrerelease) {
-        # 0.1.0 -> 0.2.0-alpha
-        $currentVersion.Minor++
-        $currentVersion.PrereleaseLabel = 'alpha'
-        $currentVersion.PrereleaseNumber = ''
-    } else {
-        # 1.0.0 -> 1.1.0-alpha
-        $currentVersion.Minor++
-        $currentVersion.PrereleaseLabel = 'alpha'
-        $currentVersion.PrereleaseNumber = ''
-        $currentVersion.IsPrerelease = $true
-    }
+    # This implementation differs from the implementation of
+    # IncrementAndSetToPrerelease in that we always increment minor version
+    # (incrementing only the prerelease version would require the releasing dev
+    # to  update the minor version manually before releasing) and that we use
+    # the "alpha" prerelease label.
+
+    # 0.1.0 -> 0.2.0-alpha.1
+    # 0.1.0-alpha.1 -> 0.2.0-alpha.1 (this scenario is unlikely as versions with prerelease qualifiers are not supported)
+    $version.Minor++
+    $version.Patch = 0
+    $version.PrereleaseLabel = 'alpha'
+    $version.PrereleaseNumber = 1
+    $version.IsPrerelease = $true
 
     $unreleased = $true
     $replaceLatestEntryTitle = $false
-
-    $versionString = vscodeSemVerToString $currentVersion
 }
 
-setVersion $versionString
+setVersion $version
 
 . "$PSScriptRoot/../common/scripts/Update-ChangeLog.ps1" `
-    -Version $versionString `
+    -Version $version `
     -ChangeLogPath "$PSScriptRoot/../../ext/vscode/CHANGELOG.md" `
     -Unreleased $unreleased `
     -ReplaceLatestEntryTitle $replaceLatestEntryTitle

791dc80bc7540a717a1cac0974f9330a392e56da
Daniel Jurek <djurek@microsoft.com>
2022-11-18 16:22:32 -0800

    Cleanup old testing

--- eng/scripts/tests/Update-CliVersion.constants.ps1
+++
@@ -1,7 +0,0 @@
-$CliVersionFile = "$PSScriptRoot../../../../cli/version.txt"
-$CliChangelogFile = "$PSScriptRoot../../../../cli/azd/CHANGELOG.md"
-
-function CheckoutChangedFiles {
-    git checkout $CliVersionFile
-    git checkout $CliChangelogFile
-}

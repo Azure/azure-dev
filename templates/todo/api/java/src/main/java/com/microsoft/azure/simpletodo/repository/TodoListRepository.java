bdddfae0277e5dd933fc9984444f143333d905ea
Miller Wang <69189193+wangmingliang-ms@users.noreply.github.com>
2022-11-29 01:07:24 +0800

    Refactor todo-java template and fix bugs. (#1122)
    - Split controllers into `TodoListController` and `TodoItemController`
    - Update `openapi.yaml` to include default values for `top` and `skip`
    - Fix equals & hashCode in `TodoList` and `TodoItem` generated code
    - OpenApiGen: Enable `useTags` to generate separate `ListsApi` and `ItemsApi`
    - Add default formatting for Java source code
    
    Co-authored-by: Wei Lim <weilim@microsoft.com>

--- templates/todo/api/java/src/main/java/com/microsoft/azure/simpletodo/repository/TodoListRepository.java
+++ templates/todo/api/java/src/main/java/com/microsoft/azure/simpletodo/repository/TodoListRepository.java
@@ -1,16 +1,13 @@
 package com.microsoft.azure.simpletodo.repository;
 
 import com.microsoft.azure.simpletodo.model.TodoList;
-
 import java.util.List;
-
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface TodoListRepository extends MongoRepository<TodoList, String> {
-    @Aggregation(pipeline = {
-        "{ '$skip': ?0 }",
-        "{ '$limit': ?1 }",
-    })
+    @Aggregation(pipeline = { "{ '$skip': ?0 }", "{ '$limit': ?1 }" })
     List<TodoList> findAll(int skip, int limit);
+
+    TodoList deleteTodoListById(String id);
 }

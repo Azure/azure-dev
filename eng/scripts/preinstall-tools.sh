9061afb39bccaaae501fa2a1a1d7d50a626c5685
Daniel Jurek <djurek@microsoft.com>
2022-12-05 23:16:59 +0000

    Avoid az in build phase

--- eng/scripts/preinstall-tools.sh
+++ eng/scripts/preinstall-tools.sh
@@ -9,14 +9,15 @@ set pipefail
 
 # Install PowerShell
 # https://learn.microsoft.com/en-us/powershell/scripting/install/install-other-linux?view=powershell-7.3#binary-archives
-
+sudo apt update
+sudo apt install -y curl
 curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.3.0/powershell-7.3.0-linux-arm64.tar.gz
 sudo mkdir -p /opt/microsoft/powershell/7
 sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
 sudo chmod +x /opt/microsoft/powershell/7/pwsh
 sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
 
-# Install az CLI
-curl -L https://aka.ms/InstallAzureCli | bash
+echo "PowerShell install complete:"
+pwsh --version
 
 echo "Pre-reqs installed"

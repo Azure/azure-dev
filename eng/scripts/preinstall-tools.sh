90c4e705fb103a642f7c71d90293e651065f23d2
Daniel Jurek <djurek@microsoft.com>
2022-12-05 22:34:24 +0000

    Logging

--- eng/scripts/preinstall-tools.sh
+++ eng/scripts/preinstall-tools.sh
@@ -14,7 +14,7 @@ set pipefail
 # https://learn.microsoft.com/en-us/powershell/scripting/install/install-ubuntu?view=powershell-7.3
 sudo apt-get update
 sudo apt-get install -y wget apt-transport-https software-properties-common
-wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+wget "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo apt-get install -y powershell

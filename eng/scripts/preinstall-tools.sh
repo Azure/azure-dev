7a246098c8b3e4bdbddc05173d22efc2b870b8aa
Daniel Jurek <djurek@microsoft.com>
2022-12-06 15:22:24 +0000

    Go setup

--- eng/scripts/preinstall-tools.sh
+++ eng/scripts/preinstall-tools.sh
@@ -20,4 +20,13 @@ sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
 echo "PowerShell install complete:"
 pwsh --version
 
+# Install Go (work around GoTool ADO task)
+go_file="go1.19.3.linux-arm64.tar.gz"
+curl -L https://golang.google.cn/dl/$go_file -o $go_file
+rm -rf /usr/local/go && tar -C /usr/local -xzf $go_file
+echo "##vso[task.prependpath]/usr/local/go/bin"
+
+echo "Go install complete:"
+/usr/local/go/bin/go version
+
 echo "Pre-reqs installed"

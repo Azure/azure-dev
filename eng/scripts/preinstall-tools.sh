cf553a1a2371a727f26e22a61f0c8ae2463a36fe
Daniel Jurek <djurek@microsoft.com>
2022-12-06 17:43:55 +0000

    NodeJS and Docker (for testing)

--- eng/scripts/preinstall-tools.sh
+++ eng/scripts/preinstall-tools.sh
@@ -54,4 +54,33 @@ sudo apt update && sudo apt install -y gcc
 echo "GCC installed"
 gcc --version
 
+echo "Install NodeJS"
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
+
+echo "NodeJS installed"
+node --version
+
+echo "Install docker"
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli 
+
+echo "Docker installed"
+sudo systemctl start docker
+docker version
+
 echo "Pre-reqs installed"

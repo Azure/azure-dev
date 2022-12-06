65451af254a8caa399848eb9745cf4ed1c20873b
hemarina <hemarina@microsoft.com>
2022-12-06 10:28:35 -0800

    revert back

--- templates/tests/test-templates.sh
+++ templates/tests/test-templates.sh
@@ -120,29 +120,6 @@ function cleanupTemplate {
     rm -rf "$FOLDER_PATH/${3:?}"
 }
 
-# Waits for the API service to be ready
-# $1 - The template name
-# $2 - The branch name
-# $3 - The environment name
-function waitForApiService {
-    echo "Waiting for API Service for $3..."
-    cd "$FOLDER_PATH/$3"
-
-    local API_URL=$(azd env get-values --output json | jq -r .REACT_APP_API_BASE_URL)
-
-    if [ "$API_URL" == "null" ]; then
-        echo "No REACT_APP_API_BASE_URL export, not waiting."
-        return 0
-    fi
-
-    while [ "$(curl --silent "$API_URL/lists" | jq -r type)" != "array" ]
-    do
-        echo "API Service not ready, waiting 30 seconds."
-        sleep 30
-    done
-}
-
-
 export AZURE_LOCATION="$LOCATION"
 
 if [[ -z $TEMPLATE_NAME ]]; then
@@ -168,7 +145,6 @@ if [[ -z $TEMPLATE_NAME ]]; then
 
     # Test the templates serially
     for TEMPLATE in "${!ENV_TEMPLATE_MAP[@]}"; do
-        waitForApiService "$TEMPLATE" "$BRANCH_NAME" "${ENV_TEMPLATE_MAP[$TEMPLATE]}" || continue
         testTemplate "$TEMPLATE" "$BRANCH_NAME" "${ENV_TEMPLATE_MAP[$TEMPLATE]}" || continue
     done
 

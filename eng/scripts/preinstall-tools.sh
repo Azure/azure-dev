f36ec6d32b651438d7ce17007d2e4059ed8943b9
Daniel Jurek <djurek@microsoft.com>
2022-12-06 16:09:24 +0000

    /tmp

--- eng/scripts/preinstall-tools.sh
+++ eng/scripts/preinstall-tools.sh
@@ -14,7 +14,7 @@ sudo apt update
 sudo apt install -y curl
 curl -L -o "$temp_dir/powershell.tar.gz" https://github.com/PowerShell/PowerShell/releases/download/v7.3.0/powershell-7.3.0-linux-arm64.tar.gz
 sudo mkdir -p /opt/microsoft/powershell/7
-sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+sudo tar zxf "$temp_dir/powershell.tar.gz" -C /opt/microsoft/powershell/7
 sudo chmod +x /opt/microsoft/powershell/7/pwsh
 sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
 

886a68a48922edc69e94b8f1c2295441c70d1f62
Aaron Powell <me@aaron-powell.com>
2022-12-15 03:54:10 +0000

    Disabling linting on long commands

--- cli/azd/pkg/exec/util_test.go
+++ cli/azd/pkg/exec/util_test.go
@@ -223,7 +223,9 @@ func TestRedactSensitiveData(t *testing.T) {
 }`},
 
 		{scenario: "SWADeploymentToken",
-			input:    `npx -y @azure/static-web-apps-cli@1.0.0 deploy --tenant-id abc-123 --subscription-id abc-123 --resource-group r --app-name app-name --app-location / --output-location . --env default --no-use-keychain --deployment-token abc-123`,
+			// nolint:lll
+			input: `npx -y @azure/static-web-apps-cli@1.0.0 deploy --tenant-id abc-123 --subscription-id abc-123 --resource-group r --app-name app-name --app-location / --output-location . --env default --no-use-keychain --deployment-token abc-123`,
+			// nolint:lll
 			expected: `npx -y @azure/static-web-apps-cli@1.0.0 deploy --tenant-id abc-123 --subscription-id abc-123 --resource-group r --app-name app-name --app-location / --output-location . --env default --no-use-keychain --deployment-token <redacted>`},
 
 		{scenario: "DockerLoginUsernameAndPassword",

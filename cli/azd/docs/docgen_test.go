36ca605d56528dc38776d67845d6956c6e3ed2cb
Daniel Jurek <djurek@microsoft.com>
2022-12-16 22:35:58 +0000

    Formatting

--- cli/azd/docs/docgen_test.go
+++ cli/azd/docs/docgen_test.go
@@ -14,11 +14,31 @@ func Test_formatCommandLine(t *testing.T) {
 		args args
 		want string
 	}{
-		{name: "No dollar sign", args: args{line: "hello world"}, want: "hello world"},
-		{name: "No dollar sign", args: args{line: "hello world"}, want: "hello world"},
-		{name: "Dollar sign but not command", args: args{line: "hello $ world"}, want: "hello $ world"},
-		{name: "Command without leading spaces", args: args{line: "$ command --with someParameters"}, want: "command --with someParameters"},
-		{name: "Command with leading spaces", args: args{line: "  $ command --with someParameters"}, want: "  command --with someParameters"},
+		{
+			name: "No dollar sign",
+			args: args{line: "hello world"},
+			want: "hello world",
+		},
+		{
+			name: "No dollar sign",
+			args: args{line: "hello world"},
+			want: "hello world",
+		},
+		{
+			name: "Dollar sign but not command",
+			args: args{line: "hello $ world"},
+			want: "hello $ world",
+		},
+		{
+			name: "Command without leading spaces",
+			args: args{line: "$ command --with someParameters"},
+			want: "command --with someParameters",
+		},
+		{
+			name: "Command with leading spaces",
+			args: args{line: "  $ command --with someParameters"},
+			want: "  command --with someParameters",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {

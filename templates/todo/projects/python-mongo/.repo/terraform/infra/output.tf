e9b5f9e48114b336ba170092fc4ffc8ae238f149
Wenjie Yu (Wicresoft North America Ltd) <v-wenjyu@microsoft.com>
2022-12-21 13:53:16 +0800

    fix output

--- templates/todo/projects/python-mongo/.repo/terraform/infra/output.tf
+++ templates/todo/projects/python-mongo/.repo/terraform/infra/output.tf
@@ -16,7 +16,7 @@ output "REACT_APP_WEB_BASE_URL" {
 }
 
 output "REACT_APP_API_BASE_URL" {
-  value = (var.useAPIM == "true" ) ? module.apimApi. : module.api.URI
+  value = (var.useAPIM == "true" ) ? module.apimApi.SERVICE_API_URI : module.api.URI
 }
 
 output "AZURE_LOCATION" {

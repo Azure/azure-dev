6e418c6e0e0e657419fa04c06cf8a281f197e25e
Daniel Jurek <djurek@microsoft.com>
2023-01-09 08:10:22 -0800

    Simplify test scenarios

--- cli/installer/test-win-install.ps1
+++ cli/installer/test-win-install.ps1
@@ -4,8 +4,6 @@ param(
     [string] $InstallFolder = "$($env:USERPROFILE)\azd-install-test"
 )
 
-
-
 function assertSuccessfulExecution($errorMessage) {
     if ($LASTEXITCODE -or !$?) {
         Write-Error $errorMessage
@@ -49,49 +47,8 @@ try {
     exit 1
     }
 
-    $afterInstallPathType = $regKey.GetValueKind('PATH')
-    if ($originalPathType -ne $afterInstallPathType) {
-        Write-Error "Path registry key type does not match"
-        Write-Error "Expected: $originalPathType"
-        Write-Error "Actual: $afterInstallPathType"
-        exit 1
-    }
-
     & $InstallFolder/azd version
     assertSuccessfulExecution "Could not execute 'azd version'"
-
-    & $PSScriptRoot/uninstall-azd.ps1 -InstallFolder $InstallFolder -Verbose
-    assertSuccessfulExecution "Uninstall failed"
-
-    $currentPath = $regKey.GetValue( `
-        'PATH', `
-        '', `
-        [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames `
-    )
-    $afterUninstallPathType = $regKey.GetValueKind('PATH')
-
-    if ($currentPath -ne $originalPath) {
-        Write-Error "Path does not match original path after uninstall"
-        Write-Error "Expected: $originalPath"
-        Write-Error "Actual: $currentPath"
-        exit 1
-    }
-
-    if ($originalPathType -ne $afterUninstallPathType) {
-        Write-Error "Path registry key type does not match"
-        Write-Error "Expected: $originalPathType"
-        Write-Error "Actual: $afterUninstallPathType"
-        exit 1
-    }
-
-    $azdCommand = Get-Command azd -ErrorAction Ignore
-    if ($azdCommand) {
-        $sourceFolder = Split-Path -Parent (Resolve-Path $azdCommand.Source)
-        if ($sourceFolder -eq $InstallFolder) {
-            Write-Error "Command still availble in tested install location"
-            exit 1
-        }
-    }
 
     Write-Host "Test succeeded"
     exit 0

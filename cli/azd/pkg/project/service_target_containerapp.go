3260dbb4b4cf895afa3a56639ee4c0c4754cb523
Victor Vazquez <vhvb1989@gmail.com>
2023-01-10 01:36:23 +0000

    update silentConsole to mutedConsole with input support

--- cli/azd/pkg/project/service_target_containerapp.go
+++ cli/azd/pkg/project/service_target_containerapp.go
@@ -114,7 +114,9 @@ func (at *containerAppTarget) Deploy(
 		at.config.Infra,
 		at.console.IsUnformatted(),
 		at.cli,
-		&silentConsole{}, // make provision output silence
+		&mutedConsole{
+			parentConsole: at.console,
+		}, // make provision output silence
 		at.commandRunner,
 	)
 	if err != nil {
@@ -270,62 +272,60 @@ func checkResourceType(resource *environment.TargetResource) error {
 // A console implementation which output goes only to logs
 // This is used to prevent or stop actions using the terminal output, for
 // example, when calling provision during deploying a service.
-type silentConsole struct {
+type mutedConsole struct {
+	parentConsole input.Console
 }
 
 // Sets the underlying writer for output the console or
 // if writer is nil, sets it back to the default writer.
-func (sc *silentConsole) SetWriter(writer io.Writer) {
+func (sc *mutedConsole) SetWriter(writer io.Writer) {
 	log.Println("tried to set writer for silent console is a no-op action")
 }
 
-func (sc *silentConsole) GetFormatter() output.Formatter {
+func (sc *mutedConsole) GetFormatter() output.Formatter {
 	return nil
 }
 
-func (sc *silentConsole) IsUnformatted() bool {
+func (sc *mutedConsole) IsUnformatted() bool {
 	return true
 }
 
 // Prints out a message to the underlying console write
-func (sc *silentConsole) Message(ctx context.Context, message string) {
+func (sc *mutedConsole) Message(ctx context.Context, message string) {
 	log.Println(message)
 }
 
-func (sc *silentConsole) MessageUxItem(ctx context.Context, item ux.UxItem) {
+func (sc *mutedConsole) MessageUxItem(ctx context.Context, item ux.UxItem) {
 	sc.Message(ctx, item.ToString(""))
 }
 
-func (sc *silentConsole) ShowSpinner(ctx context.Context, title string, format input.SpinnerUxType) {
+func (sc *mutedConsole) ShowSpinner(ctx context.Context, title string, format input.SpinnerUxType) {
 	log.Printf("request to show spinner on silent console with message: %s", title)
 }
 
-func (sc *silentConsole) StopSpinner(ctx context.Context, lastMessage string, format input.SpinnerUxType) {
+func (sc *mutedConsole) StopSpinner(ctx context.Context, lastMessage string, format input.SpinnerUxType) {
 	log.Printf("request to stop spinner on silent console with message: %s", lastMessage)
 }
 
-// Prompts the user for a single value
-func (sc *silentConsole) Prompt(ctx context.Context, options input.ConsoleOptions) (string, error) {
-	log.Panic("Can't call Prompt from a silent console.")
-	return "", nil
+// Use parent console for input
+func (sc *mutedConsole) Prompt(ctx context.Context, options input.ConsoleOptions) (string, error) {
+	return sc.parentConsole.Prompt(ctx, options)
 }
 
-// Prompts the user to select from a set of values
-func (sc *silentConsole) Select(ctx context.Context, options input.ConsoleOptions) (int, error) {
-	log.Panic("Can't call Select from a silent console.")
-	return 0, nil
+// Use parent console for input
+func (sc *mutedConsole) Select(ctx context.Context, options input.ConsoleOptions) (int, error) {
+	return sc.parentConsole.Select(ctx, options)
 }
 
-// Prompts the user to confirm an operation
-func (sc *silentConsole) Confirm(ctx context.Context, options input.ConsoleOptions) (bool, error) {
-	log.Panic("Can't call Confirm from a silent console.")
-	return false, nil
+// Use parent console for input
+func (sc *mutedConsole) Confirm(ctx context.Context, options input.ConsoleOptions) (bool, error) {
+	return sc.parentConsole.Confirm(ctx, options)
 }
 
-func (sc *silentConsole) GetWriter() io.Writer {
+func (sc *mutedConsole) GetWriter() io.Writer {
 	return nil
 }
 
-func (sc *silentConsole) Handles() input.ConsoleHandles {
-	return input.ConsoleHandles{}
+func (sc *mutedConsole) Handles() input.ConsoleHandles {
+	return sc.parentConsole.Handles()
 }

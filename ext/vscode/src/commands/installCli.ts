0fdbbd929a81ff87bd4e2449796d0a8e9943be63
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2023-02-01 10:54:45 -0500

    Add a command to install AZD

---
+++ ext/vscode/src/commands/installCli.ts
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { DialogResponses, IActionContext } from '@microsoft/vscode-azext-utils';
+import * as os from 'os';
+import { localize } from '../localize';
+import { executeInTerminal } from '../utils/executeInTerminal';
+import { getAzDevTerminalTitle } from './cmdUtil';
+
+const WindowsTerminalCommand = `powershell -ex AllSigned -c "Invoke-RestMethod 'https://aka.ms/install-azd.ps1' | Invoke-Expression"`;
+const LinuxTerminalCommand = `curl -fsSL https://aka.ms/install-azd.sh | bash`;
+const MacTerminalCommand = LinuxTerminalCommand; // Same as Linux
+
+export async function installCli(context: IActionContext, shouldPrompt: boolean = true): Promise<void> {
+    if (shouldPrompt) {
+        const message = localize('azure-dev.commands.cli.install.prompt', 'This will install or update the Azure Developer CLI. Do you want to continue?');
+        // Don't need to check the result, if the user chooses cancel a UserCancelledError will be thrown
+        await context.ui.showWarningMessage(message, { modal: true }, DialogResponses.yes, DialogResponses.cancel);
+    }
+
+    let terminalCommand: string;
+
+    if (os.platform() === 'win32') {
+        terminalCommand = WindowsTerminalCommand;
+    } else if (os.platform() === 'linux') {
+        terminalCommand = LinuxTerminalCommand;
+    } else if (os.platform() === 'darwin') {
+        terminalCommand = MacTerminalCommand;
+    } else {
+        context.errorHandling.suppressReportIssue = true;
+        throw new Error(localize('azure-dev.commands.cli.install.unsupportedPlatform', 'Unsupported platform: {0}', os.platform()));
+    }
+
+    // The installation process will be started but not itself awaited, so we won't know the ultimate result
+    await executeInTerminal(terminalCommand, { name: getAzDevTerminalTitle() });
+}

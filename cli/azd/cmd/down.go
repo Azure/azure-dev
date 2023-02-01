8dfcc33f362b19ef55cd8331090aa356849b1248
Wallace Breza <wallace.breza@microsoft.com>
2023-01-31 16:07:26 -0800

    Fixed issues with flags not resolving on aliased commands

---
+++ cli/azd/cmd/down.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"context"
+
+	"github.com/azure/azure-dev/cli/azd/cmd/actions"
+	"github.com/azure/azure-dev/cli/azd/cmd/middleware"
+	"github.com/azure/azure-dev/cli/azd/internal"
+	"github.com/spf13/cobra"
+)
+
+type downFlags struct {
+	infraDeleteFlags
+}
+
+func newDownFlags(cmd *cobra.Command, infraDeleteFlags *infraDeleteFlags, global *internal.GlobalCommandOptions) *downFlags {
+	flags := &downFlags{}
+	flags.Bind(cmd.Flags(), global)
+
+	return flags
+}
+
+func newDownCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:     "down",
+		Short:   "Delete Azure resources for an app.",
+		Aliases: []string{"infra delete"},
+	}
+}
+
+type downAction struct {
+	runner      middleware.MiddlewareContext
+	infraDelete *infraDeleteAction
+}
+
+func newDownAction(
+	runner middleware.MiddlewareContext,
+	downFlags *downFlags,
+	infraDelete *infraDeleteAction,
+) actions.Action {
+	// Required to ensure the sub action flags are bound correctly to the actions
+	infraDelete.flags = &downFlags.infraDeleteFlags
+
+	return &downAction{
+		infraDelete: infraDelete,
+		runner:      runner,
+	}
+}
+
+func (a *downAction) Run(ctx context.Context) (*actions.ActionResult, error) {
+	runOptions := &middleware.Options{Name: "infradelete"}
+	return a.runner.RunChildAction(ctx, runOptions, a.infraDelete)
+}
---
+++ cli/azd/cmd/provision.go
@@ -0,0 +1,62 @@
+package cmd
+
+import (
+	"context"
+
+	"github.com/azure/azure-dev/cli/azd/cmd/actions"
+	"github.com/azure/azure-dev/cli/azd/cmd/middleware"
+	"github.com/azure/azure-dev/cli/azd/internal"
+	"github.com/spf13/cobra"
+)
+
+type provisionFlags struct {
+	infraCreateFlags
+}
+
+func newProvisionFlags(cmd *cobra.Command, global *internal.GlobalCommandOptions) *provisionFlags {
+	flags := &provisionFlags{}
+	flags.Bind(cmd.Flags(), global)
+
+	return flags
+}
+
+func newProvisionCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:     "provision",
+		Aliases: []string{"infra create"},
+		Short:   "Provision the Azure resources for an app.",
+		//nolint:lll
+		Long: `Provision the Azure resources for an app.
+
+The command prompts you for the following values:
+- Environment name: The name of your environment.
+- Azure location: The Azure location where your resources will be deployed.
+- Azure subscription: The Azure subscription where your resources will be deployed.
+
+Depending on what Azure resources are created, running this command might take a while. To view progress, go to the Azure portal and search for the resource group that contains your environment name.`,
+	}
+}
+
+type provisionAction struct {
+	runner      middleware.MiddlewareContext
+	infraCreate *infraCreateAction
+}
+
+func newProvisionAction(
+	runner middleware.MiddlewareContext,
+	provisionFlags *provisionFlags,
+	infraCreate *infraCreateAction,
+) actions.Action {
+	// Required to ensure the sub action flags are bound correctly to the actions
+	infraCreate.flags = &provisionFlags.infraCreateFlags
+
+	return &provisionAction{
+		runner:      runner,
+		infraCreate: infraCreate,
+	}
+}
+
+func (a *provisionAction) Run(ctx context.Context) (*actions.ActionResult, error) {
+	runOptions := &middleware.Options{Name: "infracreate"}
+	return a.runner.RunChildAction(ctx, runOptions, a.infraCreate)
+}

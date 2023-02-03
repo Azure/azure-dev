e7a5b9449c441141da67abc0a62e68df37929777
Wei Lim <weilim@microsoft.com>
2023-02-03 10:45:14 -0800

    Improve docs

--- cli/azd/pkg/account/models.go
+++ cli/azd/pkg/account/models.go
@@ -7,9 +7,10 @@ type Account struct {
 }
 
 type Subscription struct {
-	Id                 string `json:"id"`
-	Name               string `json:"name"`
-	TenantId           string `json:"tenantId"`
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	TenantId string `json:"tenantId"`
+	// The tenant under which the user has access to the subscription.
 	UserAccessTenantId string `json:"userAccessTenantId"`
 	IsDefault          bool   `json:"isDefault,omitempty"`
 }

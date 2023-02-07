79c01bb3fe5d5a116a7a40d96b6ecae9e4d8a9ca
Daniel Jurek <djurek@microsoft.com>
2023-02-07 13:14:46 -0800

    Embedded packages do not require SHA

--- eng/scripts/New-ChocoPackage.ps1
+++ eng/scripts/New-ChocoPackage.ps1
@@ -1,18 +1,11 @@
 param(
     [string] $Version,
-    [string] $Tag = "azure-dev-cli_$Version",
-    [string] $MsiSha256
+    [string] $Tag = "azure-dev-cli_$Version"
 )
 
 $originalLocation = Get-Location
 try {
     Set-Location "$PSScriptRoot../../../cli/installer/choco"
-
-    # Set SHA256 in chocolateyinstall.ps1
-    $CHOCO_INSTALL_SCRIPT_PATH = 'tools/chocolateyinstall.ps1'
-    $chocolateyInstallContent = Get-Content -Raw $CHOCO_INSTALL_SCRIPT_PATH
-    $chocolateyInstallContent = $chocolateyInstallContent.Replace('%SHA256%', $MsiSha256)
-    Set-Content -Path $CHOCO_INSTALL_SCRIPT_PATH -Value $chocolateyInstallContent
 
     # Copy NOTICE.txt to tools directory
     Copy-Item -Path "$PSScriptRoot/../../NOTICE.txt" -Destination 'tools/NOTICE.txt'

bb5714a46938b8e4dd333e584ca45b67b0d14804
Daniel Jurek <djurek@microsoft.com>
2023-02-08 09:11:47 -0800

    Add % to brew publishing

--- eng/scripts/Update-HomebrewFormula.ps1
+++ eng/scripts/Update-HomebrewFormula.ps1
@@ -8,6 +8,6 @@ param(
 $sha256 =  (Get-FileHash -Path $ZipFilePath -Algorithm SHA256).Hash.ToLower()
 
 $content = Get-Content $TemplatePath -Raw
-$updatedContent = $content.Replace('%VERSION%', $Version).Replace('%SHA256', $sha256)
+$updatedContent = $content.Replace('%VERSION%', $Version).Replace('%SHA256%', $sha256)
 
 Set-Content -Path $OutFile -Value $updatedContent

72d9225f4334b3c88e5232b2142e6d6d91af2e68
Daniel Jurek <djurek@microsoft.com>
2023-02-15 07:53:13 -0800

    Build packages using FPM

---
+++ eng/scripts/New-LinuxPackages.ps1
@@ -0,0 +1,43 @@
+param(
+    $Version = $env:CLI_VERSION
+)
+
+# Iterate over entire item
+$PACKAGE_TYPES = 'deb', 'rpm'
+
+$originalLocation = Get-Location
+try { 
+    Set-Location "$PSScriptRoot/../../cli/installer/fpm"
+
+    if (!(Test-Path "./azd-linux-amd64")) { 
+        Write-Error "Cannot find azd-linux-amd64"
+    }
+    Copy-Item "$PSScriptRoot/../../NOTICE.txt" "NOTICE.txt"
+    Copy-Item "$PSScriptRoot/../../LICENSE" "LICENSE"
+
+    # Symlink points to potentially invalid location but will point correctly 
+    # once package is installed
+    ln -s /opt/microsoft/azd/azd-linux-amd64 azd
+    chmod +x azd
+    chmod +x azd-linux-amd64
+
+    foreach ($type in $PACKAGE_TYPES) { 
+        fpm `
+            --output-type $type `
+            --version $Version `
+            --architecture amd64 `
+            --force `
+            azd-linux-amd64=/opt/microsoft/azd/azd-linux-amd64 `
+            azd=/usr/local/bin/azd `
+            NOTICE.txt=/opt/microsoft/azd/NOTICE.txt `
+            LICENSE=/opt/microsoft/azd/LICENSE
+        
+        if ($LASTEXITCODE) { 
+            Write-Host "Error building package type: $type"
+            exit $LASTEXITCODE
+        }
+    }
+
+} finally { 
+    Set-Location $originalLocation
+}
---
+++ eng/scripts/Test-LinuxPackages.ps1
@@ -0,0 +1,28 @@
+# TODO: Parameterize?
+$PACKAGE_TYPES = 'deb', 'rpm'
+
+$originalLocation = Get-Location 
+try {
+    Set-Location "$PSSCriptRoot/../../cli/installer/fpm"
+
+    $currentPath = (Get-Location).Path
+
+    foreach ($type in $PACKAGE_TYPES) { 
+        docker build . -f "test-$type.Dockerfile" -t test-linux-package
+        if ($LASTEXITCODE) { 
+            Write-Host "Error building test container for type: $type"
+            exit 1
+        }
+
+        docker run `
+            -v "$($currentPath):/work" `
+            -t test-linux-package
+
+        if ($LASTEXITCODE) { 
+            Write-Host "Error running test container for type: $type"
+            exit 1
+        }
+    }
+} finally { 
+    Set-Location $originalLocation
+}

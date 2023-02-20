3beb310dbe7ff200c8d838ae6a81c94836eee34d
Daniel Jurek <djurek@microsoft.com>
2023-02-20 12:44:23 -0800

    Use MSI versioning scheme

--- eng/scripts/Get-MsiVersion.Tests.ps1
+++ eng/scripts/Get-MsiVersion.Tests.ps1
@@ -16,3 +16,31 @@ Describe 'Valid verison numbers' {
         $actual | Should -BeExactly $expected
     }
 }
+
+Describe 'getSemverParsedVersion' {
+    BeforeEach {
+        # Import cmdlet functions into current session
+        . "$PSScriptRoot/Get-MsiVersion.ps1" -CliVersion 9.9.9
+    }
+
+    It 'Given <version> returns <expected>' -ForEach @(
+        @{ version = '1.2.3-beta.1'; expected = '1.2.3-beta.1' },
+        @{ version = '1.2.3'; expected = '1.2.3' },
+        @{ version = '0.4.0-beta.2-pr.2021242'; expected = '0.4.0-beta.2'},
+        @{ version = '0.4.0-beta.2-daily.2026027'; expected = '0.4.0-beta.2'}
+    ) {
+        $actual = getSemverParsedVersion $version
+        $actual | Should -Be $expected
+    }
+
+    It 'Throws on unexpected version of <version>' -ForEach @(
+        @{ version = '1.2.3-beta.1.2.3' },
+        @{ version = '1.2.3-beta.1.pr.123' },
+        @{ version = '1.2.3-beta' },
+        @{ version = '1.2.3-beta.100' }
+        @{ version = '1.2.3-beta.0' }
+        @{ version = '1.2.3-beta.-1' }
+    ) {
+        { getSemverParsedVersion $version } | Should -Throw
+    }
+}

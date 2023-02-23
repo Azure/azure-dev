b0046e5fba3f3e31f45c6cd6efcb4cdca0cdb9e9
Victor Vazquez <vhvb1989@gmail.com>
2023-02-23 00:39:21 -0800

    azd help done

--- cli/azd/cmd/root.go
+++ cli/azd/cmd/root.go
@@ -21,6 +21,7 @@ import (
 	"github.com/azure/azure-dev/cli/azd/internal/telemetry"
 	"github.com/azure/azure-dev/cli/azd/pkg/output"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 const (
@@ -29,6 +30,8 @@ const (
 	cmdGroupManage  string = string(i18nCmdGroupTitleManage)
 	cmdGroupMonitor string = string(i18nCmdGroupTitleMonitor)
 	cmdGroupAbout   string = string(i18nCmdGroupTitleAbout)
+	// this is used for aligning titles in the console.
+	endOfTitleSentinel string = "\x00"
 )
 
 func annotateGroupCmd(cmd *cobra.Command, group string) {
@@ -261,18 +264,91 @@ func getRootCmdHelp(cmd *cobra.Command) string {
 		output.WithBold(output.WithUnderline(i18nGetText(i18nUsage))), i18nGetText(i18nAzdUsage))
 	commands := fmt.Sprintf("%s\n",
 		output.WithBold(output.WithUnderline(i18nGetText(i18nCommands))))
-	commandsDetails := getCommandsDetails(cmd)
+	commandsDetails := getRootCommandsDetails(cmd)
 	flags := fmt.Sprintf("%s\n",
 		output.WithBold(output.WithUnderline(i18nGetText(i18nFlags))))
-	return fmt.Sprintf("\n%s\n\n%s\n%s%s%s",
+	flagsDetails := getFlagsDetails(cmd)
+	footer := getRootCmdFooter(cmd)
+	return fmt.Sprintf("\n%s\n\n%s\n%s%s%s%s\n%s\n",
 		description,
 		usage,
 		commands,
 		commandsDetails,
-		flags)
+		flags,
+		flagsDetails,
+		footer)
 }
 
-func getCommandsDetails(cmd *cobra.Command) (result string) {
+func getRootCmdFooter(cmd *cobra.Command) string {
+	return fmt.Sprintf("%s %s %s %s %s\n\n%s\n  %s %s %s %s\n  %s %s.\n    %s\n\n%s %s.\n",
+		i18nGetText(i18nUse),
+		output.WithHighLightFormat(i18nGetText(i18nAzd)),
+		output.WithWarningFormat("[%s]", i18nGetText(i18nCommand)),
+		output.WithHighLightFormat("--%s", i18nGetText(i18nHelp)),
+		i18nGetText(i18nCmdRootHelpFooterTitle),
+		output.WithBold(output.WithUnderline(i18nGetText(i18nCmdRootHelpFooterQuickStart))),
+		i18nGetText(i18nCmdRootHelpFooterQuickStartDetail),
+		output.WithHighLightFormat(i18nGetText(i18nAzdUpTemplate)),
+		output.WithWarningFormat("[%s]", i18nGetText(i18nTemplateName)),
+		i18nGetText(i18nCmdRootHelpFooterQuickStartLast),
+		output.WithGrayFormat(i18nGetText(i18nCmdRootHelpFooterQuickStartNote)),
+		output.WithLinkFormat(i18nGetText(i18nAwesomeAzdUrl)),
+		output.WithHighLightFormat(i18nGetText(i18nAzdUpNodeJsMongo)),
+		i18nGetText(i18nCmdRootHelpFooterReportBug),
+		output.WithLinkFormat(i18nGetText(i18nAzdHats)),
+	)
+}
+
+func getFlagsDetails(cmd *cobra.Command) (result string) {
+	persistedFlags := cmd.PersistentFlags()
+	var lines []string
+	max := 0
+	persistedFlags.VisitAll(func(flag *pflag.Flag) {
+		if flag.Hidden {
+			return
+		}
+
+		line := ""
+		if flag.Shorthand != "" && flag.ShorthandDeprecated == "" {
+			line = fmt.Sprintf("  -%s, --%s", flag.Shorthand, flag.Name)
+		} else {
+			line = fmt.Sprintf("      --%s", flag.Name)
+		}
+
+		varName, usage := pflag.UnquoteUsage(flag)
+		if varName != "" {
+			line += " " + varName
+		}
+
+		// insert a sentinel for the end of the flag titles. Lines are aligned based on the longest line.
+		line += endOfTitleSentinel
+		lineLen := len(line)
+		if lineLen > max {
+			// the max value is used later to fill all lines with same size
+			max = lineLen
+		}
+		line += usage
+		if len(flag.Deprecated) != 0 {
+			line += fmt.Sprintf(" (DEPRECATED: %s)", flag.Deprecated)
+		}
+
+		lines = append(lines, line)
+	})
+
+	alignTitles(lines, max)
+	return fmt.Sprintf("  %s\n", strings.Join(lines, "\n  "))
+}
+
+func alignTitles(lines []string, longestLineLen int) {
+	for i, line := range lines {
+		sentinelIndex := strings.Index(line, endOfTitleSentinel)
+		// calculate the difference between the longest line to each line ending. It's 0 for the longest
+		gapToFill := strings.Repeat(" ", longestLineLen-sentinelIndex)
+		lines[i] = fmt.Sprintf("%s%s\t: %s", line[:sentinelIndex], gapToFill, line[sentinelIndex+1:])
+	}
+}
+
+func getRootCommandsDetails(cmd *cobra.Command) (result string) {
 	childrenCommands := cmd.Commands()
 	groups := []i18nTextId{
 		i18nCmdGroupTitleConfig, i18nCmdGroupTitleManage, i18nCmdGroupTitleMonitor, i18nCmdGroupTitleAbout}
@@ -280,9 +356,13 @@ func getCommandsDetails(cmd *cobra.Command) (result string) {
 	var commandGroups = make(map[i18nTextId][]string, len(groups))
 	// Add hardcoded message for help, as there is not a command for it and we want it in the list
 	commandGroups[i18nCmdGroupTitleAbout] = append(commandGroups[i18nCmdGroupTitleAbout],
-		fmt.Sprintf("%s\t: %s", i18nGetText(i18nHelp), i18nGetText(i18nCmdHelp)))
+		fmt.Sprintf("%s%s%s", i18nGetText(i18nHelp), endOfTitleSentinel, i18nGetText(i18nCmdHelp)))
+
+	// stores the longes line len
+	max := 0
 
 	for _, childCommand := range childrenCommands {
+		// we rely on commands annotations for command grouping. Commands w/o annotation are ignored.
 		if childCommand.Annotations == nil {
 			continue
 		}
@@ -292,18 +372,17 @@ func getCommandsDetails(cmd *cobra.Command) (result string) {
 		}
 		groupType := i18nTextId(group)
 
-		// if the name of the command is les than 4 chars, we need an extra tab.
 		commandName := childCommand.Name()
-		tabs := "\t"
-		if len(commandName) < 4 {
-			tabs += "\t"
+		commandNameLen := len(commandName)
+		if commandNameLen > max {
+			max = commandNameLen
 		}
-
-		// newList := commandGroups[groupType]
-		// newList = append(newList, fmt.Sprintf("%s%s: %s", commandName, tabs, childCommand.Short))
-		// commandGroups[groupType] = newList
 		commandGroups[groupType] = append(commandGroups[groupType],
-			fmt.Sprintf("%s%s: %s", commandName, tabs, childCommand.Short))
+			fmt.Sprintf("%s%s%s", commandName, endOfTitleSentinel, childCommand.Short))
+	}
+	// align all lines
+	for id := range commandGroups {
+		alignTitles(commandGroups[id], max)
 	}
 
 	for _, title := range groups {

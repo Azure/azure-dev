e64b93ba1ea2de67bc894c002de00a2a9f6a13a6
Victor Vazquez <vhvb1989@gmail.com>
2023-03-03 16:31:11 -0800

    use sdk identity preview in mvn

--- templates/todo/api/java/src/main/java/com/microsoft/azure/simpletodo/configuration/MongoDBConfiguration.java
+++ templates/todo/api/java/src/main/java/com/microsoft/azure/simpletodo/configuration/MongoDBConfiguration.java
@@ -5,13 +5,10 @@
 
 package com.microsoft.azure.simpletodo.configuration;
 
-import com.mongodb.ConnectionString;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.Arrays;
 import java.util.Date;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
@@ -25,16 +22,6 @@ public class MongoDBConfiguration {
         return new MongoCustomConversions(
             Arrays.asList(new OffsetDateTimeReadConverter(), new OffsetDateTimeWriteConverter())
         );
-    }
-
-    @Bean
-    public MongoClientSettingsBuilderCustomizer customizer(
-        @Value("${custom.azure.keyvault.secret.mongodb}") String keyVaultSettings
-    ) {
-        var config = keyVaultSettings.split(",", 0);
-        var connString = ConfigurationSources.KeyVaultSecrets.get(config[0], config[1]);
-        ConnectionString connection = new ConnectionString(connString);
-        return settings -> settings.applyConnectionString(connection);
     }
 
     static class OffsetDateTimeWriteConverter implements Converter<OffsetDateTime, Date> {

eedbf429ce8358fa59f5fda5aee49091ac3c8f3d
Daniel Jurek <djurek@microsoft.com>
2023-03-08 05:24:59 +0000

    Test sh install errors in a separate script

--- cli/installer/test-sh-install.sh
+++ cli/installer/test-sh-install.sh
@@ -9,7 +9,6 @@ say() {
     printf "test-sh-install: %b\n" "$1"
 }
 
-# Normal install scenario
 if ! cat ./install-azd.sh | "$1" -s -- --verbose --base-url "$2" --version "$3"; then
     say_error "Install failed"
     exit 1
@@ -27,20 +26,6 @@ fi
 
 if which azd; then
     say_error "Uninstall did not remove azd"
-    exit 1
-fi
-
-
-# Test install when folder does not exist
-install_folder_error=$(cat ./install-azd.sh | "$1" -s -- --no-telemetry --verbose --base-url "$2" --version "$3" --install-folder "/install/folder/does/not/exist" 2>&1)
-
-if [ ! $? ]; then
-    say_error "Install should have failed on folder not existing"
-    exit 1
-fi
-
-if [[ "$install_folder_error" != *"Install folder does not exist"* ]]; then
-    say_error "Install should have notified the user that the folder does not exist"
     exit 1
 fi
 

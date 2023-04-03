a10a90a55587355dce887bc081521095642e2ef2
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2023-04-03 12:28:50 -0400

    Scope to a single modal delete prompt

--- ext/vscode/src/commands/down.ts
+++ ext/vscode/src/commands/down.ts
@@ -10,36 +10,27 @@ import { TelemetryId } from '../telemetry/telemetryId';
 import { AzureDevCliApplication } from '../views/workspace/AzureDevCliApplication';
 import { isTreeViewModel, TreeViewModel } from '../utils/isTreeViewModel';
 import { localize } from '../localize';
+import { MessageItem } from 'vscode';
+import { DialogResponses } from '@microsoft/vscode-azext-utils';
 
 export async function down(context: IActionContext, selectedItem?: vscode.Uri | TreeViewModel): Promise<void> {
     const selectedFile = isTreeViewModel(selectedItem) ? selectedItem.unwrap<AzureDevCliApplication>().context.configurationFile : selectedItem;
     const workingFolder = await getWorkingFolder(context, selectedFile);
 
-    const confirmPrompt = localize('azure-dev.commands.cli.down.confirm-prompt', "Are you sure you want to delete all application's Azure resources?");
-    const confirmAck = localize('azure-dev.commands.cli.down.confirm-ack', "Delete resources");
+    const confirmPrompt = localize('azure-dev.commands.cli.down.confirm-prompt', "Are you sure you want to delete all application's Azure resources? You can soft-delete certain resources like Azure KeyVaults to preserve their data, or permanently delete and purge them.");
+    const confirmTitle = localize('azure-dev.commands.cli.down.confirm-ack', "Delete resources");
 
-    await context.ui.showWarningMessage(confirmPrompt, { modal: true }, { title: confirmAck });
+    const softDelete: MessageItem = { title: localize("azure-dev.commands.cli.down.soft-delete", "Soft Delete") };
+    const purgeDelete: MessageItem = { title: localize("azure-dev.commands.cli.down.purge-delete", "Delete and Purge") };
 
-    const choices = [
-        {
-            label: localize("azure-dev.commands.cli.down.no-purge", "Do not purge"),
-            data: false
-        },
-        {
-            label: localize("azure-dev.commands.cli.down.purge", "Permanently delete (purge)"),
-            data: true
-        }
-    ];
-    const purgeChoice = await context.ui.showQuickPick(choices, {
-        placeHolder: localize("azure-dev.commands.cli.down.purge-prompt", "Permanently delete resources with are soft-deleted by default (e.g. KeyVaults)?"), // TODO: Should this be title?
-        suppressPersistence: true,
-        canPickMany: false,
-    });
-    context.telemetry.properties.purge = purgeChoice ? 'true' : 'false';
+    // If cancel is chosen or the modal is closed, a `UserCancelledError` will automatically be thrown, so we don't need to check for it
+    const choice = await context.ui.showWarningMessage(confirmPrompt, { modal: true }, { title: confirmTitle }, purgeDelete, softDelete, DialogResponses.cancel);
+
+    context.telemetry.properties.purge = choice === purgeDelete ? 'true' : 'false';
 
     const azureCli = await createAzureDevCli(context);
     const command = azureCli.commandBuilder.withArg('down').withArg('--force');
-    if (purgeChoice.data) {
+    if (choice === purgeDelete) {
         command.withArg('--purge');
     }
 

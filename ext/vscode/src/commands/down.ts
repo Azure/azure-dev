628354a33a0b049c3e6342d0a560817921f60428
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2023-04-03 12:37:58 -0400

    Delete extra button

--- ext/vscode/src/commands/down.ts
+++ ext/vscode/src/commands/down.ts
@@ -18,13 +18,12 @@ export async function down(context: IActionContext, selectedItem?: vscode.Uri | 
     const workingFolder = await getWorkingFolder(context, selectedFile);
 
     const confirmPrompt = localize('azure-dev.commands.cli.down.confirm-prompt', "Are you sure you want to delete all this application's Azure resources? You can soft-delete certain resources like Azure KeyVaults to preserve their data, or permanently delete and purge them.");
-    const confirmTitle = localize('azure-dev.commands.cli.down.confirm-ack', "Delete resources");
 
     const softDelete: MessageItem = { title: localize("azure-dev.commands.cli.down.soft-delete", "Soft Delete") };
     const purgeDelete: MessageItem = { title: localize("azure-dev.commands.cli.down.purge-delete", "Delete and Purge") };
 
     // If cancel is chosen or the modal is closed, a `UserCancelledError` will automatically be thrown, so we don't need to check for it
-    const choice = await context.ui.showWarningMessage(confirmPrompt, { modal: true }, { title: confirmTitle }, purgeDelete, softDelete, DialogResponses.cancel);
+    const choice = await context.ui.showWarningMessage(confirmPrompt, { modal: true }, purgeDelete, softDelete, DialogResponses.cancel);
 
     context.telemetry.properties.purge = choice === purgeDelete ? 'true' : 'false';
 

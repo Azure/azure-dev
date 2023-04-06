e3f6b23cc5d0330941cc85adf76f981f16615697
hemarina <hemarina@microsoft.com>
2023-04-06 15:52:59 -0700

    add to get a string or null

--- templates/todo/api/js/src/app.ts
+++ templates/todo/api/js/src/app.ts
@@ -18,7 +18,7 @@ export const createApp = async (): Promise<Express> => {
     // Middleware
     app.use(express.json());
 
-    const apiUrl:string = process.env.REACT_APP_WEB_BASE_URL;
+    const apiUrl:string = process.env.REACT_APP_WEB_BASE_URL!;
     if (apiUrl != ""){
         app.use(cors({
             origin: ["https://portal.azure.com",

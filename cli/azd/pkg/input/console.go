605c6c8219a16791378106ed8381dbbdc8c78e2c
Victor Vazquez <vhvb1989@gmail.com>
2023-05-01 21:00:07 +0000

    remove non-required indentation reset (#2016)

--- cli/azd/pkg/input/console.go
+++ cli/azd/pkg/input/console.go
@@ -251,7 +251,6 @@ func (c *AskerConsole) StopSpinner(ctx context.Context, lastMessage string, form
 
 	c.spinner.StopMessage(lastMessage)
 	_ = c.spinner.Stop()
-	c.currentIndent = ""
 }
 
 func (c *AskerConsole) IsSpinnerRunning(ctx context.Context) bool {

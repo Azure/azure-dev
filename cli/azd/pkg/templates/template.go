a7b768dc3a1a067654c453a6995b55d3f28ff806
Wei Lim <weilim@microsoft.com>
2023-05-04 17:02:09 -0700

    Add doc, update vscode

--- cli/azd/pkg/templates/template.go
+++ cli/azd/pkg/templates/template.go
@@ -13,12 +13,15 @@ type Template struct {
 	Path string `json:"path"`
 }
 
+// ContractedTemplate is the template contract that is used expose by the CLI.
+//
+// Use NewContract to create a ContractedTemplate from a Template.
 type ContractedTemplate struct {
 	Template
 
 	// RepositoryPath is always set to the same value as template.Path
-	// It is added for backwards compatibility with the old contract.
-	// This can be removed once the old contract is no longer used.
+	// It is added for backwards compatibility with old versions of azd where repositoryPath was surfaced in the contract.
+	// This can be removed once backwards compatibility is no longer required.
 	RepositoryPath string `json:"repositoryPath"`
 }
 

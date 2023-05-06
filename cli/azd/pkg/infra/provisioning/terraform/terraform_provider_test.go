a67084b8c586f6720f76911a44861d38cd515765
Wei Lim <weilim@microsoft.com>
2023-05-05 19:23:08 -0700

    Fix terraform tests

--- cli/azd/pkg/infra/provisioning/terraform/terraform_provider_test.go
+++ cli/azd/pkg/infra/provisioning/terraform/terraform_provider_test.go
@@ -201,6 +201,7 @@ func TestTerraformState(t *testing.T) {
 func createTerraformProvider(mockContext *mocks.MockContext) *TerraformProvider {
 	projectDir := "../../../../test/functional/testdata/samples/resourcegroupterraform"
 	options := Options{
+		Path:   "infra",
 		Module: "main",
 	}
 

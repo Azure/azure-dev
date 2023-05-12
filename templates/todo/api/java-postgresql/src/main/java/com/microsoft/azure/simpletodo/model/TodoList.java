a640197fe143304b4e49a9ddf7682f6b49d90237
hemarina <hemarina@microsoft.com>
2023-05-12 13:19:47 -0700

    breaking change on springboot

--- templates/todo/api/java-postgresql/src/main/java/com/microsoft/azure/simpletodo/model/TodoList.java
+++ templates/todo/api/java-postgresql/src/main/java/com/microsoft/azure/simpletodo/model/TodoList.java
@@ -3,12 +3,12 @@ package com.microsoft.azure.simpletodo.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Objects;
-import javax.annotation.Generated;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import org.hibernate.annotations.GenericGenerator;
 
 /**

b3156210ef02891d47b1323d25609ea3fa54d5a8
hemarina <hemarina@microsoft.com>
2023-05-15 16:58:49 -0700

    separate pr

--- templates/todo/api/python/todo/app.py
+++ templates/todo/api/python/todo/app.py
@@ -64,10 +64,11 @@ async def startup_event():
         exporter = AzureMonitorTraceExporter.from_connection_string(
             settings.APPLICATIONINSIGHTS_CONNECTION_STRING
         )
+        #console exporter
         tracer = TracerProvider(
             resource=Resource({SERVICE_NAME: settings.APPLICATIONINSIGHTS_ROLENAME})
         )
-        tracer.add_span_processor(BatchSpanProcessor(exporter))
+        tracer.add_span_processor(BatchSpanProcessor(exporter)) #add it here check BatchSpanProcessor
 
         FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer)
 

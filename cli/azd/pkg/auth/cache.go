31b80c07ac940568275dc2c16b00d7d1678c30f6
Wei Lim <weilim@microsoft.com>
2023-05-18 11:29:32 -0700

    Add handling for non-existing cache file (#2259)
    * Add handling for non-existing file
    
    * Do not set value if key isn't found
    
    * Fix comments

--- cli/azd/pkg/auth/cache.go
+++ cli/azd/pkg/auth/cache.go
@@ -5,23 +5,25 @@ package auth
 
 import (
 	"context"
+	"errors"
 
 	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/cache"
 )
 
-// msalCacheAdapter adapts our interface to the one expected by cache.ExportReplace. Since that
-// interface is not error returning, any errors during Export or Replace are simply logged for
-// debugging purposes.
+// msalCacheAdapter adapts our interface to the one expected by cache.ExportReplace.
 type msalCacheAdapter struct {
 	cache Cache
 }
 
 func (a *msalCacheAdapter) Replace(ctx context.Context, cache cache.Unmarshaler, cacheHints cache.ReplaceHints) error {
 	val, err := a.cache.Read(cacheHints.PartitionKey)
-	if err != nil {
+	if errors.Is(err, errCacheKeyNotFound) {
+		return nil
+	} else if err != nil {
 		return err
 	}
 
+	// Replace the msal cache contents with the new value retrieved.
 	if err := cache.Unmarshal(val); err != nil {
 		return err
 	}
@@ -41,3 +43,5 @@ type Cache interface {
 	Read(key string) ([]byte, error)
 	Set(key string, value []byte) error
 }
+
+var errCacheKeyNotFound = errors.New("key not found")

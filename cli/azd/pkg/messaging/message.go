076ddd2be3c4d3f82e5de66e7f9eb777380a4624
Wallace Breza <wallace.breza@microsoft.com>
2023-06-05 12:37:01 -0700

    Updates inline docs

--- cli/azd/pkg/messaging/message.go
+++ cli/azd/pkg/messaging/message.go
@@ -4,12 +4,16 @@ import (
 	"time"
 )
 
+// MessageKind represents the kind of a message.
+// This is used to filter messages.
 type MessageKind string
 
 const (
+	// SimpleMessage is a simple message.
 	SimpleMessage MessageKind = "Simple"
 )
 
+// Message represents a message sent to a topic.
 type Message struct {
 	Timestamp time.Time
 	Type      MessageKind
@@ -17,6 +21,7 @@ type Message struct {
 	Tags      map[string]any
 }
 
+// NewMessage creates a new message with the specified kind and value.
 func NewMessage[T any](kind MessageKind, value T) *Message {
 	return &Message{
 		Type:      kind,

6f751db27ff29e47500d04509fcb47ec5fff9a42
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2023-06-14 09:22:12 -0400

    Update the branch

--- ext/vscode/src/language/languageFeatures.ts
+++ ext/vscode/src/language/languageFeatures.ts
@@ -3,22 +3,12 @@
 
 import * as vscode from 'vscode';
 import ext from '../ext';
-import { AzureYamlCompletionItemProvider } from './AzureYamlCompletionItemProvider';
 import { AzureYamlDiagnosticProvider } from './AzureYamlDiagnosticProvider';
-import { AzureYamlProjectRenameProvider } from './AzureYamlProjectRenameProvider';
 
 export const AzureYamlSelector: vscode.DocumentSelector = { language: 'yaml', scheme: 'file', pattern: '**/azure.{yml,yaml}' };
 
 export function registerLanguageFeatures(): void {
     ext.context.subscriptions.push(
-        vscode.languages.registerCompletionItemProvider(AzureYamlSelector, new AzureYamlCompletionItemProvider(), '/'),
-    );
-
-    ext.context.subscriptions.push(
         new AzureYamlDiagnosticProvider(AzureYamlSelector)
-    );
-
-    ext.context.subscriptions.push(
-        new AzureYamlProjectRenameProvider()
     );
 }

d605760c63e5848b7bf58cf68f16b92c7d40dff1
Wallace Breza <wallace.breza@microsoft.com>
2023-06-14 11:55:58 -0700

    Fixes error when unmarshalling AKS ingress resources with TLS hosts (#2405)
    Fixes #2390

--- cli/azd/pkg/tools/kubectl/models_test.go
+++ cli/azd/pkg/tools/kubectl/models_test.go
@@ -3,9 +3,11 @@ package kubectl
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
 )
 
 func Test_Port_TargetPort_Unmarshalling(t *testing.T) {
@@ -43,4 +45,30 @@ func Test_Port_TargetPort_Unmarshalling(t *testing.T) {
 			require.Equal(t, "http", port.Protocol)
 		})
 	}
+}
+
+func Test_Ingress_UnMarshalling(t *testing.T) {
+	t.Run("json", func(t *testing.T) {
+		var ingressResources List[Ingress]
+		ingressBytes, err := os.ReadFile("../../../test/testdata/k8s/ingress.json")
+		require.NoError(t, err)
+
+		err = json.Unmarshal(ingressBytes, &ingressResources)
+		require.NoError(t, err)
+
+		require.Equal(t, "myapp.centralus.cloudapp.azure.com", *ingressResources.Items[0].Spec.Rules[0].Host)
+		require.Equal(t, "myapp.centralus.cloudapp.azure.com", ingressResources.Items[0].Spec.Tls[0].Hosts[0])
+	})
+
+	t.Run("yaml", func(t *testing.T) {
+		var ingressResources List[Ingress]
+		ingressBytes, err := os.ReadFile("../../../test/testdata/k8s/ingress.yaml")
+		require.NoError(t, err)
+
+		err = yaml.Unmarshal(ingressBytes, &ingressResources)
+		require.NoError(t, err)
+
+		require.Equal(t, "myapp.centralus.cloudapp.azure.com", *ingressResources.Items[0].Spec.Rules[0].Host)
+		require.Equal(t, "myapp.centralus.cloudapp.azure.com", ingressResources.Items[0].Spec.Tls[0].Hosts[0])
+	})
 }

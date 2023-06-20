e96cb51976ca32093b49f4f159f514901844da08
Wei Lim <weilim@microsoft.com>
2023-06-20 16:53:22 -0700

    Add copyright notices

--- cli/azd/test/recording/recording.go
+++ cli/azd/test/recording/recording.go
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 // recording implements a proxy server that records and plays back HTTP interactions.
 // The implementation largely reuses [go-vcr], but adds support for:
 //   - Saving and loading of variables in the recording session

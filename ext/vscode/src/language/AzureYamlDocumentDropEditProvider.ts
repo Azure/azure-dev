5a4895376415de034e9cfb837ab932d5f86782bc
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2023-06-21 08:50:59 -0400

    Git is making me sad

---
+++ ext/vscode/src/language/AzureYamlDocumentDropEditProvider.ts
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { AzExtFsExtra } from '@microsoft/vscode-azext-utils';
+import * as path from 'path';
+import * as vscode from 'vscode';
+import { getProjectRelativePath } from './azureYamlUtils';
+
+export class AzureYamlDocumentDropEditProvider implements vscode.DocumentDropEditProvider {
+    public async provideDocumentDropEdits(document: vscode.TextDocument, position: vscode.Position, dataTransfer: vscode.DataTransfer, token: vscode.CancellationToken): Promise<vscode.DocumentDropEdit | undefined> {
+        const maybeFolder = dataTransfer.get('text/uri-list')?.value;
+        const maybeFolderUri = vscode.Uri.parse(maybeFolder);
+
+
+        if (await AzExtFsExtra.pathExists(maybeFolderUri) && await AzExtFsExtra.isDirectory(maybeFolderUri)) {
+            const basename = path.basename(maybeFolderUri.fsPath);
+            const newRelativePath = getProjectRelativePath(document.uri, maybeFolderUri);
+
+            const snippet = new vscode.SnippetString('\t')
+                .appendPlaceholder(basename).appendText(':\n')
+                .appendText(`\t\tproject: ${newRelativePath}\n`)
+                .appendText('\t\tlanguage: ')
+                .appendChoice(['dotnet', 'csharp', 'fsharp', 'py', 'python', 'js', 'ts', 'java'])
+                .appendText('\n')
+                .appendText('\t\thost: ')
+                .appendChoice(['appservice', 'containerapp', 'function', 'staticwebapp', 'aks'])
+                .appendText('\n');
+            return new vscode.DocumentDropEdit(snippet);
+        }
+
+        return undefined;
+    }
+}

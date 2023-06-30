4979a831f314e94015c50240a8deeb0c54e51857
hemarina <hemarina@microsoft.com>
2023-06-30 15:00:27 -0700

    add xdg-utils/version due to local test/feedback

--- ext/devcontainer/src/azd/install.sh
+++ ext/devcontainer/src/azd/install.sh
@@ -18,8 +18,8 @@ check_packages() {
 
 echo "(*) Ensuring dependencies are installed"
 
-check_packages apt-transport-https curl ca-certificates
-check-packages $(apt-cache search '^libicu[0-9]+$' | cut -d' ' -f1)
+check_packages apt-transport-https curl ca-certificates xdg-utils
+check_packages $(apt-cache search '^libicu[0-9]+$' | cut -d' ' -f1)
 
 echo "(*) Installing Azure Developer CLI"
 

03a086ceb31be338c746d12f10de2f179681c471
Victor Vazquez <vhvb1989@gmail.com>
2023-07-10 21:02:31 +0000

    remove template java-postgresql

--- templates/todo/api/java-postgresql/src/main/java/com/microsoft/azure/simpletodo/repository/TodoListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.microsoft.azure.simpletodo.repository;
-
-import com.microsoft.azure.simpletodo.model.TodoList;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface TodoListRepository extends PagingAndSortingRepository<TodoList, String>, CrudRepository<TodoList, String> {}

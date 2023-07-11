54656744db9145c5ddd20665e8ab494b4ec9e66b
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2023-07-11 09:57:04 -0400

    Add telemetry

--- ext/vscode/src/language/AzureYamlDocumentDropEditProvider.ts
+++ ext/vscode/src/language/AzureYamlDocumentDropEditProvider.ts
@@ -1,34 +1,40 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { AzExtFsExtra } from '@microsoft/vscode-azext-utils';
+import { AzExtFsExtra, IActionContext, callWithTelemetryAndErrorHandling } from '@microsoft/vscode-azext-utils';
 import * as path from 'path';
 import * as vscode from 'vscode';
 import { getProjectRelativePath } from './azureYamlUtils';
+import { TelemetryId } from '../telemetry/telemetryId';
 
 export class AzureYamlDocumentDropEditProvider implements vscode.DocumentDropEditProvider {
-    public async provideDocumentDropEdits(document: vscode.TextDocument, position: vscode.Position, dataTransfer: vscode.DataTransfer, token: vscode.CancellationToken): Promise<vscode.DocumentDropEdit | undefined> {
-        const maybeFolder = dataTransfer.get('text/uri-list')?.value;
-        const maybeFolderUri = vscode.Uri.parse(maybeFolder);
+    public provideDocumentDropEdits(document: vscode.TextDocument, position: vscode.Position, dataTransfer: vscode.DataTransfer, token: vscode.CancellationToken): Promise<vscode.DocumentDropEdit | undefined> {
+        return callWithTelemetryAndErrorHandling(TelemetryId.AzureYamlProvideDocumentDropEdits, async (context: IActionContext) => {
+            const maybeFolder = dataTransfer.get('text/uri-list')?.value;
+            const maybeFolderUri = vscode.Uri.parse(maybeFolder);
 
-        if (await AzExtFsExtra.pathExists(maybeFolderUri) && await AzExtFsExtra.isDirectory(maybeFolderUri)) {
-            const basename = path.basename(maybeFolderUri.fsPath);
-            const newRelativePath = getProjectRelativePath(document.uri, maybeFolderUri);
+            if (await AzExtFsExtra.pathExists(maybeFolderUri) && await AzExtFsExtra.isDirectory(maybeFolderUri)) {
+                const basename = path.basename(maybeFolderUri.fsPath);
+                const newRelativePath = getProjectRelativePath(document.uri, maybeFolderUri);
 
-            const initialWhitespace = position.character === 0 ? '\n\t' : '\n';
+                const initialWhitespace = position.character === 0 ? '\n\t' : '\n';
 
-            const snippet = new vscode.SnippetString(initialWhitespace)
-                .appendPlaceholder(basename).appendText(':\n')
-                .appendText(`\t\tproject: ${newRelativePath}\n`)
-                .appendText('\t\tlanguage: ')
-                .appendChoice(['dotnet', 'csharp', 'fsharp', 'py', 'python', 'js', 'ts', 'java'])
-                .appendText('\n')
-                .appendText('\t\thost: ')
-                .appendChoice(['appservice', 'containerapp', 'function', 'staticwebapp', 'aks'])
-                .appendText('\n');
-            return new vscode.DocumentDropEdit(snippet);
-        }
+                const snippet = new vscode.SnippetString(initialWhitespace)
+                    .appendPlaceholder(basename).appendText(':\n')
+                    .appendText(`\t\tproject: ${newRelativePath}\n`)
+                    .appendText('\t\tlanguage: ')
+                    .appendChoice(['dotnet', 'csharp', 'fsharp', 'py', 'python', 'js', 'ts', 'java'])
+                    .appendText('\n')
+                    .appendText('\t\thost: ')
+                    .appendChoice(['appservice', 'containerapp', 'function', 'staticwebapp', 'aks'])
+                    .appendText('\n');
 
-        return undefined;
+                context.telemetry.properties.editProvided = 'true';
+                return new vscode.DocumentDropEdit(snippet);
+            }
+
+            context.telemetry.properties.editProvided = 'false';
+            return undefined;
+        });
     }
 }

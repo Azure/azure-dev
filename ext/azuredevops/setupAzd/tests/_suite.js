a0a97bcef15a6f6d3712cae546b196fd88ec281c
Victor Vazquez <vhvb1989@gmail.com>
2023-08-09 19:20:21 +0000

    wip

---
+++ ext/azuredevops/setupAzd/tests/_suite.js
@@ -0,0 +1,48 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const path = __importStar(require("path"));
+const assert = __importStar(require("assert"));
+const ttm = __importStar(require("azure-pipelines-task-lib/mock-test"));
+describe('setup azd tests', function () {
+    before(function () { });
+    after(() => { });
+    it('should succeed with empty version', function (done) {
+        let tp = path.join(__dirname, 'success.js');
+        let tr = new ttm.MockTestRunner(tp);
+        tr.run();
+        console.log(tr.succeeded);
+        console.log(tr.stdout);
+        assert.equal(tr.succeeded, true, 'should have succeeded');
+        assert.equal(tr.warningIssues.length, 0, "should have no warnings");
+        assert.equal(tr.errorIssues.length, 0, "should have no errors");
+        console.log(tr.stdout);
+        //assert.equal(tr.stdout.indexOf('using version') >= 0, true, "should display version");
+        done();
+    });
+    // it('it should fail if tool returns 1', function(done: Mocha.Done) {
+    //     // Add failure test here
+    // });    
+});

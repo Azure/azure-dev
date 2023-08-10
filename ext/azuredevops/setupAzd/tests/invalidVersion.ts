1c4ae8f9d29edc020253c4d18ea009e3e4b4c03f
Victor Vazquez <vhvb1989@gmail.com>
2023-08-10 00:07:32 +0000

    lint for ext

--- ext/azuredevops/setupAzd/tests/invalidVersion.ts
+++ ext/azuredevops/setupAzd/tests/invalidVersion.ts
@@ -1,9 +1,9 @@
 import ma = require('azure-pipelines-task-lib/mock-answer');
-import tmrm = require('azure-pipelines-task-lib/mock-run');
+import taskMock = require('azure-pipelines-task-lib/mock-run');
 import path = require('path');
 
 let taskPath = path.join(__dirname, '..', 'index.js');
-let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+let tmr: taskMock.TaskMockRunner = new taskMock.TaskMockRunner(taskPath);
 
 tmr.setAnswers({
     cwd: {'cwd': 'path'},

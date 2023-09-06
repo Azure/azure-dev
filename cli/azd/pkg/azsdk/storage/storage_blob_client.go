af31d27da09a10c9a307b18c96a886f3a09a9f21
Wallace Breza <wallace.breza@microsoft.com>
2023-09-06 16:09:45 -0700

    Adds recording/playback tests for blob client

--- cli/azd/pkg/azsdk/storage/storage_blob_client.go
+++ cli/azd/pkg/azsdk/storage/storage_blob_client.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
-	"github.com/azure/azure-dev/cli/azd/internal"
 	"github.com/azure/azure-dev/cli/azd/pkg/auth"
 	"github.com/azure/azure-dev/cli/azd/pkg/azsdk"
 	"github.com/azure/azure-dev/cli/azd/pkg/httputil"
@@ -47,24 +46,18 @@ type BlobClient interface {
 
 // NewBlobClient creates a new BlobClient instance to manage blobs within a container.
 func NewBlobClient(
-	config AccountConfig,
-	authManager *auth.Manager,
-	httpClient httputil.HttpClient,
+	config *AccountConfig,
+	client *azblob.Client,
 ) BlobClient {
 	return &blobClient{
-		config:      config,
-		authManager: authManager,
-		httpClient:  httpClient,
-		userAgent:   internal.UserAgent(),
+		config: config,
+		client: client,
 	}
 }
 
 type blobClient struct {
-	config      AccountConfig
-	authManager *auth.Manager
-	httpClient  httputil.HttpClient
-	userAgent   string
-	client      *azblob.Client
+	config *AccountConfig
+	client *azblob.Client
 }
 
 // Blob represents a blob within a storage account container.
@@ -77,16 +70,11 @@ type Blob struct {
 
 // Items returns a list of blobs in the configured storage account container.
 func (bc *blobClient) Items(ctx context.Context) ([]*Blob, error) {
-	client, err := bc.createClient(ctx)
-	if err != nil {
-		return nil, err
-	}
-
 	blobs := []*Blob{}
 
-	pager := client.NewListBlobsFlatPager(bc.config.ContainerName, nil)
+	pager := bc.client.NewListBlobsFlatPager(bc.config.ContainerName, nil)
 	for pager.More() {
-		page, err := pager.NextPage(context.Background())
+		page, err := pager.NextPage(ctx)
 		if err != nil {
 			err = fmt.Errorf("failed to get next page of blobs, %w", err)
 			var responseErr *azcore.ResponseError
@@ -111,16 +99,11 @@ func (bc *blobClient) Items(ctx context.Context) ([]*Blob, error) {
 
 // Download downloads a blob from the configured storage account container.
 func (bc *blobClient) Download(ctx context.Context, blobPath string) (io.ReadCloser, error) {
-	client, err := bc.createClient(ctx)
-	if err != nil {
-		return nil, err
-	}
-
 	if err := bc.ensureContainerExists(ctx); err != nil {
 		return nil, err
 	}
 
-	resp, err := client.DownloadStream(ctx, bc.config.ContainerName, blobPath, nil)
+	resp, err := bc.client.DownloadStream(ctx, bc.config.ContainerName, blobPath, nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed to download blob '%s', %w", blobPath, err)
 	}
@@ -130,16 +113,11 @@ func (bc *blobClient) Download(ctx context.Context, blobPath string) (io.ReadClo
 
 // Upload uploads a blob to the configured storage account container.
 func (bc *blobClient) Upload(ctx context.Context, blobPath string, reader io.Reader) error {
-	client, err := bc.createClient(ctx)
-	if err != nil {
-		return err
-	}
-
 	if err := bc.ensureContainerExists(ctx); err != nil {
 		return err
 	}
 
-	_, err = client.UploadStream(ctx, bc.config.ContainerName, blobPath, reader, nil)
+	_, err := bc.client.UploadStream(ctx, bc.config.ContainerName, blobPath, reader, nil)
 	if err != nil {
 		return fmt.Errorf("failed to upload blob '%s', %w", blobPath, err)
 	}
@@ -149,16 +127,11 @@ func (bc *blobClient) Upload(ctx context.Context, blobPath string, reader io.Rea
 
 // Delete deletes a blob from the configured storage account container.
 func (bc *blobClient) Delete(ctx context.Context, blobPath string) error {
-	client, err := bc.createClient(ctx)
-	if err != nil {
-		return err
-	}
-
 	if err := bc.ensureContainerExists(ctx); err != nil {
 		return err
 	}
 
-	_, err = client.DeleteBlob(ctx, bc.config.ContainerName, blobPath, nil)
+	_, err := bc.client.DeleteBlob(ctx, bc.config.ContainerName, blobPath, nil)
 	if err != nil {
 		return fmt.Errorf("failed to delete blob '%s', %w", blobPath, err)
 	}
@@ -169,14 +142,9 @@ func (bc *blobClient) Delete(ctx context.Context, blobPath string) error {
 // Check if the specified container exists
 // If it doesn't already exist then create it
 func (bc *blobClient) ensureContainerExists(ctx context.Context) error {
-	client, err := bc.createClient(ctx)
-	if err != nil {
-		return err
-	}
-
 	exists := false
 
-	pager := client.NewListContainersPager(nil)
+	pager := bc.client.NewListContainersPager(nil)
 	for pager.More() {
 		if exists {
 			break
@@ -196,7 +164,7 @@ func (bc *blobClient) ensureContainerExists(ctx context.Context) error {
 	}
 
 	if !exists {
-		_, err = client.CreateContainer(ctx, bc.config.ContainerName, nil)
+		_, err := bc.client.CreateContainer(ctx, bc.config.ContainerName, nil)
 		if err != nil {
 			return fmt.Errorf("failed to create container '%s', %w", bc.config.ContainerName, err)
 		}
@@ -206,35 +174,35 @@ func (bc *blobClient) ensureContainerExists(ctx context.Context) error {
 }
 
 // createClient creates a new blob client and caches it for future use
-func (bc *blobClient) createClient(ctx context.Context) (*azblob.Client, error) {
-	if bc.client != nil {
-		return bc.client, nil
-	}
-
-	credential, err := bc.authManager.CredentialForCurrentUser(ctx, nil)
+func NewBlobSdkClient(
+	ctx context.Context,
+	authManager *auth.Manager,
+	accountConfig *AccountConfig,
+	httpClient httputil.HttpClient,
+	userAgent httputil.UserAgent,
+) (*azblob.Client, error) {
+	credential, err := authManager.CredentialForCurrentUser(ctx, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	coreOptions := azsdk.
-		DefaultClientOptionsBuilder(ctx, bc.httpClient, bc.userAgent).
+		DefaultClientOptionsBuilder(ctx, httpClient, string(userAgent)).
 		BuildCoreClientOptions()
 
 	blobOptions := &azblob.ClientOptions{
 		ClientOptions: *coreOptions,
 	}
 
-	if bc.config.Endpoint == "" {
-		bc.config.Endpoint = DefaultBlobEndpoint
+	if accountConfig.Endpoint == "" {
+		accountConfig.Endpoint = DefaultBlobEndpoint
 	}
 
-	serviceUrl := fmt.Sprintf("https://%s.%s", bc.config.AccountName, bc.config.Endpoint)
+	serviceUrl := fmt.Sprintf("https://%s.%s", accountConfig.AccountName, accountConfig.Endpoint)
 	client, err := azblob.NewClient(serviceUrl, credential, blobOptions)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create blob client, %w", err)
 	}
 
-	bc.client = client
-
-	return bc.client, nil
+	return client, nil
 }

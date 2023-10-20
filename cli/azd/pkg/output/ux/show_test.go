71e5934a68e55f26755ee4b1c95c56f9c3939728
Victor Vazquez <vhvb1989@gmail.com>
2023-10-20 23:48:52 +0000

    tests

--- cli/azd/pkg/output/ux/show_test.go
+++ cli/azd/pkg/output/ux/show_test.go
@@ -34,3 +34,73 @@ func TestShow(t *testing.T) {
 	output := pp.ToString("")
 	snapshot.SnapshotT(t, output)
 }
+
+func TestShowNoEnv(t *testing.T) {
+	pp := &Show{
+		AppName: "Foo",
+		Services: []*ShowService{
+			{
+				Name:      "xx",
+				IngresUrl: "bar",
+			},
+		},
+		Environments:    []*ShowEnvironment{},
+		AzurePortalLink: "foo.com",
+	}
+
+	output := pp.ToString("")
+	snapshot.SnapshotT(t, output)
+}
+
+func TestShowNoPortalLink(t *testing.T) {
+	pp := &Show{
+		AppName: "Foo",
+		Services: []*ShowService{
+			{
+				Name:      "xx",
+				IngresUrl: "bar",
+			},
+		},
+		Environments:    []*ShowEnvironment{},
+		AzurePortalLink: "",
+	}
+
+	output := pp.ToString("")
+	snapshot.SnapshotT(t, output)
+}
+
+func TestShowEmpty(t *testing.T) {
+	pp := &Show{
+		AppName:         "Foo",
+		Services:        []*ShowService{},
+		Environments:    []*ShowEnvironment{},
+		AzurePortalLink: "",
+	}
+
+	output := pp.ToString("")
+	snapshot.SnapshotT(t, output)
+}
+
+func TestShowRemoteAndCurrent(t *testing.T) {
+	pp := &Show{
+		AppName:  "Foo",
+		Services: []*ShowService{},
+		Environments: []*ShowEnvironment{
+			{
+				Name:      "foo",
+				IsCurrent: true,
+			},
+			{
+				Name: "bar",
+			},
+			{
+				Name:     "vh",
+				IsRemote: true,
+			},
+		},
+		AzurePortalLink: "",
+	}
+
+	output := pp.ToString("")
+	snapshot.SnapshotT(t, output)
+}

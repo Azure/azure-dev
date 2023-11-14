6cb723e44eb0697d4d8d09f7694169f5410c7d6f
Wei Lim <weilim@microsoft.com>
2023-11-13 17:04:25 -0800

    Add usage for service targets (#2967)
    Add usage telemetry for service targets resolved from hosts.

--- cli/azd/internal/tracing/fields/fields.go
+++ cli/azd/internal/tracing/fields/fields.go
@@ -83,6 +83,8 @@ const (
 	ProjectNameKey = attribute.Key("project.name")
 	// The collection of hashed service hosts in the project.
 	ProjectServiceHostsKey = attribute.Key("project.service.hosts")
+	// The collection of service targets (resolved service hosts) in the project.
+	ProjectServiceTargetsKey = attribute.Key("project.service.targets")
 	// The collection of hashed service languages in the project.
 	ProjectServiceLanguagesKey = attribute.Key("project.service.languages")
 	// The service language being executed.

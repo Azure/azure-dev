f385e0bc935d146d10d5bdf1bfcc6f4a409fcd82
Wei Lim <weilim@microsoft.com>
2023-12-12 16:20:25 -0800

    buildpacks: Update dotnet base image to bullseye (#3112)
    Upgrade dotnet base images to be based on bullseye.
    
    Also, bump to 20131107.2 which is the current stable version and supports .NET 8.
    
    Fixes https://github.com/Azure/azure-dev/issues/3108

--- cli/azd/pkg/project/framework_service_docker.go
+++ cli/azd/pkg/project/framework_service_docker.go
@@ -289,8 +289,7 @@ func (p *dockerProject) Package(
 }
 
 // Default builder image to produce container images from source
-const DefaultBuilderImage = "mcr.microsoft.com/oryx/builder:debian-bullseye-20231004.1"
-const DefaultDotNetBuilderImage = "mcr.microsoft.com/oryx/builder:debian-buster-20231004.1"
+const DefaultBuilderImage = "mcr.microsoft.com/oryx/builder:debian-bullseye-20231107.2"
 
 func (p *dockerProject) packBuild(
 	ctx context.Context,
@@ -302,9 +301,6 @@ func (p *dockerProject) packBuild(
 		return nil, err
 	}
 	builder := DefaultBuilderImage
-	if svc.Language == ServiceLanguageDotNet {
-		builder = DefaultDotNetBuilderImage
-	}
 
 	environ := []string{}
 	userDefinedImage := false

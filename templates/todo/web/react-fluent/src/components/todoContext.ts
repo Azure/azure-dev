d3fb37d8eda0c89b861e7ccb9ff1a38b37abb0cd
Victor Vazquez <vhvb1989@gmail.com>
2024-01-04 20:13:53 +0000

    hook

--- templates/todo/web/react-fluent/src/components/todoContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-import { AppContext, getDefaultState } from "../models/applicationState";
-
-const initialState = getDefaultState();
-const dispatch = () => { return };
-
-export const TodoContext = createContext<AppContext>({ state: initialState, dispatch: dispatch });
--- templates/todo/web/react-fluent/src/services/itemService.ts
+++
@@ -1,8 +0,0 @@
-import { RestService } from './restService';
-import { TodoItem } from '../models';
-
-export class ItemService extends RestService<TodoItem> {
-    public constructor(baseUrl: string, baseRoute: string) {
-        super(baseUrl, baseRoute);
-    }
-}

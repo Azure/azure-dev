b5ad0dd8000406d16971e1d357d672c1c669774b
Charles Lowell <10964656+chlowell@users.noreply.github.com>
2024-01-19 16:27:00 -0800

    tests

---
+++ cli/azd/pkg/oneauth/oneauth_windows_test.go
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+//go:build oneauth && windows
+
+package oneauth
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestStart(t *testing.T) {
+	require.NoError(t, start("clientID", true))
+}
+
+func TestSupported(t *testing.T) {
+	require.True(t, Supported)
+}

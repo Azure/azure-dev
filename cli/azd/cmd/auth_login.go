ba850c11798446e32099675b7d7de3db69a01beb
Daniel Jurek <djurek@microsoft.com>
2024-01-29 14:10:57 -0800

    Include this rename of LoginScopes

--- cli/azd/cmd/auth_login.go
+++ cli/azd/cmd/auth_login.go
@@ -245,7 +245,7 @@ const cLoginSuccessMessage = "Logged in to Azure."
 
 func (la *loginAction) Run(ctx context.Context) (*actions.ActionResult, error) {
 	if len(la.flags.scopes) == 0 {
-		la.flags.scopes = la.authManager.GetLoginScopes()
+		la.flags.scopes = la.authManager.LoginScopes()
 	}
 
 	if la.annotations[loginCmdParentAnnotation] == "" {

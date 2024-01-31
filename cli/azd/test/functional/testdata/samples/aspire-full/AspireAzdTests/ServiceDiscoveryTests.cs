bbb51b449f6149495abe9afb20426e973c7e7410
Wei Lim <weilim@microsoft.com>
2024-01-31 10:18:37 -0800

    add sample

---
+++ cli/azd/test/functional/testdata/samples/aspire-full/AspireAzdTests/ServiceDiscoveryTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Playwright;
+using Should;
+using System.Text.RegularExpressions;
+
+namespace AspireAzdTests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class ServiceDiscoveryTests : TestBase
+{
+
+    [Test]
+    public async Task ServiceToServiceWorks()
+    {
+        await GetHomePage();
+
+        // Click the get started link.
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Weather" })
+            .ClickAsync();
+
+        // Expects the URL to contain intro.
+        await Expect(Page).ToHaveURLAsync(new Regex(".*weather"));
+
+        // wait for the weather forecast to load
+        await Task.Delay(2000);
+
+        // make sure we have data
+        (await Page.EvalOnSelectorAsync<int>("//table", "tbl => tbl.rows.length"))
+            .ShouldBeGreaterThan(3);
+    }
+}

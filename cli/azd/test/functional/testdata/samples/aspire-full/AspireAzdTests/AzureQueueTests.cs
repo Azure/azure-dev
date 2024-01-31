201dfc17342d8e398a6cdc937ce47240093223d4
Wei Lim <weilim@microsoft.com>
2024-01-31 14:05:18 -0800

    fix  queue tests

--- cli/azd/test/functional/testdata/samples/aspire-full/AspireAzdTests/AzureQueueTests.cs
+++ cli/azd/test/functional/testdata/samples/aspire-full/AspireAzdTests/AzureQueueTests.cs
@@ -23,6 +23,29 @@ public  class AzureQueueTests : TestBase
         // wait for the data to load
         await Task.Delay(2000);
 
+        // expect the page to have a button
+        var button = Page.GetByRole(AriaRole.Button, new() { Name = "Send" });
+
+        // click the button to send the message
+        await button.ClickAsync(new()
+        {
+            Timeout = 3000,
+            Force = true
+        });
+
+        // wait for data to roundtrip
+        await Task.Delay(2000);
+
+        // click the button to send the message
+        await button.ClickAsync(new()
+        {
+            Timeout = 3000,
+            Force = true
+        });
+
+        // wait for data to roundtrip
+        await Task.Delay(2000);
+
         // make sure we have data
         (await Page.EvalOnSelectorAsync<int>("//table", "tbl => tbl.rows.length"))
             .ShouldBeGreaterThan(1);

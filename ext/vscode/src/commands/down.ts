03de49bc1e7744e3a93b92de93881fadd31ac400
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2024-03-07 13:03:53 -0500

    Revert changes to `down` command

--- ext/vscode/src/commands/down.ts
+++ ext/vscode/src/commands/down.ts
@@ -9,13 +9,28 @@ import { executeAsTask } from '../utils/executeAsTask';
 import { TelemetryId } from '../telemetry/telemetryId';
 import { AzureDevCliApplication } from '../views/workspace/AzureDevCliApplication';
 import { isTreeViewModel, TreeViewModel } from '../utils/isTreeViewModel';
+import { MessageItem } from 'vscode';
+import { DialogResponses } from '@microsoft/vscode-azext-utils';
 
 export async function down(context: IActionContext, selectedItem?: vscode.Uri | TreeViewModel): Promise<void> {
     const selectedFile = isTreeViewModel(selectedItem) ? selectedItem.unwrap<AzureDevCliApplication>().context.configurationFile : selectedItem;
     const workingFolder = await getWorkingFolder(context, selectedFile);
 
+    const confirmPrompt = vscode.l10n.t("Are you sure you want to delete all this application's Azure resources? You can soft-delete certain resources like Azure KeyVaults to preserve their data, or permanently delete and purge them.");
+
+    const softDelete: MessageItem = { title: vscode.l10n.t("Soft Delete") };
+    const purgeDelete: MessageItem = { title: vscode.l10n.t("Delete and Purge") };
+
+    // If cancel is chosen or the modal is closed, a `UserCancelledError` will automatically be thrown, so we don't need to check for it
+    const choice = await context.ui.showWarningMessage(confirmPrompt, { modal: true }, softDelete, purgeDelete, DialogResponses.cancel);
+
+    context.telemetry.properties.purge = choice === purgeDelete ? 'true' : 'false';
+
     const azureCli = await createAzureDevCli(context);
-    const command = azureCli.commandBuilder.withArg('down');
+    const command = azureCli.commandBuilder.withArg('down').withArg('--force');
+    if (choice === purgeDelete) {
+        command.withArg('--purge');
+    }
 
     // Don't wait
     void executeAsTask(command.build(), getAzDevTerminalTitle(), {

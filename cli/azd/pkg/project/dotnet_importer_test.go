47c7db1c45784356d9d683ce4c46c6909def8857
Victor Vazquez <vhvb1989@gmail.com>
2024-03-19 03:49:24 +0000

    deterministic test

--- cli/azd/pkg/project/dotnet_importer_test.go
+++ cli/azd/pkg/project/dotnet_importer_test.go
@@ -1,6 +1,7 @@
 package project
 
 import (
+	"slices"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,6 +12,7 @@ func TestMapToStringSlice(t *testing.T) {
 	m1 := make(map[string]string)
 	expected1 := []string(nil)
 	result1 := mapToStringSlice(m1, ":")
+	slices.Sort(result1)
 	assert.Equal(t, expected1, result1)
 
 	// Test case 2: Map with values
@@ -21,6 +23,7 @@ func TestMapToStringSlice(t *testing.T) {
 	}
 	expected2 := []string{"key1:value1", "key2:value2", "key3:value3"}
 	result2 := mapToStringSlice(m2, ":")
+	slices.Sort(result2)
 	assert.Equal(t, expected2, result2)
 
 	// Test case 3: Map with empty values
@@ -31,5 +34,6 @@ func TestMapToStringSlice(t *testing.T) {
 	}
 	expected3 := []string{"key1", "key2", "key3"}
 	result3 := mapToStringSlice(m3, ":")
+	slices.Sort(result3)
 	assert.Equal(t, expected3, result3)
 }

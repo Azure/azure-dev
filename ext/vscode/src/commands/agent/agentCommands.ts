a5ed23255768c6654ba2a0cc5821bfc01745dc96
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2024-04-11 12:43:03 -0400

    Commit current progress

--- ext/vscode/src/commands/agent/agentCommands.ts
+++ ext/vscode/src/commands/agent/agentCommands.ts
@@ -2,10 +2,8 @@
 // Licensed under the MIT License.
 
 import * as vscode from 'vscode';
-import { AzExtUserInputWithInputQueue, AzureUserInputQueue, IAzureUserInput, callWithTelemetryAndErrorHandling, registerCommand, type IActionContext } from '@microsoft/vscode-azext-utils';
+import { registerCommand, type IActionContext } from '@microsoft/vscode-azext-utils';
 import { SimpleCommandConfig, SkillCommandConfig as SkillCommandConfigAgent, WizardCommandConfig } from 'vscode-azure-agent-api';
-import { init } from '../init';
-import { up } from '../up';
 import { azdSkillCommand } from './azdSkillCommand';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -15,8 +13,6 @@ type CommandConfig = SimpleCommandConfig | SkillCommandConfig | WizardCommandCon
 
 export function registerAgentCommands(): void {
     vscode.commands.registerCommand('azure-dev.commands.agent.getCommands', getAgentCommands);
-    vscode.commands.registerCommand('azure-dev.commands.agent.runWizardCommandWithoutExecution', runWizardCommandWithoutExecution);
-    vscode.commands.registerCommand('azure-dev.commands.agent.runWizardCommandWithInputs', runWizardCommandWithInputs);
 
     for (const command of agentCommands) {
         if ('handler' in command && typeof command.handler === 'function') {
@@ -29,57 +25,11 @@ function getAgentCommands(): Promise<CommandConfig[]> {
     return Promise.resolve(agentCommands);
 }
 
-async function runWizardCommandWithoutExecution(command: WizardCommandConfig, ui: IAzureUserInput): Promise<void> {
-    if (command.commandId === 'azure-dev.commands.cli.init') {
-        await callWithTelemetryAndErrorHandling('azure-dev.commands.cli.init.viaAgent', async (context) => {
-            return await init({ ...context, ui: ui, skipExecute: true });
-        });
-    } else if (command.commandId === 'azure-dev.commands.cli.up') {
-        await callWithTelemetryAndErrorHandling('azure-dev.commands.cli.up.viaAgent', async (context) => {
-            return await up({ ...context, ui: ui, skipExecute: true });
-        });
-    } else {
-        throw new Error('Unknown command: ' + command.commandId);
-    }
-}
-
-async function runWizardCommandWithInputs(command: WizardCommandConfig, inputsQueue: AzureUserInputQueue): Promise<void> {
-    if (command.commandId === 'azure-dev.commands.cli.init') {
-        await callWithTelemetryAndErrorHandling('azure-dev.commands.cli.init.viaAgentActual', async (context) => {
-            const azureUserInput = new AzExtUserInputWithInputQueue(context, inputsQueue);
-            return await init({ ...context, ui: azureUserInput });
-        });
-    } else if (command.commandId === 'azure-dev.commands.cli.up') {
-        await callWithTelemetryAndErrorHandling('azure-dev.commands.cli.up.viaAgentActual', async (context) => {
-            const azureUserInput = new AzExtUserInputWithInputQueue(context, inputsQueue);
-            return await up({ ...context, ui: azureUserInput });
-        });
-    } else {
-        throw new Error('Unknown command: ' + command.commandId);
-    }
-}
-
 const agentCommands: CommandConfig[] = [
-    {
-        type: 'wizard',
-        name: 'azdInit',
-        commandId: 'azure-dev.commands.cli.init',
-        displayName: 'Initialize with Azure Developer CLI',
-        intentDescription: 'This is best when users ask to set up or initialize their application for Azure.',
-        requiresAzureLogin: false,
-    } satisfies WizardCommandConfig,
-    {
-        type: 'wizard',
-        name: 'azdUp',
-        commandId: 'azure-dev.commands.cli.up',
-        displayName: 'Deploy to Azure with Azure Developer CLI',
-        intentDescription: 'This is best when users ask to deploy their application to Azure.',
-        requiresAzureLogin: true,
-    } satisfies WizardCommandConfig,
     {
         type: 'skill',
         name: 'azdSkill',
-        commandId: 'azure-dev.commands.cli.skill',
+        commandId: 'azure-dev.commands.agent.skill',
         displayName: 'Azure Developer CLI Skill',
         intentDescription: 'This is best when users ask to do something magical.',
         requiresAzureLogin: false,
---
+++ ext/vscode/src/commands/agent/azdSkillCommand.ts
@@ -0,0 +1,48 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import * as vscode from 'vscode';
+import { IActionContext } from '@microsoft/vscode-azext-utils';
+import { SkillCommandArgs, SkillCommandResult } from 'vscode-azure-agent-api';
+import { getAzdLoginStatus } from '../../utils/azureDevCli';
+
+export async function azdSkillCommand(context: IActionContext, args: SkillCommandArgs): Promise<SkillCommandResult> {
+    const responseStream = args.agentRequest.responseStream;
+
+    responseStream.progress(vscode.l10n.t('Determining what to do...'));
+
+    const loginStatus = await getAzdLoginStatus();
+
+    const azdInstalled = loginStatus !== undefined;
+    const azdLoggedIn = azdInstalled && loginStatus.status === 'success';
+    const workspaceInitialized = await isWorkspaceInitialized(context);
+
+    if (!azdInstalled) {
+        responseStream.markdown(vscode.l10n.t('- First things first, it looks like the Azure Developer CLI is not installed. Let\'s get that taken care of.'));
+        responseStream.button({ title: vscode.l10n.t('Install Azure Developer CLI'), command: 'azure-dev.commands.cli.install' });
+    }
+
+    if (!azdLoggedIn) {
+        responseStream.markdown(vscode.l10n.t('- You\'ll need to be logged in with the Azure Developer CLI. Click below to sign in.'));
+        responseStream.button({ title: vscode.l10n.t('Sign in with Azure Developer CLI'), command: 'azure-dev.commands.cli.login' });
+    }
+
+    if (!workspaceInitialized) {
+        responseStream.markdown(vscode.l10n.t('- It looks like the workspace is not set up for use with the Azure Development CLI.'));
+        responseStream.button({ title: vscode.l10n.t('Initialize workspace'), command: 'azure-dev.commands.cli.init' });
+    }
+
+    responseStream.markdown(vscode.l10n.t('- All that\'s left is to deploy your application to Azure!'));
+    responseStream.button({ title: vscode.l10n.t('Deploy to Azure'), command: 'azure-dev.commands.cli.up' });
+
+    return {
+        chatAgentResult: { }
+    };
+}
+
+async function isWorkspaceInitialized(context: IActionContext): Promise<boolean> {
+    // Look for at most one file named azure.yml or azure.yaml, only at the root, to avoid perf issues
+    const fileResults = await vscode.workspace.findFiles('azure.{yml,yaml}', undefined, 1);
+
+    return !!fileResults?.length;
+}

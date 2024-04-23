c836afc3b190e2e2264c0bf13ee5874d4345b011
Matt Ellis <matell@microsoft.com>
2024-04-23 18:57:04 -0400

    Support `args` on `project.v0` and `dockerfile.v0` resources (#3720)
    We allow binding expressions to refer to properties of other
    resources, but we fail if any of these values are secrets (since ACA
    has no way to treat these as ACA or KeyVault secrets and reference
    them). If we detect this case, we fail and encourage the user to pass
    these values via environment variaibles.
    
    Fixes #3678

--- cli/azd/pkg/apphost/generate_test.go
+++ cli/azd/pkg/apphost/generate_test.go
@@ -26,6 +26,9 @@ var aspireDockerManifest []byte
 //go:embed testdata/aspire-storage.json
 var aspireStorageManifest []byte
 
+//go:embed testdata/aspire-args.json
+var aspireArgsManifest []byte
+
 //go:embed testdata/aspire-bicep.json
 var aspireBicepManifest []byte
 
@@ -210,6 +213,25 @@ func TestAspireDockerGeneration(t *testing.T) {
 		return nil
 	})
 	require.NoError(t, err)
+}
+
+func TestAspireArgsGeneration(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Skipping due to EOL issues on Windows with the baselines")
+	}
+
+	ctx := context.Background()
+	mockCtx := mocks.NewMockContext(ctx)
+	mockPublishManifest(mockCtx, aspireArgsManifest, nil)
+	mockCli := dotnet.NewDotNetCli(mockCtx.CommandRunner)
+
+	m, err := ManifestFromAppHost(ctx, filepath.Join("testdata", "AspireArgs.AppHost.csproj"), mockCli, "")
+	require.NoError(t, err)
+
+	manifest, err := ContainerAppManifestTemplateForProject(m, "apiservice", false)
+	require.NoError(t, err)
+
+	snapshot.SnapshotT(t, manifest)
 }
 
 func TestAspireContainerGeneration(t *testing.T) {
--- cli/azd/pkg/apphost/generate_types.go
+++ cli/azd/pkg/apphost/generate_types.go
@@ -70,11 +70,13 @@ type genDockerfile struct {
 	Env       map[string]string
 	Bindings  custommaps.WithOrder[Binding]
 	BuildArgs map[string]string
+	Args      []string
 }
 
 type genProject struct {
 	Path     string
 	Env      map[string]string
+	Args     []string
 	Bindings custommaps.WithOrder[Binding]
 }
 
@@ -154,6 +156,7 @@ type genContainerAppManifestTemplateContext struct {
 	KeyVaultSecrets             map[string]string
 	Dapr                        *genContainerAppManifestTemplateContextDapr
 	AutoConfigureDataProtection bool
+	Args                        []string
 }
 
 type genProjectFileContext struct {

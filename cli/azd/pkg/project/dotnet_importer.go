c4c923cf7f2f1843f20fe6b5778a32702fc6eb4d
Wei Lim <weilim@microsoft.com>
2024-04-30 12:24:23 -0700

    remove need for normalized path

--- cli/azd/pkg/project/dotnet_importer.go
+++ cli/azd/pkg/project/dotnet_importer.go
@@ -294,13 +294,6 @@ func (ai *DotNetImporter) SynthAllInfrastructure(
 		return nil, err
 	}
 
-	// Use canonical paths for Rel comparison due to absolute paths provided by ManifestFromAppHost
-	// being possibly symlinked paths.
-	root, err := filepath.EvalSymlinks(p.Path)
-	if err != nil {
-		return nil, err
-	}
-
 	// writeManifestForResource writes the containerApp.tmpl.yaml for the given resource to the generated filesystem. The
 	// manifest is written to a file name "containerApp.tmpl.yaml" in the same directory as the project that produces the
 	// container we will deploy.
@@ -313,12 +306,7 @@ func (ai *DotNetImporter) SynthAllInfrastructure(
 			return fmt.Errorf("generating containerApp.tmpl.yaml for resource %s: %w", name, err)
 		}
 
-		normalPath, err := filepath.EvalSymlinks(path)
-		if err != nil {
-			return err
-		}
-
-		projectRelPath, err := filepath.Rel(root, normalPath)
+		projectRelPath, err := filepath.Rel(p.Path, path)
 		if err != nil {
 			return err
 		}

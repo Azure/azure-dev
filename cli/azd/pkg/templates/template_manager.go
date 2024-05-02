ab33451128e8f43bc04e4e6a7c7786f7c0d33684
Wallace Breza <wallace.breza@microsoft.com>
2024-05-02 12:41:01 -0700

    Adds 'tags' to templates and supports filtering & showing

--- cli/azd/pkg/templates/template_manager.go
+++ cli/azd/pkg/templates/template_manager.go
@@ -29,9 +29,11 @@ func NewTemplateManager(sourceManager SourceManager, console input.Console) (*Te
 
 type ListOptions struct {
 	Source string
+	Tags   []string
 }
 
 type sourceFilterPredicate func(config *SourceConfig) bool
+type templateFilterPredicate func(template *Template) bool
 
 // ListTemplates retrieves the list of templates in a deterministic order.
 func (tm *TemplateManager) ListTemplates(ctx context.Context, options *ListOptions) ([]*Template, error) {
@@ -41,26 +43,52 @@ func (tm *TemplateManager) ListTemplates(ctx context.Context, options *ListOptio
 
 	allTemplates := []*Template{}
 
-	var filterPredicate sourceFilterPredicate
+	var sourceFilterPredicate sourceFilterPredicate
 	if options != nil && options.Source != "" {
-		filterPredicate = func(config *SourceConfig) bool {
+		sourceFilterPredicate = func(config *SourceConfig) bool {
 			return strings.EqualFold(config.Key, options.Source)
 		}
 	}
 
-	sources, err := tm.getSources(ctx, filterPredicate)
+	var templateFilterPredicate templateFilterPredicate
+	if options != nil && len(options.Tags) > 0 {
+		// Find templates that match all the incoming tags
+		templateFilterPredicate = func(template *Template) bool {
+			match := false
+			for _, optionTag := range options.Tags {
+				match = slices.ContainsFunc(template.Tags, func(templateTag string) bool {
+					return strings.EqualFold(optionTag, templateTag)
+				})
+
+				if !match {
+					break
+				}
+			}
+
+			return match
+		}
+	}
+
+	sources, err := tm.getSources(ctx, sourceFilterPredicate)
 	if err != nil {
 		return nil, fmt.Errorf("failed listing templates: %w", err)
 	}
 
 	for _, source := range sources {
-		templates, err := source.ListTemplates(ctx)
+		filteredTemplates := []*Template{}
+		sourceTemplates, err := source.ListTemplates(ctx)
 		if err != nil {
 			return nil, fmt.Errorf("unable to list templates: %w", err)
 		}
 
+		for _, template := range sourceTemplates {
+			if templateFilterPredicate == nil || templateFilterPredicate(template) {
+				filteredTemplates = append(filteredTemplates, template)
+			}
+		}
+
 		// Sort by source, then repository path and finally name
-		slices.SortFunc(templates, func(a *Template, b *Template) bool {
+		slices.SortFunc(filteredTemplates, func(a *Template, b *Template) bool {
 			if a.Source != b.Source {
 				return a.Source < b.Source
 			}
@@ -72,7 +100,7 @@ func (tm *TemplateManager) ListTemplates(ctx context.Context, options *ListOptio
 			return a.Name < b.Name
 		})
 
-		allTemplates = append(allTemplates, templates...)
+		allTemplates = append(allTemplates, filteredTemplates...)
 	}
 
 	return allTemplates, nil

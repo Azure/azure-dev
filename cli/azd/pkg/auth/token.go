107c70395613a1b3f2b8b3188bf132645a9a5372
Wei Lim <weilim@microsoft.com>
2024-05-01 17:25:18 -0700

    add token claims

--- cli/azd/pkg/auth/token.go
+++ cli/azd/pkg/auth/token.go
@@ -18,84 +18,74 @@ var jwtClaimsRegex = regexp.MustCompile(`^[a-zA-Z0-9-_]*\.([a-zA-Z0-9-_]*)\.[a-z
 
 // cspell: enable
 
+// TokenClaims contains claims about a user.
+// https://docs.microsoft.com/azure/active-directory/develop/id-tokens .
+type TokenClaims struct {
+	PreferredUsername string `json:"preferred_username,omitempty"`
+	GivenName         string `json:"given_name,omitempty"`
+	FamilyName        string `json:"family_name,omitempty"`
+	MiddleName        string `json:"middle_name,omitempty"`
+	Name              string `json:"name,omitempty"`
+	Oid               string `json:"oid,omitempty"`
+	TenantId          string `json:"tid,omitempty"`
+	Subject           string `json:"sub,omitempty"`
+	UPN               string `json:"upn,omitempty"`
+	Email             string `json:"email,omitempty"`
+	AlternativeID     string `json:"alternative_id,omitempty"`
+	Issuer            string `json:"iss,omitempty"`
+	Audience          string `json:"aud,omitempty"`
+	ExpirationTime    int64  `json:"exp,omitempty"`
+	IssuedAt          int64  `json:"iat,omitempty"`
+	NotBefore         int64  `json:"nbf,omitempty"`
+}
+
 func GetTenantIdFromToken(token string) (string, error) {
-	return getTidClaimFromAccessToken(token)
+	claims, err := GetClaimsFromAccessToken(token)
+	if err != nil {
+		return "", err
+	}
+
+	if claims.TenantId == "" {
+		return "", errors.New("no tid claim")
+	}
+
+	return claims.TenantId, nil
 }
 
 // GetOidFromAccessToken extracts a string claim with the name "oid" from an access token.
 // Access Tokens are JWT and the middle component is a base64 encoded string of a JSON object
 // with claims.
 func GetOidFromAccessToken(token string) (string, error) {
-	matches := jwtClaimsRegex.FindStringSubmatch(token)
-	if len(matches) != 2 {
-		return "", errors.New("malformed access token")
-	}
-
-	bytes, err := base64.RawURLEncoding.DecodeString(matches[1])
+	claims, err := GetClaimsFromAccessToken(token)
 	if err != nil {
 		return "", err
 	}
 
-	var claims struct {
-		Oid *string
-	}
-
-	if err := json.Unmarshal(bytes, &claims); err != nil {
-		return "", err
-	}
-
-	if claims.Oid == nil {
+	if claims.Oid == "" {
 		return "", errors.New("no oid claim")
 	}
 
-	return *claims.Oid, nil
+	return claims.Oid, nil
 }
 
 // GetClaimsFromAccessToken extracts claims from an access token.
 // Access Tokens are JWT and the middle component is a base64 encoded string of a JSON object
 // with claims.
-func GetClaimsFromAccessToken(token string) (map[string]any, error) {
+func GetClaimsFromAccessToken(token string) (TokenClaims, error) {
 	matches := jwtClaimsRegex.FindStringSubmatch(token)
 	if len(matches) != 2 {
-		return nil, errors.New("malformed access token")
+		return TokenClaims{}, errors.New("malformed access token")
 	}
 
 	bytes, err := base64.RawURLEncoding.DecodeString(matches[1])
 	if err != nil {
-		return nil, err
+		return TokenClaims{}, err
 	}
 
-	var claims map[string]any
-
+	var claims TokenClaims
 	if err := json.Unmarshal(bytes, &claims); err != nil {
-		return nil, err
+		return TokenClaims{}, err
 	}
 
 	return claims, nil
-}
-
-func getTidClaimFromAccessToken(token string) (string, error) {
-	matches := jwtClaimsRegex.FindStringSubmatch(token)
-	if len(matches) != 2 {
-		return "", errors.New("malformed access token")
-	}
-
-	bytes, err := base64.RawURLEncoding.DecodeString(matches[1])
-	if err != nil {
-		return "", err
-	}
-
-	var claims struct {
-		Tid *string
-	}
-
-	if err := json.Unmarshal(bytes, &claims); err != nil {
-		return "", err
-	}
-
-	if claims.Tid == nil {
-		return "", errors.New("no tid claim")
-	}
-
-	return *claims.Tid, nil
 }

a47928ac827801dc8bba5928110b335862d23ea1
Victor Vazquez <vhvb1989@gmail.com>
2024-05-30 18:39:52 +0000

    enhance comment

--- cli/azd/pkg/project/framework_service.go
+++ cli/azd/pkg/project/framework_service.go
@@ -42,7 +42,8 @@ func parseServiceLanguage(kind ServiceLanguageKind) (ServiceLanguageKind, error)
 		ServiceLanguageTypeScript,
 		ServiceLanguagePython,
 		ServiceLanguageJava:
-		// Excluding ServiceLanguageDocker since it is implicitly derived currently, and not an actual language
+		// Excluding ServiceLanguageDocker and ServiceLanguageSwa since it is implicitly derived currently,
+		// and not an actual language
 		return kind, nil
 	}
 

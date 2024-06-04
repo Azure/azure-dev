33132a65343247392c2668c2fd4232bcee296e73
Victor Vazquez <vhvb1989@gmail.com>
2024-06-04 05:59:34 +0000

    test

--- cli/azd/pkg/tools/terraform/terraform_test.go
+++ cli/azd/pkg/tools/terraform/terraform_test.go
@@ -2,6 +2,8 @@ package terraform
 
 import (
 	"context"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/azure/azure-dev/cli/azd/pkg/exec"
@@ -11,16 +13,32 @@ import (
 
 func Test_WithEnv(t *testing.T) {
 	ran := false
-	expectedEnvVars := []string{"TF_DATA_DIR=MYDIR"}
+	expectedEnvVars := []string{"TF_DATA_DIR=MYDIR", "AZURE_AZ_EMULATOR=true"}
 
 	mockContext := mocks.NewMockContext(context.Background())
 	mockContext.CommandRunner.When(func(args exec.RunArgs, command string) bool {
 		return args.Cmd == "terraform"
 	}).RespondFn(func(args exec.RunArgs) (exec.RunResult, error) {
 		ran = true
-		require.Len(t, expectedEnvVars, 1)
-		require.Equal(t, expectedEnvVars, args.Env)
-
+		require.GreaterOrEqual(t, len(args.Env), 2)
+		for _, expectedEnvVar := range expectedEnvVars {
+			require.Contains(t, args.Env, expectedEnvVar)
+		}
+		var pathKey, pathValue string
+		for _, envV := range args.Env {
+			parts := strings.Split(envV, "=")
+			pathKey = parts[0]
+			if pathKey == "PATH" {
+				if len(parts) > 1 {
+					pathValue = parts[1]
+				}
+				break
+			}
+		}
+		// can't match pathValue as it is different depending on the OS.
+		// So just check that it is not empty and contains the path to emulate
+		require.NotEmpty(t, pathValue)
+		require.Contains(t, pathValue, string(filepath.Separator)+"azEmulate")
 		return exec.NewRunResult(0, "", ""), nil
 	})
 

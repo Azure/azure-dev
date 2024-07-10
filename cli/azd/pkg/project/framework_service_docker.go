a5e651842f1c70128cbc2f39b8bdf3250792aebc
Zhiyong Li <iamlizhiyong@outlook.com>
2024-07-10 08:18:38 +0800

    Oryx builder upgrade: Use the standard builder (#4046)
    The mcr.microsoft.com/oryx/builder:aca-debian-bullseye-20240424.1 is used to run in cloud environment, local builder should use the standard one mcr.microsoft.com/oryx/builder:debian-bullseye-20240424.1

--- cli/azd/pkg/project/framework_service_docker.go
+++ cli/azd/pkg/project/framework_service_docker.go
@@ -376,8 +376,8 @@ func (p *dockerProject) Package(
 	)
 }
 
-// Default builder image to produce container images from source, needn't java jdk storage
-const DefaultBuilderImage = "mcr.microsoft.com/oryx/builder:aca-debian-bullseye-20240424.1"
+// Default builder image to produce container images from source, needn't java jdk storage, use the standard bp
+const DefaultBuilderImage = "mcr.microsoft.com/oryx/builder:debian-bullseye-20240424.1"
 
 func (p *dockerProject) packBuild(
 	ctx context.Context,

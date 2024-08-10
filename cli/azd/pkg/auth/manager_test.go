741bf355ff56fac953357078d8a1ddb66e9a5bda
Matt Ellis <matell@microsoft.com>
2024-08-10 01:17:38 -0700

    chore: Remove "lowercase c" pattern from the codebase (#4196)
    Early on in the project, we used to use this pattern where we'd
    prepend a lowercase c in front of `const` values (and var values which
    were logical constants but couldn't be marked as `const` because their
    values were not compile time constants). We did this to ensure the
    symbol was not exported but to still have something
    `ThatLookedLikeItWouldBeExported` with the idea that this would make
    it more obvious in code this was a constant value.
    
    However, idiomatic go code doesn't do this, so we stopped doing it
    ourselves on new code. This change does a pass over the old pattern
    and removes it from the codebase. In most cases I just dropped the
    leading c and lowercased the first letter (so the symbol is not
    exported) but in a few cases where the symbol was only used in one or
    two places I inlined it or rewrote code slightly if I felt it would
    improve overall clarity.

--- cli/azd/pkg/auth/manager_test.go
+++ cli/azd/pkg/auth/manager_test.go
@@ -18,6 +18,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/public"
+	"github.com/azure/azure-dev/cli/azd/internal/runcontext"
 	"github.com/azure/azure-dev/cli/azd/pkg/cloud"
 	"github.com/azure/azure-dev/cli/azd/pkg/config"
 	"github.com/azure/azure-dev/cli/azd/pkg/github"
@@ -87,7 +88,7 @@ func TestServicePrincipalLoginClientSecret(t *testing.T) {
 }
 
 //go:embed testdata/certificate.pem
-var cTestClientCertificate []byte
+var testClientCertificate []byte
 
 func TestServicePrincipalLoginClientCertificate(t *testing.T) {
 	credentialCache := &memoryCache{
@@ -102,7 +103,7 @@ func TestServicePrincipalLoginClientCertificate(t *testing.T) {
 	}
 
 	cred, err := m.LoginWithServicePrincipalCertificate(
-		context.Background(), "testClientId", "testTenantId", cTestClientCertificate,
+		context.Background(), "testClientId", "testTenantId", testClientCertificate,
 	)
 
 	require.NoError(t, err)
@@ -176,7 +177,7 @@ func TestLegacyAzCliCredentialSupport(t *testing.T) {
 	cfg, err := mgr.Load()
 	require.NoError(t, err)
 
-	err = cfg.Set(cUseAzCliAuthKey, "true")
+	err = cfg.Set(useAzCliAuthKey, "true")
 	require.NoError(t, err)
 
 	err = mgr.Save(cfg)
@@ -193,7 +194,7 @@ func TestLegacyAzCliCredentialSupport(t *testing.T) {
 }
 
 func TestCloudShellCredentialSupport(t *testing.T) {
-	t.Setenv("AZD_IN_CLOUDSHELL", "1")
+	t.Setenv(runcontext.AzdInCloudShellEnvVar, "1")
 	m := Manager{
 		configManager:     newMemoryConfigManager(),
 		userConfigManager: newMemoryUserConfigManager(),
@@ -267,7 +268,7 @@ func TestAuthFileConfigUpgrade(t *testing.T) {
 	userCfg := config.NewEmptyConfig()
 	userCfgMgr := newMemoryUserConfigManager()
 
-	err := userCfg.Set(cCurrentUserKey, &userProperties{
+	err := userCfg.Set(currentUserKey, &userProperties{
 		HomeAccountID: to.Ptr("homeAccountID"),
 	})
 	require.NoError(t, err)
@@ -291,7 +292,7 @@ func TestAuthFileConfigUpgrade(t *testing.T) {
 
 	// as part of running readAuthConfig, we migrated the setting from the user config to the auth config
 	// so the current user key should no longer be set in the user configuration.
-	_, has := userCfgMgr.config.Get(cCurrentUserKey)
+	_, has := userCfgMgr.config.Get(currentUserKey)
 	require.False(t, has)
 }
 

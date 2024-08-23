4e9986d93c6d53f69e8c4afa4bf968059c450918
Victor Vazquez <vhvb1989@gmail.com>
2024-08-22 23:19:22 -0700

    update swa cli - version (#4183)
    * pin cspell version due to issue on last release
    
    * update swa-cli version

--- cli/azd/pkg/tools/swa/swa.go
+++ cli/azd/pkg/tools/swa/swa.go
@@ -17,7 +17,7 @@ import (
 )
 
 // swaCliPackage is the npm package (including the version version) we execute with npx to run the SWA CLI.
-const swaCliPackage = "@azure/static-web-apps-cli@1.1.8"
+const swaCliPackage = "@azure/static-web-apps-cli@2.0.1"
 
 var _ tools.ExternalTool = (*Cli)(nil)
 

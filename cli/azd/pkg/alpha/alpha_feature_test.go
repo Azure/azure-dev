fb6efa1d3f71cd134d5a82d0cb66220bc6244ea1
Wallace Breza <wallace.breza@microsoft.com>
2024-08-26 12:30:13 -0700

    Adds unit tests for checking alpha feature enablement

--- cli/azd/pkg/alpha/alpha_feature_test.go
+++ cli/azd/pkg/alpha/alpha_feature_test.go
@@ -1,6 +1,8 @@
 package alpha
 
 import (
+	"fmt"
+	"os"
 	"sync"
 	"testing"
 
@@ -144,6 +146,54 @@ func Test_AlphaToggle(t *testing.T) {
 		_ = NewFeaturesManager(config.NewUserConfigManager(config.NewFileConfigManager(config.NewManager())))
 	})
 
+}
+
+func Test_AlphaFeature_IsEnabled(t *testing.T) {
+	mockAlphaFeatures := []Feature{}
+	for i := 1; i <= 10; i++ {
+		mockAlphaFeatures = append(mockAlphaFeatures, Feature{
+			Id:          fmt.Sprintf("category.feature.%d", i),
+			Description: fmt.Sprintf("Description for feature %d", i),
+		})
+	}
+
+	featuresResolver := func() []Feature {
+		return mockAlphaFeatures
+	}
+
+	mockConfig := config.NewConfig(nil)
+
+	// Enable even numbered features
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			featureId := fmt.Sprintf("alpha.category.feature.%d", i)
+			mockConfig.Set(featureId, "on")
+		}
+	}
+
+	alphaManager := newFeatureManagerForTest(featuresResolver, mockConfig)
+
+	t.Run("enabled from config", func(t *testing.T) {
+		actual := alphaManager.IsEnabled(FeatureId("category.feature.2"))
+		require.True(t, actual)
+	})
+
+	t.Run("disabled from config", func(t *testing.T) {
+		actual := alphaManager.IsEnabled(FeatureId("category.feature.1"))
+		require.False(t, actual)
+	})
+
+	t.Run("enabled from env var", func(t *testing.T) {
+		os.Setenv("AZD_ALPHA_ENABLE_CATEGORY_FEATURE_3", "true")
+		actual := alphaManager.IsEnabled(FeatureId("category.feature.3"))
+		require.True(t, actual)
+	})
+
+	t.Run("enabled from default", func(t *testing.T) {
+		SetDefaultEnablement("category.feature.9", true)
+		actual := alphaManager.IsEnabled(FeatureId("category.feature.9"))
+		require.True(t, actual)
+	})
 }
 
 func newFeatureManagerForTest(alphaFeatureResolver func() []Feature, config config.Config) *FeatureManager {

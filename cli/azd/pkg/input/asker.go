b7abfe41faeccb94f59239f3e719aadb47debf79
Wei Lim <weilim@microsoft.com>
2024-09-10 19:44:05 -0700

    remove multiselect customization

--- cli/azd/pkg/input/asker.go
+++ cli/azd/pkg/input/asker.go
@@ -14,27 +14,6 @@ import (
 	"github.com/AlecAivazis/survey/v2"
 )
 
-func init() {
-	survey.MultiSelectQuestionTemplate = `
-	{{- define "option"}}
-		{{- if eq .SelectedIndex .CurrentIndex }}{{color .Config.Icons.SelectFocus.Format }}{{ .Config.Icons.SelectFocus.Text }}{{color "reset"}}{{else}} {{end}}
-		{{- if index .Checked .CurrentOpt.Index }}{{color .Config.Icons.MarkedOption.Format }} {{ .Config.Icons.MarkedOption.Text }} {{else}}{{color .Config.Icons.UnmarkedOption.Format }} {{ .Config.Icons.UnmarkedOption.Text }} {{end}}
-		{{- color "reset"}}
-		{{- " "}}{{- .CurrentOpt.Value}}
-	{{end}}
-	{{- if .ShowHelp }}{{- color .Config.Icons.Help.Format }}{{ .Config.Icons.Help.Text }} {{ .Help }}{{color "reset"}}{{"\n"}}{{end}}
-	{{- color .Config.Icons.Question.Format }}{{ .Config.Icons.Question.Text }} {{color "reset"}}
-	{{- color "default+hb"}}{{ .Message }}{{ .FilterMessage }}{{color "reset"}}
-	{{- if .ShowAnswer}}{{color "cyan"}} {{.Answer}}{{color "reset"}}{{"\n"}}
-	{{- else }}
-		{{- "  "}}{{- color "cyan"}}[Use arrows to move, space to select, enter to continue, type to filter{{- if and .Help (not .ShowHelp)}}, {{ .Config.HelpInput }} for more help{{end}}]{{color "reset"}}
-	  {{- "\n"}}
-	  {{- range $ix, $option := .PageEntries}}
-		{{- template "option" $.IterateOption $ix $option}}
-	  {{- end}}
-	{{- end}}`
-}
-
 type Asker func(p survey.Prompt, response interface{}) error
 
 func NewAsker(noPrompt bool, isTerminal bool, w io.Writer, r io.Reader) Asker {

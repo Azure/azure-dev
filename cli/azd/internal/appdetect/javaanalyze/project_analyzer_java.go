de2e922389f53eda82345990e643843e96585960
rujche <rujche@microsoft.com>
2024-10-15 17:26:38 +0800

    Implement feature: detect redis by analyzing pom file.

--- cli/azd/internal/appdetect/javaanalyze/project_analyzer_java.go
+++ cli/azd/internal/appdetect/javaanalyze/project_analyzer_java.go
@@ -14,6 +14,7 @@ func Analyze(path string) []AzureYaml {
 		&ruleMysql{},
 		&rulePostgresql{},
 		&ruleMongo{},
+		&ruleRedis{},
 		&ruleStorage{},
 		&ruleServiceBusScsb{},
 	}
--- cli/azd/internal/appdetect/javaanalyze/rule_redis.go
+++ cli/azd/internal/appdetect/javaanalyze/rule_redis.go
@@ -4,6 +4,16 @@ type ruleRedis struct {
 }
 
 func (r *ruleRedis) match(javaProject *javaProject) bool {
+	if javaProject.mavenProject.Dependencies != nil {
+		for _, dep := range javaProject.mavenProject.Dependencies {
+			if dep.GroupId == "org.springframework.boot" && dep.ArtifactId == "spring-boot-starter-data-redis" {
+				return true
+			}
+			if dep.GroupId == "org.springframework.boot" && dep.ArtifactId == "spring-boot-starter-data-redis-reactive" {
+				return true
+			}
+		}
+	}
 	return false
 }
 

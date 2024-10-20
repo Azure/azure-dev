5823bc3ead96bb4e93a93e93cff7f41bb92330f7
rujche <rujche@microsoft.com>
2024-10-21 00:30:08 +0800

    Fix test failure.

--- cli/azd/internal/appdetect/appdetect_test.go
+++ cli/azd/internal/appdetect/appdetect_test.go
@@ -194,7 +194,7 @@ func TestDetectDocker(t *testing.T) {
 		DetectionRule: "Inferred by presence of: dotnettestapp.csproj, Program.cs",
 		Docker: &Docker{
 			Path:  filepath.Join(dir, "dotnet", "Dockerfile"),
-			Ports: []Port{},
+			Ports: nil,
 		},
 	})
 }

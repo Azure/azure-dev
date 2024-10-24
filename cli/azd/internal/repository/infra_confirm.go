8fd80cbbc4a53a6cc9f0b9a6016dc016d2fddcaa
Victor Vazquez <vhvb1989@gmail.com>
2024-10-23 19:37:34 -0700

    fix projects with empty spaces aspire (#4472)
    * use label as package out of repository and re-use for apphost project generation
    
    * use names
    
    * use GetDefaultProjectName
    
    * fix `show` not using project name as AppName
    
    * refactor as a function
    
    * clean up one spot
    
    * add copyright notice
    
    * remove panic
    
    ---------
    
    Co-authored-by: Wei Lim <weilim@microsoft.com>

--- cli/azd/internal/repository/infra_confirm.go
+++ cli/azd/internal/repository/infra_confirm.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/azure/azure-dev/cli/azd/internal/appdetect"
+	"github.com/azure/azure-dev/cli/azd/internal/names"
 	"github.com/azure/azure-dev/cli/azd/internal/scaffold"
 	"github.com/azure/azure-dev/cli/azd/pkg/input"
 	"github.com/azure/azure-dev/cli/azd/pkg/output/ux"
@@ -93,7 +94,7 @@ func (i *Initializer) infraSpecFromDetect(
 	}
 
 	for _, svc := range detect.Services {
-		name := LabelName(filepath.Base(svc.Path))
+		name := names.LabelName(filepath.Base(svc.Path))
 		serviceSpec := scaffold.ServiceSpec{
 			Name: name,
 			Port: -1,

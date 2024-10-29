56eaa409e7b8a72a61008cce13ba65833e7b0d09
Victor Vazquez <vhvb1989@gmail.com>
2024-10-29 12:48:19 -0700

    azdo ext add node 20 for warning (#4497)
    * Update Install Azd - azdo task
    
    * increase timeout for resolving 404

--- ext/azuredevops/setupAzd/tests/InstallFail.ts
+++ ext/azuredevops/setupAzd/tests/InstallFail.ts
@@ -5,7 +5,7 @@ import * as fs from 'fs'
 import { log } from 'console';
 
 describe('setup azd tests - fails', function () {
-    setTimeout(() => {}, 10000);
+    setTimeout(() => { }, 60000);
     before(function () { });
     afterEach(() => {
         fs.rmSync('path', { recursive: true, force: true })
@@ -15,12 +15,15 @@ describe('setup azd tests - fails', function () {
         setTimeout(() => { }, 10000);
         let tp = path.join(__dirname, 'invalidVersion.js');
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-       
-        tr.run();
-        assert.equal(tr.succeeded, false, 'should have failed');
-        assert.equal(tr.warningIssues.length, 0, "should have no warnings");
-        assert.equal(tr.errorIssues.length, 1, "should have error");
-        assert.equal(tr.stdout.indexOf('Response code 404 (Not Found)') >= 0, true, "should display error");
-        done();
+
+        tr.runAsync().then(() => {
+            assert.equal(tr.succeeded, false, 'should have failed');
+            assert.equal(tr.warningIssues.length, 0, "should have no warnings");
+            assert.equal(tr.errorIssues.length, 1, "should have error");
+            assert.equal(tr.stdout.indexOf('Response code 404 (The specified blob does not exist.)') >= 0, true, "should display error");
+            done();
+        }).catch((reason) => {
+            done(reason);
+        });
     });
 });

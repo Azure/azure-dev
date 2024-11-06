31f8240eaf90966815d6d808ccdc33b166214b77
HaoZhang <haozhangms@outlook.com>
2024-11-06 11:24:58 +0800

    Update azure.yaml schema reference for private preview (#13)
    * update azure yaml schema to support `resources`
    
    * small update for private preview
    
    ---------
    
    Co-authored-by: Hao Zhang <haozhan@microsoft.com>

--- cli/azd/pkg/project/project.go
+++ cli/azd/pkg/project/project.go
@@ -23,7 +23,8 @@ import (
 
 const (
 	//nolint:lll
-	projectSchemaAnnotation = "# yaml-language-server: $schema=https://raw.githubusercontent.com/Azure/azure-dev/main/schemas/v1.0/azure.yaml.json"
+	// todo(haozhan): update this line for sjad private preview, need to revert it when merge into azure-dev/main branch
+	projectSchemaAnnotation = "# yaml-language-server: $schema=https://raw.githubusercontent.com/azure-javaee/azure-dev/feature/sjad/schemas/alpha/azure.yaml.json"
 )
 
 func New(ctx context.Context, projectFilePath string, projectName string) (*ProjectConfig, error) {

1e8bdb71afdb2f5e204eb08e43a3c81d7f10c58f
Daniel Jurek <djurek@microsoft.com>
2024-12-13 15:16:13 -0800

    Migrate to new CDN endpoint

--- cli/installer/test-pwsh-xplat-install.ps1
+++ cli/installer/test-pwsh-xplat-install.ps1
@@ -1,5 +1,5 @@
 param(
-    [string] $BaseUrl = 'https://azdrelease.azureedge.net/azd/standalone/release',
+    [string] $BaseUrl = 'https://azd-release-gfgac2cmf7b8cuay.b02.azurefd.net/azd/standalone/release',
     [string] $Version = 'latest',
     [string] $InstallShScriptUrl = 'https://aka.ms/install-azd.sh',
     [string] $UninstallShScriptUrl = 'https://aka.ms/uninstall-azd.sh'

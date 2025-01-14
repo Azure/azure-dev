87ef7cf71c250049ac1570e150822f9bee351da5
Wallace Breza <wallace.breza@microsoft.com>
2025-01-14 12:16:33 -0800

    Adds grpc proto files and generated services

---
+++ cli/azd/pkg/azdext/azd_client.go
@@ -0,0 +1,91 @@
+package azdext
+
+import (
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type AzdClientOption func(*AzdClient) error
+
+type AzdClient struct {
+	connection        *grpc.ClientConn
+	projectClient     ProjectServiceClient
+	environmentClient EnvironmentServiceClient
+	userConfigClient  UserConfigServiceClient
+	promptClient      PromptServiceClient
+	deploymentClient  DeploymentServiceClient
+}
+
+func WithAddress(address string) AzdClientOption {
+	return func(c *AzdClient) error {
+		connection, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			return err
+		}
+
+		c.connection = connection
+		return nil
+	}
+}
+
+func NewAzdClient(opts ...AzdClientOption) (*AzdClient, error) {
+	if opts == nil {
+		opts = append(opts, WithAddress(os.Getenv("AZD_SERVER")))
+	}
+
+	client := &AzdClient{}
+
+	for _, opt := range opts {
+		if err := opt(client); err != nil {
+			return nil, err
+		}
+	}
+
+	return client, nil
+}
+
+func (c *AzdClient) Close() {
+	c.connection.Close()
+}
+
+func (c *AzdClient) Project() ProjectServiceClient {
+	if c.projectClient == nil {
+		c.projectClient = NewProjectServiceClient(c.connection)
+	}
+
+	return c.projectClient
+}
+
+func (c *AzdClient) Environment() EnvironmentServiceClient {
+	if c.environmentClient == nil {
+		c.environmentClient = NewEnvironmentServiceClient(c.connection)
+	}
+
+	return c.environmentClient
+}
+
+func (c *AzdClient) UserConfig() UserConfigServiceClient {
+	if c.userConfigClient == nil {
+		c.userConfigClient = NewUserConfigServiceClient(c.connection)
+	}
+
+	return c.userConfigClient
+}
+
+func (c *AzdClient) Prompt() PromptServiceClient {
+	if c.promptClient == nil {
+		c.promptClient = NewPromptServiceClient(c.connection)
+	}
+
+	return c.promptClient
+}
+
+func (c *AzdClient) Deployment() DeploymentServiceClient {
+	if c.deploymentClient == nil {
+		c.deploymentClient = NewDeploymentServiceClient(c.connection)
+	}
+
+	return c.deploymentClient
+}

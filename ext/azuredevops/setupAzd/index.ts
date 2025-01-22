6b7fb06f3fc84f014b05a927f86f0416897ee184
hemarina <hemarina@microsoft.com>
2025-01-22 09:55:29 -0800

    align with gha

--- ext/azuredevops/setupAzd/index.ts
+++ ext/azuredevops/setupAzd/index.ts
@@ -1,58 +1,30 @@
 import * as task from 'azure-pipelines-task-lib/task';
-import * as path from 'path';
 import * as cp from 'child_process'
-import * as fs from 'fs'
 
 export async function runMain(): Promise<void> {
     try {
         task.setTaskVariable('hasRunMain', 'true');
-        const version = task.getInput('version') || 'latest'
-        const localAppDataPath = process.env.LocalAppData
         const os = process.platform
+        const version = task.getInput('version') || 'latest'
+        const windowsInstallScript = `powershell -c "$scriptPath = \\"$($env:TEMP)\\install-azd.ps1\\"; Invoke-RestMethod 'https://aka.ms/install-azd.ps1' -OutFile $scriptPath; . $scriptPath -Version '${version}' -Verbose:$true; Remove-Item $scriptPath"`
+        const linuxOrMacOSInstallScript = `curl -fsSL https://aka.ms/install-azd.sh | sudo bash -s -- --version ${version} --verbose`
 
-        let windowsInstallScript = `powershell -ex AllSigned -c "Invoke-RestMethod 'https://aka.ms/install-azd.ps1' | Invoke-Expression"`
-        let linuxOrMacOSInstallScript = `curl -fsSL https://aka.ms/install-azd.sh | sudo bash`
-        if (version !== 'latest') {
-            windowsInstallScript = `powershell -ex AllSigned -c "Invoke-RestMethod 'https://aka.ms/install-azd.ps1' -OutFile 'install-azd.ps1'; powershell -execAsyncutionPolicy Bypass -File ./install-azd.ps1 -Version '${version}'"`
-            linuxOrMacOSInstallScript = `curl -fsSL https://aka.ms/install-azd.sh | sudo bash -s -- --version ${version}`
-        }
-
-        console.log(`Installing azd version ${version} on ${os}.\n`)
+        console.log(`Installing azd version ${version} on ${os}.`)
 
         if (os === 'win32') {
-            cp.execSync(windowsInstallScript)
+            console.log(cp.execSync(windowsInstallScript).toString())
+            
             // Add azd to PATH
-            if (localAppDataPath) {
-                const azdPath = path.join(localAppDataPath, 'Programs', 'Azure Dev CLI')
-                fs.appendFileSync(
-                    process.env.GITHUB_PATH || '',
-                    `${azdPath}${path.delimiter}`
-                )
-            } else {
-                console.log('LocalAppData environment variable is not defined.')
-                return
-            }
+            cp.execSync('Write-Host "##vso[task.prependpath]$($env:LocalAppData)\\Programs\\Azure Dev CLI"')
         } else {
-            cp.execSync(linuxOrMacOSInstallScript)
+            console.log(cp.execSync(linuxOrMacOSInstallScript).toString())
         }
 
-        console.log(`The Azure Developer CLI collects usage data and sends that usage data to Microsoft in order to help us improve your experience.
-You can opt-out of telemetry by setting the AZURE_DEV_COLLECT_TELEMETRY environment variable to 'no' in the shell you use.
-Read more about Azure Developer CLI telemetry: https://github.com/Azure/azure-dev#data-collection`)
-
-        // Run `azd version` so we get the version that was installed written to the log.
-        let azdVersion = 'azd version'
-        if (os === 'win32' && localAppDataPath) {
-            const azdExePath = path.join(
-                localAppDataPath,
-                'Programs',
-                'Azure Dev CLI',
-                'azd.exe'
-            )
-            azdVersion = `"${azdExePath}" version`
+        // Run `azd version` to make sure if azd installation failed, it returns error on windows
+        if (os === 'win32') {
+            const azdVersion = `"$($env:LocalAppData)\\Programs\\Azure Dev CLI\\azd.exe" version`
+            cp.execSync(azdVersion)
         }
-
-        console.log(`\nChecking azd version: ${cp.execSync(azdVersion).toString()}`)
     } catch (err: any) {
         task.setResult(task.TaskResult.Failed, err.message);
     }

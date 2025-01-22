36235fae0f967321e39d0c453444ec570e89a02e
Wallace Breza <wallace.breza@microsoft.com>
2025-01-22 11:51:36 -0800

    Adds additional comments / docs for the registry and child components

--- cli/azd/pkg/extensions/registry.go
+++ cli/azd/pkg/extensions/registry.go
@@ -10,48 +10,77 @@ type ExtensionExample struct {
 
 // Registry represents the registry.json structure
 type Registry struct {
+	// Extensions is a list of extensions in the registry
 	Extensions []*ExtensionMetadata `json:"extensions"`
-	Signature  string               `json:"signature,omitempty"`
+	// Signature is used to validate the integrity of a registry source
+	Signature string `json:"signature,omitempty"`
 }
 
 // Extension represents an extension in the registry
 type ExtensionMetadata struct {
-	Id          string                       `json:"id"`
-	Namespace   string                       `json:"namespace,omitempty"`
-	DisplayName string                       `json:"displayName"`
-	Description string                       `json:"description"`
-	Versions    []ExtensionVersion           `json:"versions"`
-	Source      string                       `json:"source,omitempty"`
-	Tags        []string                     `json:"tags,omitempty"`
-	Platforms   map[string]map[string]string `json:"platforms,omitempty"`
+	// Id is a unique identifier for the extension
+	Id string `json:"id"`
+	// Namespace is used to expose extension commands within a named group
+	Namespace string `json:"namespace,omitempty"`
+	// DisplayName is the name of the extension
+	DisplayName string `json:"displayName"`
+	// Description is a brief description of the extension
+	Description string `json:"description"`
+	// Versions is a list of versions of the extension that are released over time.
+	Versions []ExtensionVersion `json:"versions"`
+	// Source is used to store the extension source from where the extension is fetched
+	Source string `json:"source,omitempty"`
+	// Tags is a list of tags that can be used to filter extensions
+	Tags []string `json:"tags,omitempty"`
+	// Platforms is a map of platform specific metadata required for extensions
+	Platforms map[string]map[string]any `json:"platforms,omitempty"`
 }
 
 // ExtensionDependency represents a dependency of an extension
 type ExtensionDependency struct {
-	Id      string `json:"id"`
+	// Id is the unique identifier of the dependent extension
+	Id string `json:"id"`
+	// Version is the version of the dependent extension and supports semantic versioning expressions.
 	Version string `json:"version,omitempty"`
 }
 
 // ExtensionVersion represents a version of an extension
 type ExtensionVersion struct {
-	Version      string                       `json:"version"`
-	Usage        string                       `json:"usage"`
-	Examples     []ExtensionExample           `json:"examples"`
-	Artifacts    map[string]ExtensionArtifact `json:"artifacts,omitempty"`
-	Dependencies []ExtensionDependency        `json:"dependencies,omitempty"`
-	EntryPoint   string                       `json:"entryPoint,omitempty"`
+	// Version is the version of the extension
+	Version string `json:"version"`
+	// Usage is show how to use the extension
+	Usage string `json:"usage"`
+	// Examples is a list of examples for the extension
+	Examples []ExtensionExample `json:"examples"`
+	// Artifacts is a map of artifacts for the extension key on platform (os & architecture)
+	Artifacts map[string]ExtensionArtifact `json:"artifacts,omitempty"`
+	// Dependencies is a list of dependencies for the extension
+	// An extension with dependencies and no artifacts is considered an extension pack.
+	// The dependencies are resolved and installed when the extension pack is installed.
+	Dependencies []ExtensionDependency `json:"dependencies,omitempty"`
+	// Entry point is the entry point for the extension
+	// This will typically be the name of the executable or script to run
+	EntryPoint string `json:"entryPoint,omitempty"`
 }
 
 // ExtensionArtifact represents the artifact information of an extension
+// An artifact can be a URL to a single binary file or a zip archive.
 type ExtensionArtifact struct {
-	URL                string            `json:"url"`
-	Checksum           ExtensionChecksum `json:"checksum"`
-	AdditionalMetadata map[string]any    `json:"-"`
+	// URL is the location of the artifact
+	URL string `json:"url"`
+	// Checksum is the checksum of the artifact
+	Checksum ExtensionChecksum `json:"checksum"`
+	// AdditionalMetadata is a map of additional metadata for the artifact
+	AdditionalMetadata map[string]any `json:"-"`
 }
 
+// ExtensionChecksum represents the checksum of an extension artifact used to validate the integrity of the artifact.
 type ExtensionChecksum struct {
+	// Algorithm is the algorithm used to calculate the checksum
+	// Examples: sha256, sha512
 	Algorithm string `json:"algorithm"`
-	Value     string `json:"value"`
+	// Value is the checksum value to match during the integrity check.
+	Value string `json:"value"`
 }
 
 func (c ExtensionArtifact) MarshalJSON() ([]byte, error) {

19050c819eed7ace7ca66d445e150b8dc2f6e717
rujche <rujche@microsoft.com>
2025-01-24 22:18:31 +0800

    Fix lint error.

--- cli/azd/internal/appdetect/java_test.go
+++ cli/azd/internal/appdetect/java_test.go
@@ -176,8 +176,7 @@ func TestToMavenProject(t *testing.T) {
 				{
 					pomFilePath: "pom.xml",
 					pomContentString: `
-						<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-							xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+						<project>
 							<modelVersion>4.0.0</modelVersion>
 							<groupId>org.springframework</groupId>
 							<artifactId>gs-multi-module</artifactId>
@@ -193,8 +192,7 @@ func TestToMavenProject(t *testing.T) {
 				{
 					pomFilePath: filepath.Join("application", "pom.xml"),
 					pomContentString: `
-						<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-							xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+						<project>
 							<modelVersion>4.0.0</modelVersion>
 							<parent>
 								<groupId>org.springframework.boot</groupId>
@@ -223,8 +221,7 @@ func TestToMavenProject(t *testing.T) {
 				{
 					pomFilePath: filepath.Join("library", "pom.xml"),
 					pomContentString: `
-						<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-							xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+						<project>
 							<modelVersion>4.0.0</modelVersion>
 							<parent>
 								<groupId>org.springframework.boot</groupId>

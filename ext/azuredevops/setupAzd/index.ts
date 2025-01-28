274cf21a7030e339f228db9f0b6b0f0de98ed004
hemarina <hemarina@microsoft.com>
2025-01-28 14:52:53 -0800

    apply suggestions

--- ext/azuredevops/setupAzd/index.ts
+++ ext/azuredevops/setupAzd/index.ts
@@ -1,18 +1,18 @@
-import * as task from 'azure-pipelines-task-lib/task';
+import * as task from 'azure-pipelines-task-lib/task'
 import * as cp from 'child_process'
 
 export async function runMain(): Promise<void> {
     try {
-        task.setTaskVariable('hasRunMain', 'true');
+        task.setTaskVariable('hasRunMain', 'true')
         const os = process.platform
         const localAppData = process.env.LocalAppData
         const envPath = process.env.PATH
         if (os === 'win32' && !localAppData) {
-            task.setResult(task.TaskResult.Failed, 'LocalAppData environment variable is not defined.');
+            task.setResult(task.TaskResult.Failed, 'LocalAppData environment variable is not defined.')
             return
         }
         if (!envPath) {
-            task.setResult(task.TaskResult.Failed, 'PATH environment variable is not defined.');
+            task.setResult(task.TaskResult.Failed, 'PATH environment variable is not defined.')
             return
         }
         const version = task.getInput('version') || 'latest'
@@ -23,7 +23,7 @@ export async function runMain(): Promise<void> {
 
         if (os === 'win32') {
             console.log(cp.execSync(windowsInstallScript).toString())
-            
+
             // Add azd to PATH
             task.setVariable('PATH', `${envPath};${localAppData}\\Programs\\Azure Dev CLI`)
         } else {
@@ -36,8 +36,8 @@ export async function runMain(): Promise<void> {
             cp.execSync(azdVersion)
         }
     } catch (err: any) {
-        task.setResult(task.TaskResult.Failed, err.message);
+        task.setResult(task.TaskResult.Failed, err.message)
     }
 }
 
-runMain();
+runMain()

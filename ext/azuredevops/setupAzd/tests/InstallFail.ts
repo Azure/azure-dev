7090b4353c83e045f36a5bdb6a5ff6884fd18b32
hemarina <hemarina@microsoft.com>
2025-01-28 15:12:01 -0800

    revert it back

--- ext/azuredevops/setupAzd/tests/InstallFail.ts
+++ ext/azuredevops/setupAzd/tests/InstallFail.ts
@@ -1,18 +1,17 @@
 import * as path from 'path';
 import * as assert from 'assert';
 import * as ttm from 'azure-pipelines-task-lib/mock-test';
-import * as fs from 'fs'
-import { log } from 'console';
+import * as fs from 'fs';
 
 describe('setup azd tests - fails', function() {
-    setTimeout(() => { }, 90000);
+    setTimeout(() => { }, 60000);
     before(function() { });
     afterEach(() => {
         fs.rmSync('path', { recursive: true, force: true })
     });
 
     it('should fail with invalid version', function(done: Mocha.Done) {
-        setTimeout(() => { }, 40000);
+        setTimeout(() => { }, 10000);
         let tp = path.join(__dirname, 'invalidVersion.js');
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 

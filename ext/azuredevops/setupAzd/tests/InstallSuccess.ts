a43dab4564faebc7a1b300d84a3a3a470c279927
hemarina <hemarina@microsoft.com>
2025-01-28 16:10:01 -0800

    apply the same for success

--- ext/azuredevops/setupAzd/tests/InstallSuccess.ts
+++ ext/azuredevops/setupAzd/tests/InstallSuccess.ts
@@ -4,14 +4,14 @@ import * as ttm from 'azure-pipelines-task-lib/mock-test';
 import * as fs from 'fs';
 
 describe('setup azd tests', function() {
-    setTimeout(() => { }, 10000);
+    this.timeout(60000);
     before(function() { });
     afterEach(() => {
         fs.rmSync('path', { recursive: true, force: true })
     });
 
     it('should succeed with undefined version', function(done: Mocha.Done) {
-        setTimeout(() => { }, 10000);
+        this.timeout(10000);
         let tp = path.join(__dirname, 'success.js');
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
         tr.runAsync().then(() => {

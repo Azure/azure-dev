14ab5e1939e5fe07a0dad594dc616a5b8a1c0ba7
Victor Vazquez <vhvb1989@gmail.com>
2025-02-04 00:14:01 +0000

    lint

--- cli/azd/pkg/infra/provisioning/bicep/prompt.go
+++ cli/azd/pkg/infra/provisioning/bicep/prompt.go
@@ -117,7 +117,7 @@ func (p *BicepProvider) promptForParameter(
 		*azdMetadata.Type == azure.AzdMetadataTypeResourceGroup {
 
 		p.console.Message(ctx, fmt.Sprintf(
-			"Parameter %s requires an %s resource group.", output.WithUnderline(key), output.WithBold("existing")))
+			"Parameter %s requires an %s resource group.", output.WithUnderline("%s", key), output.WithBold("existing")))
 		rgName, err := p.prompters.PromptResourceGroup(ctx, prompt.PromptResourceOptions{
 			DisableCreateNew: true,
 		})

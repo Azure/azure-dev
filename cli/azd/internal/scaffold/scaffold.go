d468da5a71dc8db63375f1f872fdd8ea2dec682e
Xiaolu Dai <xiada@microsoft.com>
2025-02-17 23:29:32 +0800

    address comments

--- cli/azd/internal/scaffold/scaffold.go
+++ cli/azd/internal/scaffold/scaffold.go
@@ -205,7 +205,7 @@ func preExecExpand(spec *InfraSpec) {
 		spec.Parameters = append(spec.Parameters,
 			Parameter{
 				Name:   "databasePassword",
-				Value:  "$(secretOrRandomPassword ${AZURE_KEY_VAULT_NAME} databasePassword)",
+				Value:  "$(secretOrRandomPassword ${AZURE_KEY_VAULT_NAME} db-pass)",
 				Type:   "string",
 				Secret: true,
 			})
@@ -214,7 +214,7 @@ func preExecExpand(spec *InfraSpec) {
 		spec.Parameters = append(spec.Parameters,
 			Parameter{
 				Name:   "mysqlDatabasePassword",
-				Value:  "$(secretOrRandomPassword ${AZURE_KEY_VAULT_NAME} mysqlDatabasePassword)",
+				Value:  "$(secretOrRandomPassword ${AZURE_KEY_VAULT_NAME} mysql-password)",
 				Type:   "string",
 				Secret: true,
 			})

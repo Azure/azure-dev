2442f56ad76154ba13932429ed6e0867fd8af545
Wallace Breza <wallace.breza@microsoft.com>
2025-03-06 17:29:35 -0800

    Adds questions & resource selection prompts

---
+++ cli/azd/extensions/microsoft.azd.ai.builder/internal/pkg/qna/prompt.go
@@ -0,0 +1,167 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package qna
+
+import (
+	"context"
+
+	"github.com/azure/azure-dev/cli/azd/pkg/azdext"
+)
+
+type TextPrompt struct {
+	Message string
+	Client  *azdext.AzdClient
+}
+
+func (p *TextPrompt) Ask(ctx context.Context, question Question) (any, error) {
+	promptResponse, err := p.Client.Prompt().Prompt(ctx, &azdext.PromptRequest{
+		Options: &azdext.PromptOptions{
+			Message:  p.Message,
+			Required: true,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if stringPtr, ok := question.Binding.(*string); ok {
+		*stringPtr = promptResponse.Value
+	}
+
+	return promptResponse.Value, nil
+}
+
+type SingleSelectPrompt struct {
+	Message         string
+	Choices         []Choice
+	EnableFiltering *bool
+	Client          *azdext.AzdClient
+}
+
+func (p *SingleSelectPrompt) Ask(ctx context.Context, question Question) (any, error) {
+	choices := make([]*azdext.SelectChoice, len(p.Choices))
+	for i, choice := range p.Choices {
+		choices[i] = &azdext.SelectChoice{
+			Label: choice.Label,
+			Value: choice.Value,
+		}
+	}
+
+	selectResponse, err := p.Client.Prompt().Select(ctx, &azdext.SelectRequest{
+		Options: &azdext.SelectOptions{
+			Message:         p.Message,
+			Choices:         choices,
+			EnableFiltering: p.EnableFiltering,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	selectedChoice := p.Choices[*selectResponse.Value]
+
+	if stringPtr, ok := question.Binding.(*string); ok {
+		*stringPtr = selectedChoice.Value
+	}
+
+	return selectedChoice.Value, nil
+}
+
+type MultiSelectPrompt struct {
+	Message         string
+	Choices         []Choice
+	EnableFiltering *bool
+	Client          *azdext.AzdClient
+}
+
+func (p *MultiSelectPrompt) Ask(ctx context.Context, question Question) (any, error) {
+	choices := make([]*azdext.MultiSelectChoice, len(p.Choices))
+	for i, choice := range p.Choices {
+		choices[i] = &azdext.MultiSelectChoice{
+			Label: choice.Label,
+			Value: choice.Value,
+		}
+	}
+
+	selectResponse, err := p.Client.Prompt().MultiSelect(ctx, &azdext.MultiSelectRequest{
+		Options: &azdext.MultiSelectOptions{
+			Message:         p.Message,
+			Choices:         choices,
+			EnableFiltering: p.EnableFiltering,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	selectedChoices := make([]string, len(selectResponse.Values))
+	for i, value := range selectResponse.Values {
+		selectedChoices[i] = value.Value
+	}
+
+	if stringPtr, ok := question.Binding.(*[]string); ok {
+		*stringPtr = selectedChoices
+	}
+
+	return selectedChoices, nil
+}
+
+type ConfirmPrompt struct {
+	Message      string
+	DefaultValue *bool
+	Client       *azdext.AzdClient
+}
+
+func (p *ConfirmPrompt) Ask(ctx context.Context, question Question) (any, error) {
+	confirmResponse, err := p.Client.Prompt().Confirm(ctx, &azdext.ConfirmRequest{
+		Options: &azdext.ConfirmOptions{
+			Message:      p.Message,
+			DefaultValue: p.DefaultValue,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if boolPtr, ok := question.Binding.(*bool); ok {
+		*boolPtr = *confirmResponse.Value
+	}
+
+	return *confirmResponse.Value, nil
+}
+
+type SubscriptionResourcePrompt struct {
+	ResourceType            string
+	ResourceTypeDisplayName string
+	Kinds                   []string
+	AzureContext            *azdext.AzureContext
+	Client                  *azdext.AzdClient
+	BeforeAsk               func(ctx context.Context, q *Question, p *SubscriptionResourcePrompt) error
+}
+
+func (p *SubscriptionResourcePrompt) Ask(ctx context.Context, question Question) (any, error) {
+	if p.BeforeAsk != nil {
+		if err := p.BeforeAsk(ctx, &question, p); err != nil {
+			return nil, err
+		}
+	}
+
+	resourceResponse, err := p.Client.Prompt().PromptSubscriptionResource(ctx, &azdext.PromptSubscriptionResourceRequest{
+		Options: &azdext.PromptResourceOptions{
+			ResourceType:            p.ResourceType,
+			Kinds:                   p.Kinds,
+			ResourceTypeDisplayName: p.ResourceTypeDisplayName,
+		},
+		AzureContext: p.AzureContext,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if stringPtr, ok := question.Binding.(*string); ok {
+		*stringPtr = resourceResponse.Resource.Id
+	}
+
+	return resourceResponse.Resource.Id, nil
+}

da604f9841cb47db3369862fc93e3d9048a77e8a
Wallace Breza <wallace.breza@microsoft.com>
2025-03-11 18:45:54 -0700

    wip: model selection

--- cli/azd/extensions/microsoft.azd.ai.builder/internal/pkg/qna/decision_tree.go
+++ cli/azd/extensions/microsoft.azd.ai.builder/internal/pkg/qna/decision_tree.go
@@ -9,25 +9,18 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/azure/azure-dev/cli/azd/pkg/azdext"
+	"dario.cat/mergo"
 	"github.com/fatih/color"
 )
 
 // DecisionTree represents the entire decision tree structure.
 type DecisionTree struct {
-	azdClient *azdext.AzdClient
-	config    DecisionTreeConfig
+	questions map[string]Question
 }
 
-type DecisionTreeConfig struct {
-	Questions map[string]Question `json:"questions"`
-	End       EndNode             `json:"end"`
-}
-
-func NewDecisionTree(azdClient *azdext.AzdClient, config DecisionTreeConfig) *DecisionTree {
+func NewDecisionTree(questions map[string]Question) *DecisionTree {
 	return &DecisionTree{
-		azdClient: azdClient,
-		config:    config,
+		questions: questions,
 	}
 }
 
@@ -37,19 +30,22 @@ type Prompt interface {
 
 // Question represents a single prompt in the decision tree.
 type Question struct {
-	ID        string         `json:"id"`
-	Branches  map[any]string `json:"branches"`
-	Next      string         `json:"next"`
-	Binding   any            `json:"-"`
-	Heading   string         `json:"heading,omitempty"`
-	Help      string         `json:"help,omitempty"`
+	Branches  map[any]string      `json:"branches"`
+	Next      []QuestionReference `json:"next"`
+	Binding   any                 `json:"-"`
+	Heading   string              `json:"heading,omitempty"`
+	Help      string              `json:"help,omitempty"`
 	Message   string
 	Prompt    Prompt `json:"prompt,omitempty"`
 	State     map[string]any
-	BeforeAsk func(ctx context.Context, question *Question) error
+	BeforeAsk func(ctx context.Context, question *Question, value any) error
 	AfterAsk  func(ctx context.Context, question *Question, value any) error
 }
 
+type QuestionReference struct {
+	Key   string `json:"id"`
+	State map[string]any
+}
 type Choice struct {
 	Label string `json:"label"`
 	Value string `json:"value"`
@@ -70,20 +66,19 @@ const (
 	MultiSelect  QuestionType = "multi_select"
 )
 
+// Run iterates through the decision tree and asks questions based on the user's responses.
+// It starts from the root question and continues until it reaches an end node.
 func (t *DecisionTree) Run(ctx context.Context) error {
-	rootQuestion, has := t.config.Questions["root"]
+	rootQuestion, has := t.questions["root"]
 	if !has {
 		return errors.New("root question not found")
 	}
 
-	return t.askQuestion(ctx, rootQuestion)
+	return t.askQuestion(ctx, rootQuestion, nil)
 }
 
-func (t *DecisionTree) askQuestion(ctx context.Context, question Question) error {
-	if question.Prompt == nil {
-		return errors.New("question prompt is nil")
-	}
-
+// askQuestion recursively asks questions based on the decision tree structure.
+func (t *DecisionTree) askQuestion(ctx context.Context, question Question, value any) error {
 	if question.State == nil {
 		question.State = map[string]any{}
 	}
@@ -99,84 +94,138 @@ func (t *DecisionTree) askQuestion(ctx context.Context, question Question) error
 	}
 
 	if question.BeforeAsk != nil {
-		if err := question.BeforeAsk(ctx, &question); err != nil {
+		if err := question.BeforeAsk(ctx, &question, value); err != nil {
 			return fmt.Errorf("before ask function failed: %w", err)
 		}
 	}
 
-	value, err := question.Prompt.Ask(ctx, question)
-	if err != nil {
-		return fmt.Errorf("failed to ask question: %w", err)
+	var response any
+	var err error
+
+	if question.Prompt != nil {
+		response, err = question.Prompt.Ask(ctx, question)
+		if err != nil {
+			return fmt.Errorf("failed to ask question: %w", err)
+		}
 	}
 
-	var nextQuestionKey string
+	if question.AfterAsk != nil {
+		if err := question.AfterAsk(ctx, &question, response); err != nil {
+			return fmt.Errorf("after ask function failed: %w", err)
+		}
+	}
+
+	t.applyBinding(question, response)
 
 	// Handle the case where the branch is based on the user's response
 	if len(question.Branches) > 0 {
-		switch v := value.(type) {
+		selectionValues := []any{}
+
+		switch result := response.(type) {
 		case string:
-			if branch, has := question.Branches[v]; has {
-				nextQuestionKey = branch
-			}
-
-			if question.AfterAsk != nil {
-				if err := question.AfterAsk(ctx, &question, v); err != nil {
-					return fmt.Errorf("after ask function failed: %w", err)
-				}
-			}
+			selectionValues = append(selectionValues, result)
 		case bool:
-			if branch, has := question.Branches[v]; has {
-				nextQuestionKey = branch
-			}
-
-			if question.AfterAsk != nil {
-				if err := question.AfterAsk(ctx, &question, v); err != nil {
-					return fmt.Errorf("after ask function failed: %w", err)
-				}
-			}
+			selectionValues = append(selectionValues, result)
 		case []string:
-			// Handle multi-select case
-			for _, selectedValue := range v {
-				if question.AfterAsk != nil {
-					if err := question.AfterAsk(ctx, &question, selectedValue); err != nil {
-						return fmt.Errorf("after ask function failed: %w", err)
-					}
-				}
-
-				branch, has := question.Branches[selectedValue]
-				if !has {
-					log.Printf("branch not found for selected value: %s\n", selectedValue)
-					continue
-				}
-
-				nextQuestion, has := t.config.Questions[branch]
-				if !has {
-					return fmt.Errorf("question not found for branch: %s", branch)
-				}
-
-				nextQuestion.State = question.State
-				if err = t.askQuestion(ctx, nextQuestion); err != nil {
-					return fmt.Errorf("failed to ask question: %w", err)
-				}
+			for _, selectedValue := range result {
+				selectionValues = append(selectionValues, selectedValue)
 			}
 		default:
 			return errors.New("unsupported value type")
 		}
+
+		// We need to process all the question branches from the selected values
+		// Iterate through the selected values and find the corresponding branches
+		for _, selectedValue := range selectionValues {
+			branch, has := question.Branches[selectedValue]
+			if !has {
+				log.Printf("branch not found for selected value: %s\n", selectedValue)
+				continue
+			}
+
+			nextQuestion, has := t.questions[branch]
+			if !has {
+				return fmt.Errorf("question not found for branch: %s", branch)
+			}
+
+			nextQuestion.State = question.State
+			if err = t.askQuestion(ctx, nextQuestion, selectedValue); err != nil {
+				return fmt.Errorf("failed to ask question: %w", err)
+			}
+
+			mergo.Merge(&question.State, nextQuestion.State, mergo.WithOverride)
+		}
 	}
 
-	if nextQuestionKey == "" {
-		nextQuestionKey = question.Next
-	}
-
-	if nextQuestionKey == "" || nextQuestionKey == "end" {
+	// After processing branches, we need to check if there is a next question
+	if len(question.Next) == 0 {
 		return nil
 	}
 
-	nextQuestion, has := t.config.Questions[nextQuestionKey]
-	if !has {
-		return fmt.Errorf("next question not found: %s", nextQuestionKey)
+	for _, nextQuestionRef := range question.Next {
+		nextQuestion, has := t.questions[nextQuestionRef.Key]
+		if !has {
+			return fmt.Errorf("next question not found: %s", nextQuestionRef.Key)
+		}
+
+		nextQuestion.State = question.State
+		if nextQuestionRef.State != nil {
+			mergo.Merge(&nextQuestion.State, nextQuestionRef.State, mergo.WithOverride)
+		}
+
+		if err = t.askQuestion(ctx, nextQuestion, response); err != nil {
+			return fmt.Errorf("failed to ask next question: %w", err)
+		}
+
+		mergo.Merge(&question.State, nextQuestion.State, mergo.WithOverride)
 	}
 
-	nextQuestion.State = question.State
-	return t.askQuestion(ctx, nextQuestion)
+	return nil
+}
+
+// applyBinding applies the value to the binding if it exists.
+func (t *DecisionTree) applyBinding(question Question, value any) {
+	if question.Binding == nil {
+		return
+	}
+
+	switch binding := question.Binding.(type) {
+	case *bool:
+		if boolValue, ok := value.(bool); ok {
+			*binding = boolValue
+		}
+	case *int:
+		if intValue, ok := value.(int); ok {
+			*binding = intValue
+		}
+	case *float64:
+		if floatValue, ok := value.(float64); ok {
+			*binding = floatValue
+		}
+	case *int64:
+		if int64Value, ok := value.(int64); ok {
+			*binding = int64Value
+		}
+	case *float32:
+		if float32Value, ok := value.(float32); ok {
+			*binding = float32Value
+		}
+	case *int32:
+		if int32Value, ok := value.(int32); ok {
+			*binding = int32Value
+		}
+	case *string:
+		if strValue, ok := value.(string); ok {
+			*binding = strValue
+		}
+	case *[]string:
+		if strSliceValue, ok := value.([]string); ok {
+			*binding = append(*binding, strSliceValue...)
+		}
+		if strValue, ok := value.(string); ok {
+			*binding = append(*binding, strValue)
+		}
+	default:
+		log.Printf("unsupported binding type: %T\n", binding)
+	}
 }

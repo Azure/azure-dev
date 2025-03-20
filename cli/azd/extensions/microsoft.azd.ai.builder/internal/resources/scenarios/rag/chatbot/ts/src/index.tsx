3f788adb3e9a5112fcf98a2852f395e9b5e38597
Wallace Breza <wallace.breza@microsoft.com>
2025-03-20 13:21:25 -0700

    Updates code samples for extension

---
+++ cli/azd/extensions/microsoft.azd.ai.builder/internal/resources/scenarios/rag/chatbot/ts/src/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import { I18nextProvider } from "react-i18next";
+import { HelmetProvider } from "react-helmet-async";
+import { initializeIcons } from "@fluentui/react";
+
+import "./index.css";
+
+import Chat from "./pages/chat/Chat";
+import LayoutWrapper from "./layoutWrapper";
+import i18next from "./i18n/config";
+
+initializeIcons();
+
+const router = createHashRouter([
+    {
+        path: "/",
+        element: <LayoutWrapper />,
+        children: [
+            {
+                index: true,
+                element: <Chat />
+            },
+            {
+                path: "qa",
+                lazy: () => import("./pages/ask/Ask")
+            },
+            {
+                path: "*",
+                lazy: () => import("./pages/NoPage")
+            }
+        ]
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <I18nextProvider i18n={i18next}>
+            <HelmetProvider>
+                <RouterProvider router={router} />
+            </HelmetProvider>
+        </I18nextProvider>
+    </React.StrictMode>
+);

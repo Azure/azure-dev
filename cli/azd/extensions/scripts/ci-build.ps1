c5b02e013112612407f80f40e50f41a6cd09d769
Wallace Breza <wallace.breza@microsoft.com>
2025-04-21 15:55:47 -0700

    WIP: Adding azd extension pipeline and jobs

---
+++ cli/azd/extensions/scripts/ci-build.ps1
@@ -0,0 +1,185 @@
+param(
+    [string] $Version = (Get-Content "$PSScriptRoot/../version.txt"),
+    [string] $SourceVersion = (git rev-parse HEAD),
+    [switch] $CodeCoverageEnabled,
+    [switch] $BuildRecordMode,
+    [string] $MSYS2Shell # path to msys2_shell.cmd
+)
+$PSNativeCommandArgumentPassing = 'Legacy'
+
+# Remove any previously built binaries
+go clean
+
+if ($LASTEXITCODE) {
+    Write-Host "Error running go clean"
+    exit $LASTEXITCODE
+}
+
+# On Windows, use the goversioninfo tool to embed the version information into the executable.
+if ($IsWindows) {
+    Write-Host "Windows build, set version info and run 'go generate'"
+    if (! (Get-Command "goversioninfo" -ErrorAction SilentlyContinue)) {
+        Write-Host "goversioninfo not found, installing"
+        go install github.com/josephspurrier/goversioninfo/cmd/goversioninfo@v1.4.0
+
+        try {
+            Get-Command "goversioninfo" -ErrorAction Stop
+        }
+        catch {
+            Write-Host "Could not find goversioninfo after installing"
+            Write-Host "Environment PATH: $env:PATH"
+            Get-ChildItem -Path (Join-Path (go env GOPATH) "bin") | ForEach-Object { Write-Host $_.FullName }
+        }
+    }
+
+    $VERSION_INFO_PATH = "$PSScriptRoot/versioninfo.json"
+
+    $exeFileVersion = ."$PSScriptRoot/../../eng/scripts/Get-MsiVersion.ps1" -CliVersion $Version
+    $splitExeFileVersion = $exeFileVersion -split '\.'
+    $versionInfo = Get-Content $VERSION_INFO_PATH | ConvertFrom-Json
+
+    $versionInfo.FixedFileInfo.FileVersion.Major = [int]$splitExeFileVersion[0]
+    $versionInfo.FixedFileInfo.FileVersion.Minor = [int]$splitExeFileVersion[1]
+    $versionInfo.FixedFileInfo.FileVersion.Patch = [int]$splitExeFileVersion[2]
+    $versionInfo.FixedFileInfo.FileVersion.Build = 0
+
+    # Product verison is the same as the file version
+    $versioninfo.FixedFileInfo.ProductVersion = $versionInfo.FixedFileInfo.FileVersion
+
+    $versionInfo.StringFileInfo.ProductVersion = $Version
+
+    $versionInfoJson = ConvertTo-Json $versionInfo -Depth 10
+    Set-Content $VERSION_INFO_PATH $versionInfoJson
+    Write-Host "go generate"
+    go generate
+    if ($LASTEXITCODE) {
+        Write-Host "Error running go generate"
+        exit $LASTEXITCODE
+    }
+    Write-Host "go generate succeeded"
+}
+
+# Run `go help build` to obtain detailed information about `go build` flags.
+$buildFlags = @(
+    # remove all file system paths from the resulting executable.
+    # Instead of absolute file system paths, the recorded file names
+    # will begin either a module path@version (when using modules),
+    # or a plain import path (when using the standard library, or GOPATH).
+    "-trimpath",
+
+    # Use buildmode=pie (Position Independent Executable) for enhanced security across platforms
+    # against memory corruption exploits across all major platforms.
+    #
+    # On Windows, the -buildmode=pie flag enables Address Space Layout 
+    # Randomization (ASLR) and automatically sets DYNAMICBASE and HIGH-ENTROPY-VA flags in the PE header.
+    "-buildmode=pie"
+)
+
+if ($CodeCoverageEnabled) {
+    $buildFlags += "-cover"
+}
+
+# Build constraint tags
+# cfi: Enable Control Flow Integrity (CFI),
+# cfg: Enable Control Flow Guard (CFG),
+# osusergo: Optimize for OS user accounts
+$tagsFlag = "-tags=cfi,cfg,osusergo"
+
+# ld linker flags
+# -s: Omit symbol table and debug information
+# -w: Omit DWARF symbol table
+# -X: Set variable at link time. Used to set the version in source.
+$ldFlag = "-ldflags=-s -w -X 'github.com/azure/azure-dev/cli/azd/internal.Version=$Version (commit $SourceVersion)' "
+
+if ($IsWindows) {
+    $msg = "Building for Windows"
+    Write-Host $msg
+}
+elseif ($IsLinux) {
+    Write-Host "Building for linux"
+}
+elseif ($IsMacOS) {
+    Write-Host "Building for macOS"
+}
+
+# collect flags
+$buildFlags += @(
+    $tagsFlag,
+    $ldFlag
+)
+
+function PrintFlags() {
+    param(
+        [string] $flags
+    )
+
+    # Attempt to format flags so that they are easily copy-pastable to be ran inside pwsh
+    $i = 0
+    foreach ($buildFlag in $buildFlags) {
+        # If the flag has a value, wrap it in quotes. This is not required when invoking directly below,
+        # but when repasted into a shell for execution, the quotes can help escape special characters such as ','.
+        $argWithValue = $buildFlag.Split('=', 2)
+        if ($argWithValue.Length -eq 2 -and !$argWithValue[1].StartsWith("`"")) {
+            $buildFlag = "$($argWithValue[0])=`"$($argWithValue[1])`""
+        }
+
+        # Write each flag on a newline with '`' acting as the multiline separator
+        if ($i -eq $buildFlags.Length - 1) {
+            Write-Host "  $buildFlag"
+        }
+        else {
+            Write-Host "  $buildFlag ``"
+        }
+        $i++
+    }
+}
+
+$oldGOEXPERIMENT = $env:GOEXPERIMENT
+# Enable the loopvar experiment, which makes the loop variaible for go loops like `range` behave as most folks would expect.
+# the go team is exploring making this default in the future, and we'd like to opt into the behavior now.
+$env:GOEXPERIMENT = "loopvar"
+
+try {
+    Write-Host "Running: go build ``"
+    PrintFlags -flags $buildFlags
+    go build @buildFlags
+    if ($BuildRecordMode) {
+        $recordFlagPresent = $false
+        for ($i = 0; $i -lt $buildFlags.Length; $i++) {
+            if ($buildFlags[$i].StartsWith("-tags=")) {
+                $recordFlagPresent = $true
+                $buildFlags[$i] += ",record"
+            }
+        }
+
+        if (-not $recordFlagPresent) {
+            $buildFlags[$i] += "-tags=record"
+        }
+
+        $outputFlag = "-o=azd-record"
+        if ($IsWindows) {
+            $outputFlag += ".exe"
+        }
+        $buildFlags += $outputFlag
+
+        Write-Host "Running: go build (record) ``"
+        PrintFlags -flags $buildFlags
+        go build @buildFlags
+    }
+
+    if ($LASTEXITCODE) {
+        Write-Host "Error running go build"
+        exit $LASTEXITCODE
+    }
+    Write-Host "go build succeeded"
+
+    if ($IsWindows) {
+        Write-Host "Windows exe file version info"
+        $azdExe = Get-Item azd.exe
+        Write-Host "File Version: $($azdExe.VersionInfo.FileVersionRaw)"
+        Write-Host "Product Version: $($azdExe.VersionInfo.ProductVersionRaw)"
+    }
+}
+finally {
+    $env:GOEXPERIMENT = $oldGOEXPERIMENT
+}
---
+++ cli/azd/extensions/scripts/ci-test.ps1
@@ -0,0 +1,70 @@
+param(
+    [switch] $ShortMode,
+    [string] $UnitTestCoverageDir = 'cover-unit',
+    [string] $IntegrationTestTimeout = '120m',
+    [string] $IntegrationTestCoverageDir = 'cover-int'
+)
+
+$ErrorActionPreference = 'Stop'
+
+$gopath = go env GOPATH
+if ($LASTEXITCODE) {
+    throw "go env GOPATH failed with exit code: $LASTEXITCODE, stdout: $gopath"
+}
+
+$gotestsumBinary = "gotestsum"
+if ($IsWindows) {
+    $gotestsumBinary += ".exe"
+}
+
+$gotestsum = Join-Path $gopath "bin" $gotestsumBinary
+if (-not (Test-Path $gotestsum)) {
+    throw "gotestsum is not installed at $gotestsum"
+}
+
+function New-EmptyDirectory {
+    param([string]$Path) 
+    if (Test-Path $Path) {
+        Remove-Item -Force -Recurse $Path | Out-Null
+    }
+    
+    New-Item -ItemType Directory -Force -Path $Path
+}
+$unitCoverDir = New-EmptyDirectory -Path $UnitTestCoverageDir
+Write-Host "Running unit tests..."
+
+# --test.gocoverdir is currently a "under-the-cover" way to pass the coverage directory to a test binary
+# See https://github.com/golang/go/issues/51430#issuecomment-1344711300
+#
+# This may be improved in go1.21 with an official 'go test' flag.
+& $gotestsum -- ./... -short -v -cover -args --test.gocoverdir="$($unitCoverDir.FullName)"
+if ($LASTEXITCODE) {
+    exit $LASTEXITCODE
+}
+
+if ($ShortMode) {
+    Write-Host "Short mode, skipping integration tests"
+    exit 0
+}
+
+Write-Host "Running integration tests..."
+$intCoverDir = New-EmptyDirectory -Path $IntegrationTestCoverageDir
+
+$oldGOCOVERDIR = $env:GOCOVERDIR
+$oldGOEXPERIMENT = $env:GOEXPERIMENT
+
+# GOCOVERDIR enables any binaries (in this case, azd.exe) built with '-cover',
+# to write out coverage output to the specific directory.
+$env:GOCOVERDIR = $intCoverDir.FullName
+# Set any experiment flags that are needed for the tests.
+$env:GOEXPERIMENT=""
+
+try {
+    & $gotestsum -- ./... -v -timeout $IntegrationTestTimeout
+    if ($LASTEXITCODE) {
+        exit $LASTEXITCODE
+    }    
+} finally {
+    $env:GOCOVERDIR = $oldGOCOVERDIR
+    $env:GOEXPERIMENT = $oldGOEXPERIMENT
+}

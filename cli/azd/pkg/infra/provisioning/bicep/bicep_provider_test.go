2077de96ea95466e4e034ea43d35d9204bb23a1b
Merijn Stiekema <github@mstiekema.nl>
2025-05-07 03:40:39 +0200

    Properly reading objects and arrays from parameter file (#5162)
    * Properly reading objects and arrays from parameter file
    
    * Refactor parameter loading logic to simplify type assertion and condition checks
    
    * tests
    
    ---------
    
    Co-authored-by: Victor Vazquez <vhvb1989@gmail.com>

--- cli/azd/pkg/infra/provisioning/bicep/bicep_provider_test.go
+++ cli/azd/pkg/infra/provisioning/bicep/bicep_provider_test.go
@@ -81,6 +81,33 @@ func TestBicepPlanKeyVaultRef(t *testing.T) {
 	require.Equal(t, "secretName", configuredParameters["kvSecret"].KeyVaultReference.SecretName)
 }
 
+func TestBicepPlanParameterTypes(t *testing.T) {
+	mockContext := mocks.NewMockContext(context.Background())
+	prepareBicepMocks(mockContext)
+	infraProvider := createBicepProvider(t, mockContext)
+
+	deploymentPlan, err := infraProvider.plan(*mockContext.Context)
+
+	require.Nil(t, err)
+
+	require.IsType(t, &deploymentDetails{}, deploymentPlan)
+	configuredParameters := deploymentPlan.CompiledBicep.Parameters
+
+	require.NotEmpty(t, configuredParameters["regularString"])
+	require.Equal(t, configuredParameters["regularString"].Value, "test")
+	require.Empty(t, configuredParameters["emptyString"])
+	require.Nil(t, configuredParameters["emptyString"].Value)
+
+	require.NotEmpty(t, configuredParameters["regularObject"])
+	require.Equal(t, configuredParameters["regularObject"].Value, map[string]any{"test": "test"})
+	require.Equal(t, configuredParameters["emptyObject"].Value, map[string]any{})
+
+	require.NotEmpty(t, configuredParameters["regularArray"])
+	require.Equal(t, configuredParameters["regularArray"].Value, []any{"test"})
+	require.NotEmpty(t, configuredParameters["emptyArray"])
+	require.Equal(t, configuredParameters["emptyArray"].Value, []any{})
+}
+
 const paramsArmJson = `{
 	"$schema": "https://schema.management.azure.com/schemas/2018-05-01/subscriptionDeploymentTemplate.json#",
 	"contentVersion": "1.0.0.0",
@@ -421,6 +448,12 @@ func prepareBicepMocks(
 			"environmentName": {Type: "string"},
 			"location":        {Type: "string"},
 			"kvSecret":        {Type: "securestring"},
+			"regularString":   {Type: "string", DefaultValue: ""},
+			"emptyString":     {Type: "string", DefaultValue: ""},
+			"regularObject":   {Type: "array", DefaultValue: make([]string, 0)},
+			"emptyObject":     {Type: "array", DefaultValue: make([]string, 0)},
+			"regularArray":    {Type: "object", DefaultValue: make(map[string]int)},
+			"emptyArray":      {Type: "object", DefaultValue: make(map[string]int)},
 		},
 		Outputs: azure.ArmTemplateOutputs{
 			"WEBSITE_URL": {Type: "string"},

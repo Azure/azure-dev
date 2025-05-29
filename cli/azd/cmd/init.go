e6dad031c5a9195c545c14be7cca98f89148dc04
Jeffrey Chen <jeffreychen@microsoft.com>
2025-05-29 22:47:11 +0000

    Handle azd init as minimal project

--- cli/azd/cmd/init.go
+++ cli/azd/cmd/init.go
@@ -314,54 +314,13 @@ func (i *initAction) Run(ctx context.Context) (*actions.ActionResult, error) {
 	case initProject:
 		tracing.SetUsageAttributes(fields.InitMethod.String("project"))
 
-		composeAlphaEnabled := i.featuresManager.IsEnabled(composeFeature)
-		if !composeAlphaEnabled {
-			err = i.repoInitializer.InitializeMinimal(ctx, azdCtx)
-			if err != nil {
-				return nil, err
-			}
-
-			_, err := i.initializeEnv(ctx, azdCtx, templates.Metadata{})
-			if err != nil {
-				return nil, err
-			}
-
-			followUp = ""
-		} else {
-			fi, err := os.Stat(azdCtx.ProjectPath())
-			if err != nil && !errors.Is(err, os.ErrNotExist) {
-				return nil, err
-			}
-
-			if fi != nil {
-				return nil, fmt.Errorf("project already initialized")
-			}
-
-			name, err := i.console.Prompt(ctx, input.ConsoleOptions{
-				Message:      "What is the name of your project?",
-				DefaultValue: azdcontext.ProjectName(azdCtx.ProjectDirectory()),
-			})
-			if err != nil {
-				return nil, err
-			}
-
-			prjConfig := project.ProjectConfig{
-				Name: name,
-			}
-
-			if composeAlphaEnabled {
-				prjConfig.MetaSchemaVersion = "alpha"
-			}
-
-			err = project.Save(ctx, &prjConfig, azdCtx.ProjectPath())
-			if err != nil {
-				return nil, fmt.Errorf("saving project config: %w", err)
-			}
-
-			followUp = "Run " + output.WithHighLightFormat("azd add") + " to add new Azure components to your project."
+		err = i.repoInitializer.InitializeMinimal(ctx, azdCtx)
+		if err != nil {
+			return nil, err
 		}
 
 		header = "Generated azure.yaml project file."
+		followUp = "Run " + output.WithHighLightFormat("azd add") + " to add new Azure components to your project."
 	default:
 		panic("unhandled init type")
 	}
@@ -377,8 +336,6 @@ func (i *initAction) Run(ctx context.Context) (*actions.ActionResult, error) {
 		},
 	}, nil
 }
-
-var composeFeature = alpha.MustFeatureKey("compose")
 
 type initType int
 

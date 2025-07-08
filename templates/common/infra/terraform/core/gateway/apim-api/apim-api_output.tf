14a14de53fb7ef3009def5d3950ca7844fd42b0f
Victor Vazquez <vhvb1989@gmail.com>
2025-07-08 19:40:19 +0000

    include all templates folder

--- templates/common/infra/terraform/core/gateway/apim-api/apim-api_output.tf
+++
@@ -1,3 +0,0 @@
-output "SERVICE_API_URI" {
-  value = "${data.azurerm_api_management.apim.gateway_url}/${var.api_path}"
-}
--- templates/common/infra/terraform/core/monitor/loganalytics/loganalytics_output.tf
+++
@@ -1,3 +0,0 @@
-output "LOGANALYTICS_WORKSPACE_ID" {
-  value = azurerm_log_analytics_workspace.workspace.id
-}

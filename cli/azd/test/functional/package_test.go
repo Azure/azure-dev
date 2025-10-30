3256741a96e13a418c12b2f8d14cc420d2305095
Wallace Breza <wallace.breza@microsoft.com>
2025-10-29 17:19:18 -0700

    Updates test file path generation

--- cli/azd/test/functional/package_test.go
+++ cli/azd/test/functional/package_test.go
@@ -254,7 +254,7 @@ func Test_CLI_Package_WithOutputPath(t *testing.T) {
 		)
 		require.NoError(t, err)
 		require.Contains(t, packageResult.Stdout, "Package Output:")
-		require.Contains(t, packageResult.Stdout, fmt.Sprintf("dist%cweb.zip", os.PathSeparator))
+		require.Contains(t, packageResult.Stdout, filepath.Join("dist", "web.zip"))
 
 		artifactPath := filepath.Join(dir, "dist", "web.zip")
 		info, err := os.Stat(artifactPath)

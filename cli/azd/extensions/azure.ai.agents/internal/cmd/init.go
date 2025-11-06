0154d64f1f42e4440a1ce8c4b845654cde8da6e5
Travis Angevine <trangevi@microsoft.com>
2025-11-06 13:12:13 -0800

    [AI Agents Extension] Tool parsing (#6091)
    * Add tool resource parsing for hosted
    
    Signed-off-by: trangevi <trangevi@microsoft.com>
    
    * move the handling
    
    Signed-off-by: trangevi <trangevi@microsoft.com>
    
    * Add support for prompt tools handling
    
    Signed-off-by: trangevi <trangevi@microsoft.com>
    
    * cspell
    
    Signed-off-by: trangevi <trangevi@microsoft.com>
    
    * capitalization
    
    Signed-off-by: trangevi <trangevi@microsoft.com>
    
    * cspell again
    
    Signed-off-by: trangevi <trangevi@microsoft.com>
    
    * Add check for connection id from registry
    
    Signed-off-by: trangevi <trangevi@microsoft.com>
    
    ---------
    
    Signed-off-by: trangevi <trangevi@microsoft.com>

--- cli/azd/extensions/azure.ai.agents/internal/cmd/init.go
+++ cli/azd/extensions/azure.ai.agents/internal/cmd/init.go
@@ -721,6 +721,14 @@ func (a *InitAction) downloadAgentYaml(
 		return nil, "", fmt.Errorf("failed to process manifest parameters: %w", err)
 	}
 
+	_, isPromptAgent := agentManifest.Template.(agent_yaml.PromptAgent)
+	if isPromptAgent {
+		agentManifest, err = agent_yaml.ProcessPromptAgentToolsConnections(ctx, agentManifest, a.azdClient)
+		if err != nil {
+			return nil, "", fmt.Errorf("failed to process prompt agent tools connections: %w", err)
+		}
+	}
+
 	content, err = yaml.Marshal(agentManifest)
 	if err != nil {
 		return nil, "", fmt.Errorf("marshaling agent manifest to YAML after parameter processing: %w", err)
@@ -800,6 +808,7 @@ func (a *InitAction) addToProject(ctx context.Context, targetDir string, agentMa
 	var agentConfig = project.ServiceTargetAgentConfig{}
 
 	deploymentDetails := []project.Deployment{}
+	resourceDetails := []project.Resource{}
 	switch agentDef.Kind {
 	case agent_yaml.AgentKindPrompt:
 		agentDef := agentManifest.Template.(agent_yaml.PromptAgent)
@@ -836,9 +845,39 @@ func (a *InitAction) addToProject(ctx context.Context, targetDir string, agentMa
 				deploymentDetails = append(deploymentDetails, *modelDeployment)
 			}
 		}
+
+		// Handle tool resources that require connection names
+		if agentManifest.Resources != nil {
+			for _, resource := range agentManifest.Resources {
+				// Try to cast to ToolResource
+				if toolResource, ok := resource.(agent_yaml.ToolResource); ok {
+					// Check if this is a resource that requires a connection name
+					if toolResource.Id == "bing_grounding" || toolResource.Id == "azure_ai_search" {
+						// Prompt the user for a connection name
+						resp, err := a.azdClient.Prompt().Prompt(ctx, &azdext.PromptRequest{
+							Options: &azdext.PromptOptions{
+								Message:        fmt.Sprintf("Enter connection name for %s resource:", toolResource.Id),
+								IgnoreHintKeys: true,
+							},
+						})
+						if err != nil {
+							return fmt.Errorf("prompting for connection name for %s: %w", toolResource.Id, err)
+						}
+
+						// Add to resource details
+						resourceDetails = append(resourceDetails, project.Resource{
+							Resource:       toolResource.Id,
+							ConnectionName: resp.Value,
+						})
+					}
+				}
+				// Skip the resource if the cast fails
+			}
+		}
 	}
 
 	agentConfig.Deployments = deploymentDetails
+	agentConfig.Resources = resourceDetails
 
 	var agentConfigStruct *structpb.Struct
 	if agentConfigStruct, err = project.MarshalStruct(&agentConfig); err != nil {

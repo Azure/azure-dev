c7e406474928065db9beb2c37b49124991edde19
hemarina <hemarina@microsoft.com>
2025-12-03 15:24:52 -0800

    add enhancement

--- cli/azd/cmd/middleware/error.go
+++ cli/azd/cmd/middleware/error.go
@@ -238,6 +238,7 @@ func (e *ErrorMiddleware) Run(ctx context.Context, next NextFn) (*actions.Action
             2. Resolve the error by making the minimal, targeted change required to the code or configuration.
             Avoid unnecessary modifications and focus only on what is essential to restore correct functionality.
             3. Remove any changes that were created solely for validation and are not part of the actual error fix.
+			4. Do not run azd up, azd provision, azd deploy or azd down commands. 
             Error details: %s`, errorInput))
 
 				if err != nil {
@@ -251,7 +252,9 @@ func (e *ErrorMiddleware) Run(ctx context.Context, next NextFn) (*actions.Action
 				if selectedSolution != "" {
 					// User selected a solution
 					agentOutput, err = azdAgent.SendMessage(ctx, fmt.Sprintf(
-						`Perform the following actions to resolve the error: %s
+						`Steps to follow:
+						1. Perform the following actions to resolve the error: %s.
+						2. Do not run azd up, azd provision, azd deploy or azd down commands. 
 						Error details: %s`, selectedSolution, errorInput))
 
 					if err != nil {

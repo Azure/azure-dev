3802b0da5ed7f1071819d374f97d7df97e33f2c3
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2026-02-03 14:40:13 -0500

    Fix telemetry bundling issues (#6698)
    * Update packages to fix telemetry issue
    
    * Fix lint config and lint error

--- ext/vscode/eslint.config.mjs
+++ ext/vscode/eslint.config.mjs
@@ -3,4 +3,34 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-export { azExtEslintRecommendedTypeChecked as default } from '@microsoft/vscode-azext-eng/eslint'; // Other configurations exist
+import { azExtEslintRecommendedTypeChecked } from '@microsoft/vscode-azext-eng/eslint'; // Other configurations exist
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+    azExtEslintRecommendedTypeChecked,
+    {
+        rules: {
+        'header/header': [
+            'error',
+            {
+                header: {
+                    commentType: 'line',
+                    lines: [
+                        {
+                            pattern: /Copyright.*Microsoft/,
+                            template: ' Copyright (c) Microsoft Corporation. All rights reserved.',
+                        },
+                        {
+                            pattern: /LICENSE/i,
+                            template: ' Licensed under the MIT License.',
+                        },
+                    ],
+                },
+                trailingEmptyLines: {
+                    minimum: 2,
+                },
+            },
+        ],
+    },
+    }
+]);

364a2ea41d2cfec40403710114eb20d082e9eb29
Travis Angevine <trangevi@microsoft.com>
2026-03-02 13:19:40 -0800

    Update cognitiveservices library dependency (#6957)
    Signed-off-by: trangevi <trangevi@microsoft.com>

--- cli/azd/extensions/azure.ai.agents/internal/project/parser.go
+++ cli/azd/extensions/azure.ai.agents/internal/project/parser.go
@@ -22,7 +22,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/appcontainers/armappcontainers"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/authorization/armauthorization/v3"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/cognitiveservices/armcognitiveservices"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/cognitiveservices/armcognitiveservices/v2"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
 	"github.com/azure/azure-dev/cli/azd/pkg/azdext"
 	"github.com/azure/azure-dev/cli/azd/pkg/graphsdk"

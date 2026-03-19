13f99c777682ec211be8cf14f60a971199e1381d
hemarina <104857065+hemarina@users.noreply.github.com>
2026-03-18 20:23:45 -0700

    Implement azd update to use installation script for Windows (#7166)
    * add install script installation for windows
    
    * address feedback
    
    * address feedback
    
    * address feedback, update UI, rename azd to temp and have a copy to be replaced by new azd exe
    
    * fix cspell and golang, address feedback
    
    * fix golang-run
    
    * clean up tests due to feedback

--- cli/azd/pkg/update/msi_unix.go
+++ cli/azd/pkg/update/msi_unix.go
@@ -5,15 +5,20 @@
 
 package update
 
-import "syscall"
-
-// newDetachedSysProcAttr is a no-op on non-Windows platforms.
-// updateViaMSI is only called on Windows (guarded by runtime.GOOS check in Update).
-func newDetachedSysProcAttr() *syscall.SysProcAttr {
-	return &syscall.SysProcAttr{}
+// isStandardMSIInstall is a no-op on non-Windows platforms.
+func isStandardMSIInstall() error {
+	return nil
 }
 
-// msiLogFilePath is a no-op on non-Windows platforms.
-func msiLogFilePath() (string, error) {
-	return "", nil
+// backupCurrentExe is a no-op stub on non-Windows platforms.
+func backupCurrentExe() (string, string, error) {
+	return "", "", nil
+}
+
+// restoreExeFromBackup is a no-op stub on non-Windows platforms.
+func restoreExeFromBackup(_, _ string) error { return nil }
+
+// buildInstallScriptArgs is a no-op on non-Windows platforms.
+func buildInstallScriptArgs(_ Channel) []string {
+	return nil
 }

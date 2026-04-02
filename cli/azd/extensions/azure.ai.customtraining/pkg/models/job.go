8a293c124ffa062aefd2e29046c01932933ddca3
Saanika Gupta <gsaanika@gmail.com>
2026-04-02 14:32:26 +0530

    Nit

--- cli/azd/extensions/azure.ai.customtraining/pkg/models/job.go
+++ cli/azd/extensions/azure.ai.customtraining/pkg/models/job.go
@@ -14,23 +14,23 @@ type JobResource struct {
 
 // CommandJob represents the properties of a Foundry command job.
 type CommandJob struct {
-	JobType              string                 `json:"jobType"`
-	DisplayName          string                 `json:"displayName,omitempty" table:"DISPLAY NAME"`
-	Description          string                 `json:"description,omitempty"`
-	Status               string                 `json:"status,omitempty" table:"STATUS"`
-	Command              string                 `json:"command,omitempty"`
-	EnvironmentID        string                 `json:"environmentId,omitempty"`
-	CodeID               string                 `json:"codeId,omitempty"`
-	ComputeID            string                 `json:"computeId,omitempty"`
-	Inputs               map[string]JobInput    `json:"inputs,omitempty"`
-	Outputs              map[string]JobOutput   `json:"outputs,omitempty"`
-	Distribution         *Distribution          `json:"distribution,omitempty"`
-	Resources            *ResourceConfig        `json:"resources,omitempty"`
-	Limits               *CommandJobLimits      `json:"limits,omitempty"`
-	EnvironmentVariables map[string]string      `json:"environmentVariables,omitempty"`
-	QueueSettings        *QueueSettings         `json:"queueSettings,omitempty"`
-	IsArchived           bool                   `json:"isArchived,omitempty"`
-	CreatedDateTime      string                 `json:"createdDateTime,omitempty"`
+	JobType              string                `json:"jobType"`
+	DisplayName          string                `json:"displayName,omitempty" table:"DISPLAY NAME"`
+	Description          string                `json:"description,omitempty"`
+	Status               string                `json:"status,omitempty" table:"STATUS"`
+	Command              string                `json:"command,omitempty"`
+	EnvironmentID        string                `json:"environmentId,omitempty"`
+	CodeID               string                `json:"codeId,omitempty"`
+	ComputeID            string                `json:"computeId,omitempty"`
+	Inputs               map[string]JobInput   `json:"inputs,omitempty"`
+	Outputs              map[string]JobOutput  `json:"outputs,omitempty"`
+	Distribution         *Distribution         `json:"distribution,omitempty"`
+	Resources            *ResourceConfig       `json:"resources,omitempty"`
+	Limits               *CommandJobLimits     `json:"limits,omitempty"`
+	EnvironmentVariables map[string]string     `json:"environmentVariables,omitempty"`
+	QueueSettings        *QueueSettings        `json:"queueSettings,omitempty"`
+	IsArchived           bool                  `json:"isArchived,omitempty"`
+	CreatedDateTime      string                `json:"createdDateTime,omitempty"`
 	Services             map[string]interface{} `json:"services,omitempty"`
 }
 

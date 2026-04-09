ecf1dc96ef8ed1ec1cf1154cdf5c4e60bcd3c8ef
hemarina <104857065+hemarina@users.noreply.github.com>
2026-04-09 15:48:22 -0700

    Update cli/azd/cmd/update.go
    Co-authored-by: Jon Gallant <2163001+jongio@users.noreply.github.com>

--- cli/azd/cmd/update.go
+++ cli/azd/cmd/update.go
@@ -298,9 +298,10 @@ func (a *updateAction) Run(ctx context.Context) (*actions.ActionResult, error) {
 	return &actions.ActionResult{
 		Message: &actions.ResultMessage{
 			Header: fmt.Sprintf(
-				"Updated to v%s! Run `azd --version` to confirm.",
+				"Updated azd to version %s!",
 				versionInfo.Version,
 			),
+			FollowUp: "Run `azd --version` to confirm.",
 		},
 	}, nil
 }

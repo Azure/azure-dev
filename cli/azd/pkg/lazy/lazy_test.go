b021baec09b622a5e0aab7c7a26c8cce7677915a
Yimin Jin <yiminjin@microsoft.com>
2026-08-26 16:40:48 +0800

    fix: synchronize lazy value access

--- cli/azd/pkg/lazy/lazy_test.go
+++ cli/azd/pkg/lazy/lazy_test.go
@@ -5,8 +5,8 @@ package lazy
 
 import (
 	"errors"
+	"sync/atomic"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -107,37 +107,106 @@ func Test_Lazy_SetValue(t *testing.T) {
 
 func Test_Lazy_GetValue_Concurrent(t *testing.T) {
 	expected := "test"
-	callCount := 0
+	var callCount atomic.Int32
+	initializerStarted := make(chan struct{})
+	releaseInitializer := make(chan struct{})
 
 	initFn := func() (string, error) {
-		callCount++
-		// justified: simulates a slow initializer so both goroutines are guaranteed to
-		// reach GetValue() before init completes, verifying only one runs the initFn.
-		time.Sleep(time.Millisecond * 200)
+		callCount.Add(1)
+		close(initializerStarted)
+		<-releaseInitializer
 		return expected, nil
 	}
 
 	instance := NewLazy(initFn)
 
-	var actual string
-	var err error
-
-	res := make(chan bool, 2)
-
-	go func() {
-		actual, err = instance.GetValue()
-		res <- true
-	}()
+	type result struct {
+		value string
+		err   error
+	}
+	results := make(chan result, 2)
+	gettersStarted := make(chan struct{}, 2)
 
 	go func() {
-		actual, err = instance.GetValue()
-		res <- true
+		gettersStarted <- struct{}{}
+		value, err := instance.GetValue()
+		results <- result{value: value, err: err}
 	}()
+	<-initializerStarted
 
-	done := <-res
+	go func() {
+		gettersStarted <- struct{}{}
+		value, err := instance.GetValue()
+		results <- result{value: value, err: err}
+	}()
+	<-gettersStarted
+	<-gettersStarted
+	close(releaseInitializer)
 
-	require.True(t, done)
-	require.Equal(t, expected, actual)
+	for range 2 {
+		result := <-results
+		require.Equal(t, expected, result.value)
+		require.NoError(t, result.err)
+	}
+	require.Equal(t, int32(1), callCount.Load())
+}
+
+func Test_Lazy_InitializerCanSetValue(t *testing.T) {
+	var instance *Lazy[string]
+	instance = NewLazy(func() (string, error) {
+		instance.SetValue("from setter")
+		return "from initializer", nil
+	})
+
+	value, err := instance.GetValue()
 	require.NoError(t, err)
-	require.Equal(t, 1, callCount)
+	require.Equal(t, "from setter", value)
+
+	value, err = instance.GetValue()
+	require.NoError(t, err)
+	require.Equal(t, "from setter", value)
+}
+
+func Test_Lazy_SetValueWinsOverRunningInitializer(t *testing.T) {
+	tests := []struct {
+		name           string
+		initializerErr error
+	}{
+		{name: "initializer succeeds"},
+		{name: "initializer fails", initializerErr: errors.New("initializer failed")},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			initializerStarted := make(chan struct{})
+			releaseInitializer := make(chan struct{})
+			instance := NewLazy(func() (string, error) {
+				close(initializerStarted)
+				<-releaseInitializer
+				return "from initializer", test.initializerErr
+			})
+
+			type result struct {
+				value string
+				err   error
+			}
+			resultCh := make(chan result, 1)
+			go func() {
+				value, err := instance.GetValue()
+				resultCh <- result{value: value, err: err}
+			}()
+			<-initializerStarted
+
+			instance.SetValue("from setter")
+			close(releaseInitializer)
+
+			got := <-resultCh
+			require.NoError(t, got.err)
+			require.Equal(t, "from setter", got.value)
+
+			value, err := instance.GetValue()
+			require.NoError(t, err)
+			require.Equal(t, "from setter", value)
+		})
+	}
 }

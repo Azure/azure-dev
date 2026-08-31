bb0274c95f09093748cda98f82e88bafb2974561
huimiu <miuhui@outlook.com>
2026-08-31 18:08:12 +0800

    feat: let extensions contribute command follow-up text

---
+++ cli/azd/internal/commandresult/follow_up_test.go
@@ -0,0 +1,63 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package commandresult
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestFollowUpCollector_ReplacesByExtension(t *testing.T) {
+	collector := NewFollowUpCollector()
+
+	collector.Add("test.extension", "first")
+	collector.Add("test.extension", "second")
+
+	require.Equal(t, "second", collector.Text())
+}
+
+func TestFollowUpCollector_SortsExtensions(t *testing.T) {
+	collector := NewFollowUpCollector()
+
+	collector.Add("z.extension", "z")
+	collector.Add("a.extension", "a")
+
+	require.Equal(t, "a\n\nz", collector.Text())
+}
+
+func TestFollowUpCollector_IgnoresBlankText(t *testing.T) {
+	collector := NewFollowUpCollector()
+
+	collector.Add("test.extension", " \n\t ")
+
+	require.Empty(t, collector.Text())
+}
+
+func TestFollowUpCollector_ConcurrentWrites(t *testing.T) {
+	collector := NewFollowUpCollector()
+	var group sync.WaitGroup
+
+	for i := range 100 {
+		group.Go(func() {
+			collector.Add(
+				fmt.Sprintf("extension-%d", i),
+				fmt.Sprintf("follow-up-%d", i),
+			)
+		})
+	}
+
+	group.Wait()
+	require.Len(t, collector.contributions, 100)
+}
+
+func TestFollowUpCollector_Context(t *testing.T) {
+	collector := NewFollowUpCollector()
+	ctx := WithFollowUpCollector(t.Context(), collector)
+
+	require.Same(t, collector, FollowUpCollectorFromContext(ctx))
+	require.Nil(t, FollowUpCollectorFromContext(t.Context()))
+}

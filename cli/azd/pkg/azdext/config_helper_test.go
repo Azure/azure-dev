c143efa2f5840118f1331dee03a75e3c56cdb76e
huimiu <miuhui@outlook.com>
2026-09-08 14:48:48 +0800

    fix: update environment service test stub

--- cli/azd/pkg/azdext/config_helper_test.go
+++ cli/azd/pkg/azdext/config_helper_test.go
@@ -107,6 +107,12 @@ func (s *stubEnvironmentService) SetValue(
 	return nil, nil
 }
 
+func (s *stubEnvironmentService) UnsetValue(
+	_ context.Context, _ *GetEnvRequest, _ ...grpc.CallOption,
+) (*EmptyResponse, error) {
+	return &EmptyResponse{}, nil
+}
+
 func (s *stubEnvironmentService) GetConfig(
 	_ context.Context, _ *GetConfigRequest, _ ...grpc.CallOption,
 ) (*GetConfigResponse, error) {

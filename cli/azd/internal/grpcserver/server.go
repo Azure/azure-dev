96935f719dc549226264ce70056472dad852f785
Rick Winter <4430337+RickWinter@users.noreply.github.com>
2026-09-24 15:00:32 -0700

    fix telemetry authentication tracking for versioned RPCs

--- cli/azd/internal/grpcserver/server.go
+++ cli/azd/internal/grpcserver/server.go
@@ -250,7 +250,8 @@ func (s *Server) tokenAuthInterceptor(serverInfo *ServerInfo) grpc.UnaryServerIn
 	) (any, error) {
 		ctx, err := s.validateAuthToken(ctx, serverInfo)
 		if err != nil {
-			if info.FullMethod == azdext.TelemetryService_ReportUsage_FullMethodName {
+			if info.FullMethod == v1beta.TelemetryService_ReportUsage_FullMethodName ||
+				info.FullMethod == "/azdext.TelemetryService/ReportUsage" {
 				recordExtensionUsageDrop(unattributedExtensionId, extensionUsageDropReasonUnauthenticated)
 			}
 			return nil, err
